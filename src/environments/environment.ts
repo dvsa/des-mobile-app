9ba3d928c31a277f8bccdea66e1e4637c7b6fc29
andrew <andrew.setterfield@dvsa.gov.uk>
2020-10-15 11:40:54 +0100

    updated environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,26 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// TODO This is for dev only and will need to be configured for other environments
+// TODO add config schema (MdmConfig)
+
+export const environment = {
+  production: false,
+  isRemote: true,
+  configUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/configuration/dev',
+  daysToCacheLogs: 7,
+  enableDevTools: true,
+  logoutClearsTestPersistence: true,
+  logsPostApiKey: '',
+  logsApiUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/logs',
+  logsAutoSendInterval: 6000,
+  authentication: {
+    context: 'https://login.microsoftonline.com/6c448d90-4ca1-4caf-ab59-0a2aa67d7801',
+    resourceUrl: '923b07d4-80ee-4524-8f38-c1230aefe151',
+    clientId: '923b07d4-80ee-4524-8f38-c1230aefe151',
+    redirectUrl: 'mesmobileappscheme://callback',
+    logoutUrl: 'mesmobileappscheme://callback?logout=true',
+    employeeIdKey: 'employeeid',
+  },
+};
