362a7861bee30478a4ae6a297ef68410770675a3
andrew <andrew.setterfield@dvsa.gov.uk>
2020-10-15 11:17:08 +0100

    adding plugins for auth connect and secure storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+  goToLogin() {
+    console.log('goToLogin');
+    this.router.navigate(['login']);
+  }
 
 }
