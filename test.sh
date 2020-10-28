bb2af529d0f0cfa2850aa887b15234f80cd9332a
andrew <andrew.setterfield@dvsa.gov.uk>
2020-10-28 09:51:29 +0000

    add script

--- test.sh
+++ test.sh
@@ -1,10 +1,17 @@
-array=(
+pages=(
 journal
 dashboard
+back-to-office/cat-a-mod1
+back-to-office/cat-a-mod2
+back-to-office/cat-adi-part2
+back-to-office/cat-b
+back-to-office/cat-b
+candidatedetails
+communications-page/cat-a-mod1
+communications-page/cat-a-mod2
 )
 
-for i in "${array[@]}"
+for i in "${pages[@]}"
 do
-	ionic g page pages/$i --dry-run
-	wait
+	ionic g page pages/$i
 done
