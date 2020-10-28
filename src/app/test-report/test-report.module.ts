7823c0eda952f12f34d118458d59970e03a951de
Tom <thomascrawley99@gmail.com>
2020-10-28 11:25:18 +0000

    removing almost everything

--- src/app/test-report/test-report.module.ts
+++ src/app/test-report/test-report.module.ts
@@ -4,23 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TestReportPage } from './test-report.page';
 import { TestReportPageRoutingModule } from './test-report-routing.module';
-import { ToolbarComponent } from './components/toolbar/toolbar';
 import { CompetencyComponent } from './components/competency/competency';
-import { CommonComponentsModule } from '../components/common/common-components.module';
-import { CompetencyButtonComponent } from './components/competency-button/competency-button';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CommonComponentsModule,
     TestReportPageRoutingModule,
   ],
   declarations: [
-    ToolbarComponent,
     CompetencyComponent,
-    CompetencyButtonComponent,
     TestReportPage,
   ],
 })
