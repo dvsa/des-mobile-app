3ba7f07d1023bac37bc31b0afe0131939cf11c25
andrew <andrew.setterfield@dvsa.gov.uk>
2020-10-28 11:28:11 +0000

    add prefix

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 pages=(
 journal
 dashboard
-back-to-office/cat-a-mod1
+"back-to-office/cat-a-mod1 --prefix=back-to-office"
 back-to-office/cat-a-mod2
 back-to-office/cat-adi-part2
-back-to-office/cat-b
 back-to-office/cat-b
 candidatedetails
 communications-page/cat-a-mod1
@@ -12,7 +11,9 @@ communications-page/cat-a-mod2
 communications-page/cat-adi-part2
 )
 
+echo "Create folder structure/pages for DES"
+
 for i in "${pages[@]}"
 do
-	ionic g page pages/$i
+	ng g page pages/$i
 done
