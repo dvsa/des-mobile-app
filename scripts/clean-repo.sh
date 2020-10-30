81985a70d5e7a629142653742516df646306a04d
Szabi Keresztes <szabi.keresztes@bjss.com>
2020-10-30 16:57:44 +0000

    extending clean-repo.sh to uninstall ngrx and set ionic cli as default collection

--- scripts/clean-repo.sh
+++ scripts/clean-repo.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 git reset --hard
 git clean -f -d 
+
+npm uninstall @ngrx/schematics
+npm uninstall @ngrx/{store,effects,store-devtools}
+
+ng config cli.defaultCollection @ionic/angular-toolkit
