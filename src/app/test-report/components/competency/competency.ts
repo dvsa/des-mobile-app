722e45df6f2d3c3502600a12c4271ecdbceec1e0
Tom <thomascrawley99@gmail.com>
2020-11-02 09:40:02 +0000

    current state + element being binded to

--- src/app/test-report/components/competency/competency.ts
+++ src/app/test-report/components/competency/competency.ts
@@ -4,15 +4,23 @@ import { AfterViewInit, Component, Input } from '@angular/core';
   selector: 'app-competency',
   templateUrl: 'competency.html',
 })
-export class CompetencyComponent {
+export class CompetencyComponent implements AfterViewInit {
 
   @Input()
   competency: string;
 
   faultCount: number;
 
+  hammerInstance;
+
   constructor() {
     this.faultCount = 0;
+  }
+
+  ngAfterViewInit() {
+    const element = document.getElementById('competency-button');
+    this.hammerInstance = new (window as any).Hammer(element);
+    this.hammerInstance.on('press', this.onPress.bind(this));
   }
 
   onPress($event) {
