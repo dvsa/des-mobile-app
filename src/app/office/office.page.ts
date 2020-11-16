1b354f8fd73ad3bc0a6b33c16a91dca7a3569d54
Tom <thomascrawley99@gmail.com>
2020-11-16 14:12:42 +0000

    removed stuff

--- src/app/office/office.page.ts
+++ src/app/office/office.page.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-office-page',
   templateUrl: 'office.page.html',
+  styleUrls: ['office.page.scss'],
 })
 export class OfficePage { }
