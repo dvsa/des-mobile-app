4e712379f4665fe7a91c3af26fed643715e35a8d
Pierce Morris <pierce.morris1998@gmail.com>
2020-12-03 13:18:09 +0000

    add env files and commands

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false,
+  isRemote: true,
+  configUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/configuration/dev',
+  enableDevTools: true,
+  logoutClearsTestPersistence: true,
+  logsPostApiKey: '',
+  logsApiUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/logs',
+  logsAutoSendInterval: 6000,
+  authentication: {
+    context: 'https://login.microsoftonline.com/6c448d90-4ca1-4caf-ab59-0a2aa67d7801',
+    resourceUrl: '923b07d4-80ee-4524-8f38-c1230aefe151',
+    clientId: '923b07d4-80ee-4524-8f38-c1230aefe151',
+    redirectUrl: 'mesmobileappscheme://callback',
+    logoutUrl: 'mesmobileappscheme://callback?logout=true',
+    employeeIdKey: 'employeeid',
+  },
+};
