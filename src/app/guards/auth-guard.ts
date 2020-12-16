289bbb784b2b219c9279c6690b2073609a74f199
tomcrawleyy <thomascrawley99@gmail.com>
2020-12-16 12:09:19 +0000

    Mes 6220 configure npm scripts (#11)
    * install ionic scripts
    
    * bypass auth if serve locally
    
    * add env files and commands
    
    * add serve:local script
    
    * git ignore ios folder
    
    * install ionic scripts
    
    * add env files and commands
    
    * add serve:local script
    
    * git ignore ios folder
    
    * git stash
    
    * updating package lock to sort merge conflicts
    
    * updating package lock to sort merge conflicts
    
    * updated package.lock
    
    * making sure it is inline with develop
    
    * updating scripts in package.json
    
    * flipped prefers color back and added indicator to show that serve:ipad is broken
    
    * addressed comments
    
    * move husky to dev dependencies
    
    Co-authored-by: Pierce Morris <pierce.morris1998@gmail.com>

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { Device } from '@capacitor/core';
 import { AuthenticationProvider } from '../providers/authentication/authentication';
 
 @Injectable()
 
 export class AuthGuard implements CanActivate {
+
   constructor(
     public authenticationProvider: AuthenticationProvider,
   ) {
   }
 
   async canActivate(): Promise<boolean> {
+    if (this.isIos()) {
+      return Promise.resolve(true);
+    }
     return this.authenticationProvider.isAuthenticated();
+  }
+
+  async isIos(): Promise<boolean> {
+    const info = await Device.getInfo();
+    return info.platform === 'ios';
   }
 
 }
