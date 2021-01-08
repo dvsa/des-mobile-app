61e8311c417b631d49350a360b624d824f163633
tomcrawleyy <thomascrawley99@gmail.com>
2021-01-08 09:30:45 +0000

    MES-6362 Authentication Error Screens (#17)
    * git stash
    
    * removed console log and fixed user not authorized screen
    
    * remove eslint disable

--- src/app/providers/authentication/authentication.constants.ts
+++ src/app/providers/authentication/authentication.constants.ts
@@ -1,6 +1,7 @@
 export enum AuthenticationError {
   NO_INTERNET = 'The Internet connection appears to be offline.',
-  USER_CANCELLED = 'User cancelled authentication flow',
+  USER_CANCELLED = 'The operation couldn’t be completed. '
+    + '(com.apple.AuthenticationServices.WebAuthenticationSession error 1.). (null)',
   NO_RESPONSE = 'application did not receive response from broker',
-  USER_NOT_AUTHORISED = 'user not authorised',
+  USER_NOT_AUTHORISED = 'User is not authorized to access this message with an explicit deny',
 }
