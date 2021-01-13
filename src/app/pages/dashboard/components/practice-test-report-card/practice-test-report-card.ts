4217a285f6455e15a650d0f6a7d9109f1aa5b4e5
Pierce Morris <pierce.morris1998@gmail.com>
2021-01-13 15:26:15 +0000

    remove commented code

--- src/app/pages/dashboard/components/practice-test-report-card/practice-test-report-card.ts
+++ src/app/pages/dashboard/components/practice-test-report-card/practice-test-report-card.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { AlertController, ModalController } from '@ionic/angular';
 import { Store } from '@ngrx/store';
 import { StoreModel } from '../../../../shared/models/store.model';
-// import { StartTestReportPracticeTest } from '../../../../modules/tests/tests.actions';
-// import { TellMeQuestionDrivingFault, TellMeQuestionCorrect }
-// from '../../../../modules/tests/test-data/cat-b/vehicle-checks/vehicle-checks.actions';
-// import { ModalEvent } from '../practice-test-modal/practice-test-modal.constants';
-// import { testReportPracticeModeSlot } from '../../../../modules/tests/__mocks__/tests.mock';
-// import { CAT_B } from '../../../page-names.constants';
 
 @Component({
   selector: 'practice-test-report-card',
@@ -17,9 +11,6 @@ import { StoreModel } from '../../../../shared/models/store.model';
 
 export class PracticeTestReportCardComponent {
 
-  // slotId: string = testReportPracticeModeSlot.slotDetail.slotId;
-  modal: any;
-
   constructor(
     private store$: Store<StoreModel>,
     public alertController: AlertController,
@@ -27,29 +18,11 @@ export class PracticeTestReportCardComponent {
   ) { }
 
   showDrivingFaultModal = (): void => {
-    /*
-    const options = { cssClass: 'mes-modal-alert text-zoom-regular' };
-    this.modal = this.modalController.create(options);
-    this.modal.onDidDismiss(this.onModalDismiss);
-    this.modal.present();
-    */
+    console.log('Practice test modal to be implemented');
   };
 
-  onModalDismiss = (): void => { // event: ModalEvent
-    /* switch (event) {
-      case ModalEvent.FAULT:
-        this.store$.dispatch(new StartTestReportPracticeTest(this.slotId));
-        this.store$.dispatch(new TellMeQuestionDrivingFault());
-        this.navController.push(CAT_B.TEST_REPORT_PAGE);
-        break;
-      case ModalEvent.NO_FAULT:
-        this.store$.dispatch(new StartTestReportPracticeTest(this.slotId));
-        this.store$.dispatch(new TellMeQuestionCorrect());
-        this.navController.push(CAT_B.TEST_REPORT_PAGE);
-        break;
-      case ModalEvent.CANCEL:
-        break;
-    } */
+  onModalDismiss = (): void => {
+    console.log('Practice test modal dismiss to be implemented');
   };
 
 }
