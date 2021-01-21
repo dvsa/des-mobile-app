58f10b03828e5561bbaf8c7947816ecc895e2914
Pierce Morris <pierce.morris1998@gmail.com>
2021-01-21 10:41:04 +0000

    add bike-category-detail provider

---
+++ src/app/shared/constants/bike-category-details/bike-category-details.ts
@@ -0,0 +1,59 @@
+import { BikeCategoryDetail, BikeTestType } from '../../../providers/bike-category-detail/bike-category-detail.model';
+
+export default [
+  {
+    testType: BikeTestType.MOD1,
+    categoryCode: 'EUAMM1',
+    displayId: 'AM',
+    displayName: 'Moped',
+    imageUrl: 'assets/imgs/bike-category-icons/AM.png',
+  },
+  {
+    testType: BikeTestType.MOD1,
+    categoryCode: 'EUA1M1',
+    displayId: 'A1',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A1.png',
+  },
+  {
+    testType: BikeTestType.MOD1,
+    categoryCode: 'EUA2M1',
+    displayId: 'A2',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A2.png',
+  },
+  {
+    testType: BikeTestType.MOD1,
+    categoryCode: 'EUAM1',
+    displayId: 'A',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A.png',
+  }, {
+    testType: BikeTestType.MOD2,
+    categoryCode: 'EUAMM2',
+    displayId: 'AM',
+    displayName: 'Moped',
+    imageUrl: 'assets/imgs/bike-category-icons/AM.png',
+  },
+  {
+    testType: BikeTestType.MOD2,
+    categoryCode: 'EUA1M2',
+    displayId: 'A1',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A1.png',
+  },
+  {
+    testType: BikeTestType.MOD2,
+    categoryCode: 'EUA2M2',
+    displayId: 'A2',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A2.png',
+  },
+  {
+    testType: BikeTestType.MOD2,
+    categoryCode: 'EUAM2',
+    displayId: 'A',
+    displayName: 'Motorcycle',
+    imageUrl: 'assets/imgs/bike-category-icons/A.png',
+  },
+] as BikeCategoryDetail[];
