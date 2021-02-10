468563d4ccde83fcbb72a9aae7086fc7cba2df83
andrew <andrew.setterfield@dvsa.gov.uk>
2021-02-10 07:38:10 +0000

    writing unit tests

--- src/app/pages/candidate-details/candidate-details.page.spec.ts
+++ src/app/pages/candidate-details/candidate-details.page.spec.ts
@@ -3,7 +3,7 @@ import { IonicModule, ModalController, NavParams } from '@ionic/angular';
 
 import {
   provideMockStore,
-  // MockStore,
+  MockStore,
 } from '@ngrx/store/testing';
 import { ModalControllerMock } from 'ionic-mocks';
 import { MockComponent } from 'ng-mocks';
@@ -12,7 +12,6 @@ import { CandidateDetailsPage } from './candidate-details.page';
 import { DisplayAddressComponent } from '../../../components/common/display-address/display-address';
 import { DataRowComponent } from '../../../components/common/data-row/data-row';
 import { DataRowCustomComponent } from '../../../components/common/data-row-custom/data-row-custom';
-import * as candidateDetails from '../../../store/candidate-details/candidate-details.selector';
 
 // export const spyOnFunction = <T>(classObject: T, method: keyof T): jasmine.Spy => {
 //   const spy: jasmine.Spy = jasmine.createSpy(method as string);
@@ -23,8 +22,12 @@ import * as candidateDetails from '../../../store/candidate-details/candidate-de
 fdescribe('CandidateDetailsPage', () => {
   let component: CandidateDetailsPage;
   let fixture: ComponentFixture<CandidateDetailsPage>;
-  // let store$: MockStore;
-  const initialState = { loggedIn: false };
+  let store$: MockStore;
+  const initialState = {
+    test: {
+      output: 'string',
+    },
+  };
 
   const mockNavParams = {
     get: () => {
@@ -73,7 +76,7 @@ fdescribe('CandidateDetailsPage', () => {
   beforeEach(async(() => {
     fixture = TestBed.createComponent(CandidateDetailsPage);
     component = fixture.componentInstance;
-    // store$ = TestBed.inject(MockStore);
+    store$ = TestBed.inject(MockStore);
   }));
 
   describe('Should be correctly configured', () => {
@@ -82,11 +85,26 @@ fdescribe('CandidateDetailsPage', () => {
     });
   });
 
-  describe('ngOnInit', () => {
-    it('should test ngOnInit', () => {
-      spyOnProperty(candidateDetails, 'getCandidateName', 'get').and.returnValue('Mr Tim Burr');
-      component.ngOnInit();
-      expect(true).toEqual(true);
+  // describe('ngOnInit', () => {
+  //   it('should test ngOnInit', () => {
+  //     // spyOnProperty(candidateDetails, 'getCandidateName', 'get').and.returnValue('Mr Tim Burr');
+  //     const testVar = getCandidateName.projector('test');
+  //     component.ngOnInit();
+  //     expect(true).toEqual(true);
+  //   });
+  // });
+
+  describe('ionViewDidEnter', () => {
+    it('should test ionViewDidEnter', () => {
+      spyOn(store$, 'dispatch').and.stub();
+      component.slot = {
+        slotDetail: {
+          slotId: 123,
+          start: '123',
+        },
+      };
+      component.ionViewDidEnter();
+      expect(store$.dispatch).toHaveBeenCalledTimes(2);
     });
   });
 
