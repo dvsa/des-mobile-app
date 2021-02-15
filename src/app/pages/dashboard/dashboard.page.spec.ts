8e0f9d5a1f5530a3ad683307a3d1e526567d2e91
matthew <m.bell2412@hotmail.com>
2021-02-15 10:54:20 +0000

    remove x describe from dashboard page and add unit tests and coverage

--- src/app/pages/dashboard/dashboard.page.spec.ts
+++ src/app/pages/dashboard/dashboard.page.spec.ts
@@ -5,15 +5,35 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { configureTestSuite } from 'ng-bullet';
 import { StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { DashboardPage } from './dashboard.page';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
 import { appInfoReducer } from '../../../store/app-info/app-info.reducer';
 import { AuthenticationProviderMock } from '../../providers/authentication/__mocks__/authentication.mock';
+import { AppConfigProvider } from '../../providers/app-config/app-config';
+import { AppConfigProviderMock } from '../../providers/app-config/__mocks__/app-config.mock';
+import { DateTimeProvider } from '../../providers/date-time/date-time';
+import { DateTimeProviderMock } from '../../providers/date-time/__mocks__/date-time.mock';
+import { ExaminerRole } from '../../providers/app-config/constants/examiner-role.constants';
+import { AppConfig } from '../../providers/app-config/app-config.model';
+import { LOGIN_PAGE } from '../page-names.constants';
+import { DateTime } from '../../shared/helpers/date-time';
+import { selectEmployeeName, selectVersionNumber } from '../../../store/app-info/app-info.selectors';
+import { StoreModel } from '../../shared/models/store.model';
 
-xdescribe('DashboardPage', () => {
+describe('DashboardPage', () => {
   let component: DashboardPage;
   let fixture: ComponentFixture<DashboardPage>;
-  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
+  let appConfigProvider: AppConfigProvider;
+  let authenticationProvider: AuthenticationProvider;
+  let store$: MockStore;
+  const initialState = {
+    appInfo: {
+      versionNumber: '4.0',
+      employeeName: 'Some One',
+    },
+  } as StoreModel;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -29,9 +49,12 @@ xdescribe('DashboardPage', () => {
       ],
       providers: [
         { provide: Platform, useFactory: () => PlatformMock.instance() },
-        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
         { provide: AlertController, useFactory: () => AlertControllerMock.instance() },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+        { provide: AppConfigProvider, useClass: AppConfigProviderMock },
+        { provide: DateTimeProvider, useClass: DateTimeProviderMock },
         { provide: Router, useValue: routerSpy },
+        provideMockStore({ initialState }),
       ],
     });
   });
@@ -40,9 +63,91 @@ xdescribe('DashboardPage', () => {
     fixture = TestBed.createComponent(DashboardPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    appConfigProvider = TestBed.inject(AppConfigProvider);
+    authenticationProvider = TestBed.inject(AuthenticationProvider);
+    store$ = TestBed.inject(MockStore);
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should get the selectVersionNumber and selectEmployeeName from store on init', () => {
+      const spy = spyOn(store$, 'select');
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalledTimes(2);
+      expect(spy.calls.allArgs()).toEqual([
+        [selectVersionNumber],
+        [selectEmployeeName],
+      ]);
+    });
+  });
+  describe('ionViewWillEnter', () => {
+    it('should set todaysDate and todaysDateFormatted on view will enter', () => {
+      component.ionViewWillEnter();
+      expect(component.todaysDate).toEqual(new DateTime('2019-02-01'));
+      expect(component.todaysDateFormatted).toEqual('Friday 1st February 2019');
+    });
+  });
+  describe('goToLogin', () => {
+    it('should navigate to login page', () => {
+      spyOn(routerSpy, 'navigate');
+      component.goToLogin();
+      expect(routerSpy.navigate).toHaveBeenCalledWith([LOGIN_PAGE]);
+    });
+  });
+  describe('onLogout', () => {
+    it('should should call openLogoutModal', () => {
+      spyOn(component, 'openLogoutModal');
+      component.onLogout();
+      expect(component.openLogoutModal).toHaveBeenCalled();
+    });
+  });
+  describe('showTestReportPracticeMode', () => {
+    it('should return true when enableTestReportPracticeMode is true', () => {
+      expect(component.showTestReportPracticeMode()).toEqual(true);
+    });
+    it('should return false when enableTestReportPracticeMode is not true', () => {
+      spyOn(appConfigProvider, 'getAppConfig').and.returnValue({
+        journal: {
+          enableTestReportPracticeMode: false,
+        },
+      } as AppConfig);
+      expect(component.showTestReportPracticeMode()).toEqual(false);
+    });
+  });
+  describe('showEndToEndPracticeMode', () => {
+    it('should return true when enableEndToEndPracticeMode is true', () => {
+      expect(component.showEndToEndPracticeMode()).toEqual(true);
+    });
+    it('should return false when enableEndToEndPracticeMode is not true', () => {
+      spyOn(appConfigProvider, 'getAppConfig').and.returnValue({
+        journal: {
+          enableEndToEndPracticeMode: false,
+        },
+      } as AppConfig);
+      expect(component.showEndToEndPracticeMode()).toEqual(false);
+    });
+  });
+  describe('isLogoutEnabled', () => {
+    it('should return true when logout is enabled', () => {
+      spyOn(authenticationProvider, 'logoutEnabled').and.returnValue(true);
+      expect(component.isLogoutEnabled()).toEqual(true);
+    });
+    it('should return false when logout is not enabled', () => {
+      spyOn(authenticationProvider, 'logoutEnabled').and.returnValue(false);
+      expect(component.isLogoutEnabled()).toEqual(false);
+    });
+  });
+  describe('showDelegatedExaminerRekey', () => {
+    it('should return false when role is not DLG', () => {
+      expect(component.showDelegatedExaminerRekey()).toEqual(false);
+    });
+    it('should return true when role is DLG', () => {
+      spyOn(appConfigProvider, 'getAppConfig').and.returnValue({ role: ExaminerRole.DLG } as AppConfig);
+      expect(component.showDelegatedExaminerRekey()).toEqual(true);
+    });
   });
 });
