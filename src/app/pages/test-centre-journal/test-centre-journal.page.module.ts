56ab3bedadbaad3d67d587db7a76e725d3a451be
Tom <thomascrawley99@gmail.com>
2021-03-04 14:56:46 +0000

    updated commit

---
+++ src/app/pages/test-centre-journal/test-centre-journal.page.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { TestCentreJournalPage } from './test-centre-journal.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [
+    TestCentreJournalPage,
+  ],
+})
+export class TestCentreJournalPageModule { }
---
+++ src/app/pages/test-centre-journal/test-centre-journal.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BasePageComponent } from '../../shared/classes/base-page';
+
+@Component({
+  selector: 'app-test-centre-journal',
+  templateUrl: 'test-centre-journal.page.html',
+  styleUrls: ['test-centre-journal.page.scss'],
+})
+export class TestCentreJournalPage extends BasePageComponent {
+
+}
