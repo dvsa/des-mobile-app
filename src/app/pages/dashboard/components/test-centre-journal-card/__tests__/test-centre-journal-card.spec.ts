85c31789b9f49ed64e00ba0dd414b7804a45beda
Tom <thomascrawley99@gmail.com>
2021-03-09 14:10:09 +0000

    add ng-bullet to spec

--- src/app/pages/dashboard/components/test-centre-journal-card/__tests__/test-centre-journal-card.spec.ts
+++ src/app/pages/dashboard/components/test-centre-journal-card/__tests__/test-centre-journal-card.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { configureTestSuite } from 'ng-bullet';
 import { TestCentreJournalCardComponent } from '../test-centre-journal-card';
 import { TEST_CENTRE_JOURNAL_PAGE } from '../../../../page-names.constants';
 
@@ -10,21 +11,23 @@ describe('TestCentreJournalCard', () => {
   let fixture: ComponentFixture<TestCentreJournalCardComponent>;
   const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TestCentreJournalCardComponent],
-      imports: [
-        CommonModule,
-        IonicModule,
-      ],
-      providers: [
-        { provide: Router, useValue: routerSpy },
-      ],
-    }).compileComponents();
-    fixture = TestBed.createComponent(TestCentreJournalCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+  configureTestSuite(() => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestCentreJournalCardComponent],
+        imports: [
+          CommonModule,
+          IonicModule,
+        ],
+        providers: [
+          { provide: Router, useValue: routerSpy },
+        ],
+      }).compileComponents();
+      fixture = TestBed.createComponent(TestCentreJournalCardComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
