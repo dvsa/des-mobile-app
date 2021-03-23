06706a8a0187b6fce4ebe6a4f71799ad30107486
Matthew Bell <mbell.dva@icloud.com>
2021-03-23 11:19:12 +0000

    re-introduce broken spec; add mocks; unsubscribe from subscriptions opened on destroy;

--- src/app/shared/models/error-message.ts
+++ src/app/shared/models/error-message.ts
@@ -6,4 +6,5 @@ export enum ErrorTypes {
   TEST_CENTRE_JOURNAL_NO_RESULT = 'User does not have a corresponding row in test centre table',
   TEST_CENTRE_JOURNAL_ERROR = 'Unable to retrieve test centre journal',
   TEST_CENTRE_OFFLINE = 'Offline whilst trying to access Test Centre Journal',
+  TEST_CENTRE_UNKNOWN_ERROR = 'Test centre journal error',
 }
--- src/components/common/error-message/__tests__/error-message.spec.ts
+++ src/components/common/error-message/__tests__/error-message.spec.ts
@@ -1,21 +1,29 @@
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { configureTestSuite } from 'ng-bullet';
+import { Location } from '@angular/common';
 import { ErrorMessageComponent, additionalText } from '../error-message';
 import { ErrorTypes } from '../../../../app/shared/models/error-message';
 
 describe('ErrorMessageComponent', () => {
   let fixture: ComponentFixture<ErrorMessageComponent>;
   let component: ErrorMessageComponent;
+  let location: Location;
+  const locationSpy = jasmine.createSpyObj('Location', ['back']);
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [ErrorMessageComponent],
+      providers: [
+        { provide: Location, useValue: locationSpy },
+      ],
     });
   });
 
   beforeEach(async(() => {
     fixture = TestBed.createComponent(ErrorMessageComponent);
     component = fixture.componentInstance;
+
+    location = TestBed.inject(Location);
   }));
 
   describe('Class', () => {
@@ -43,6 +51,48 @@ describe('ErrorMessageComponent', () => {
         expect(component.additionalText).toEqual(additionalText.STANDARD_TEXT);
         expect(component.redirectLinkText).toEqual('RandomPage');
         expect(component.adviceToUsePaperTest).toEqual(false);
+      });
+      it('should set defaultErrorStatement to indicate no results for today or tomorrow', () => {
+        component.returnTo = ErrorTypes.TEST_CENTRE_JOURNAL_NO_RESULT;
+        fixture.detectChanges();
+        component.ngOnInit();
+        expect(component.defaultErrorStatement)
+          .toEqual('There are no test bookings at this location for today and tomorrow');
+      });
+      it('should set additionalText to a message refering to refreshing', () => {
+        component.returnTo = ErrorTypes.TEST_CENTRE_JOURNAL_ERROR;
+        fixture.detectChanges();
+        component.ngOnInit();
+        expect(component.additionalText).toEqual(additionalText.TRY_REFRESHING);
+        expect(component.redirectLinkText).toEqual('Dashboard');
+      });
+      it('should set defaultErrorStatement to indicate app id currently offline', () => {
+        component.returnTo = ErrorTypes.TEST_CENTRE_OFFLINE;
+        fixture.detectChanges();
+        component.ngOnInit();
+        expect(component.defaultErrorStatement)
+          .toEqual('To view the Test Centre Journal please refresh once you are back online.');
+        expect(component.redirectLinkText).toEqual('Dashboard');
+      });
+      it('should set the redirectLinkText to Dashboard when unknown error via test centre', () => {
+        component.returnTo = ErrorTypes.TEST_CENTRE_UNKNOWN_ERROR;
+        fixture.detectChanges();
+        component.ngOnInit();
+        expect(component.redirectLinkText).toEqual('Dashboard');
+      });
+    });
+    describe('navigateBack', () => {
+      it('should call the back method from Location to navigate back', () => {
+        spyOn(location, 'back');
+        component.navigateBack();
+        expect(location.back).toHaveBeenCalled();
+      });
+    });
+    describe('dismiss', () => {
+      it('should call emit from exitModal when dismiss is called', () => {
+        spyOn(component.exitModal, 'emit');
+        component.dismiss();
+        expect(component.exitModal.emit).toHaveBeenCalled();
       });
     });
   });
