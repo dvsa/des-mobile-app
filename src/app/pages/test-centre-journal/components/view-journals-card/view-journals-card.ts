95f21424cbe5216b24fd67eb24c604354cd86389
Matthew Bell <mbell.dva@icloud.com>
2021-03-24 13:06:32 +0000

    amend html and add in red background for testing;

--- src/app/pages/test-centre-journal/components/view-journals-card/view-journals-card.ts
+++ src/app/pages/test-centre-journal/components/view-journals-card/view-journals-card.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+} from '@angular/core';
+import { ExaminerWorkSchedule } from '@dvsa/mes-journal-schema';
 import { TestCentreDetailResponse } from '../../../../shared/models/test-centre-journal.model';
 
 @Component({
@@ -8,16 +12,29 @@ import { TestCentreDetailResponse } from '../../../../shared/models/test-centre-
 })
 export class ViewJournalsCardComponent {
 
-  @Input() testCentreResults: TestCentreDetailResponse;
+  @Input()
+  testCentreResults: TestCentreDetailResponse;
+
+  journal: ExaminerWorkSchedule | null = null;
 
   get interfaceOptions() {
     return {
       cssClass: 'mes-select',
       enableBackdropDismiss: false,
       placeholder: 'Select examiner',
-      okText: 'Submit',
+      okText: 'Select',
       cancelText: 'Cancel',
     };
   }
+
+  examinerChanged = (staffNumber: string): void => {
+    const { journal } = this.testCentreResults?.examiners.find((examiner) => examiner.staffNumber === staffNumber);
+    this.journal = journal;
+  };
+
+  onShowJournalClick = (): void => {
+    // createSlots with this.journal
+    console.log(this.journal);
+  };
 
 }
