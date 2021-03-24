f391243c0a164f8e46df63cdf0659c1677caa3f0
Matthew Bell <mbell.dva@icloud.com>
2021-03-24 08:19:26 +0000

    create component for view journals card; create new component for test centre row display;

--- src/app/pages/test-centre-journal/components/test-centre-journal-components.module.ts
+++ src/app/pages/test-centre-journal/components/test-centre-journal-components.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { CandidateSearchCardComponent } from './candidate-search-card/candidate-search-card';
+import { TestCentreNameComponent } from './test-centre-name/test-centre-name';
+import { ViewJournalsCardComponent } from './view-journals-card/view-journals-card';
 import { ComponentsModule } from '../../../../components/common/common-components.module';
 
 @NgModule({
   declarations: [
     CandidateSearchCardComponent,
+    TestCentreNameComponent,
+    ViewJournalsCardComponent,
   ],
   imports: [
     IonicModule,
@@ -15,6 +19,8 @@ import { ComponentsModule } from '../../../../components/common/common-component
   ],
   exports: [
     CandidateSearchCardComponent,
+    TestCentreNameComponent,
+    ViewJournalsCardComponent,
   ],
 })
 export class TestCentreJournalComponentsModule { }
---
+++ src/app/pages/test-centre-journal/components/test-centre-name/test-centre-name.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { TestCentre } from '../../../../shared/models/test-centre-journal.model';
+
+@Component({
+  selector: 'test-centre-name',
+  templateUrl: 'test-centre-name.html',
+  styleUrls: ['test-centre-name.scss'],
+})
+export class TestCentreNameComponent {
+
+  @Input() testCentres: TestCentre[] = [];
+
+  get testCentreNames(): string {
+    return this.testCentres?.map((testCentre: TestCentre) => testCentre.name).join(', ');
+  }
+
+}
