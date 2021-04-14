8ce95c979c986b0b5884d0e9ca3a0261cf79d181
Andrew Setterfield <andrew.setterfield@dvsa.gov.uk>
2021-04-14 14:07:34 +0100

    route-by-category.spec.ts updated

--- src/app/providers/route-by-category/__tests__/route-by-category.spec.ts
+++ src/app/providers/route-by-category/__tests__/route-by-category.spec.ts
@@ -2,12 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { configureTestSuite } from 'ng-bullet';
 import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
 import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import * as pageConstants from '@pages/page-names.constants';
-import { RouterTestingModule } from '@angular/router/testing';
 import { JournalPage } from '@pages/journal/journal.page';
 
-fdescribe('RouteByCategoryProvider', () => {
+describe('RouteByCategoryProvider', () => {
 
   let provider: RouteByCategoryProvider;
   const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
@@ -34,14 +34,12 @@ fdescribe('RouteByCategoryProvider', () => {
 
   describe('navigateToPage', () => {
     it('should call router.navigate', async () => {
-      provider.router = {
-        config: [],
-      } as Router;
+      provider.router.config = [];
       spyOn(pageConstants, 'getPageNameByCategoryAndKey').and.returnValue('WaitingRoomToCarCatBPage');
       spyOn(provider, 'categoryToPage').and.returnValue('cat-b');
       spyOn(provider, 'pagePath').and.returnValue('waiting-room-to-car');
       await provider.navigateToPage('WAITING_ROOM_TO_CAR_PAGE', TestCategory.B);
-      expect(routerSpy.navigate).toHaveBeenCalled();
+      expect(routerSpy.navigate).toHaveBeenCalledWith(['WaitingRoomToCarCatBPage']);
     });
   });
 
