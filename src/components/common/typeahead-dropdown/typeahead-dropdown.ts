4910ef195ef8f640ff6e00b2e05b96a8d0f9d920
Tom <thomascrawley99@gmail.com>
2021-04-21 12:48:07 +0100

    .

--- src/components/common/typeahead-dropdown/typeahead-dropdown.ts
+++ src/components/common/typeahead-dropdown/typeahead-dropdown.ts
@@ -6,6 +6,10 @@ import {
   Component, Input, Output, EventEmitter,
 } from '@angular/core';
 
+enum TypeaheadFail {
+  NO_RESULTS = 'No results',
+}
+
 @Component({
   selector: 'typeahead-dropdown',
   templateUrl: 'typeahead-dropdown.html',
@@ -30,9 +34,13 @@ export class TypeaheadDropdownComponent {
     distinctUntilChanged(),
     filter((term) => term.length >= 3),
     map((term) => this.listValues.filter((state) => new RegExp(term, 'mi').test(state[this.property])).slice(0, 10)),
+    map((list) => list.length === 0 ? [{ [this.property]: TypeaheadFail.NO_RESULTS }] : list),
   );
 
   onModelChanged(value) {
+    if (value && value[this.property] === TypeaheadFail.NO_RESULTS) {
+      value = null;
+    }
     this.candidateChange.emit(value);
   }
 
