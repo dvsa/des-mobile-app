b8d3585ac6ab707323dfa197ca33fc7673449ea1
Andrew Setterfield <andrew.setterfield@dvsa.gov.uk>
2021-04-21 14:31:18 +0100

    rebase

--- src/app/pages/pass-finalisation/cat-a-mod1/pass-finalisation.cat-a-mod1-routing.module.ts
+++ src/app/pages/pass-finalisation/cat-a-mod1/pass-finalisation.cat-a-mod1-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { PassFinalisationCatAMod1Page } from './pass-finalisation.cat-a-mod1.page';
 
 const routes: Routes = [
