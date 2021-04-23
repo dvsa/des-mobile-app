56cd352fe47136b6e046aef52174603d3c5e975d
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2021-04-23 10:41:52 +0100

    MES-6830 / Office basepage (#134)
    * implement base page for office
    
    * fix type assertion

--- src/app/pages/office/cat-b/office.cat-b.page.ts
+++ src/app/pages/office/cat-b/office.cat-b.page.ts
@@ -1,24 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import { Router } from '@angular/router';
 import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
+import {
+  CommonOfficePageState,
+  OfficeBasePageComponent,
+} from '@shared/classes/test-flow-base-pages/office/office-base-page';
+import { Store } from '@ngrx/store';
+import { StoreModel } from '@shared/models/store.model';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+
 import { JOURNAL_PAGE, TestFlowPageNames } from '../../page-names.constants';
+
+interface CatBOfficePageState {}
+
+type OfficePageState = CommonOfficePageState & CatBOfficePageState;
 
 @Component({
   selector: 'app-office-cat-b',
   templateUrl: './office.cat-b.page.html',
   styleUrls: ['./office.cat-b.page.scss'],
 })
-export class OfficeCatBPage implements OnInit {
+export class OfficeCatBPage extends OfficeBasePageComponent implements OnInit {
+
+  pageState: OfficePageState;
 
   constructor(
     private navController: NavController,
     public routeByCat: RouteByCategoryProvider,
-    private router: Router,
-  ) { }
+    store$: Store<StoreModel>,
+    platform: Platform,
+    authenticationProvider: AuthenticationProvider,
+    router: Router,
+  ) {
+    super(store$, platform, authenticationProvider, router);
+  }
 
   ngOnInit() {
+    super.onInitialisation();
+    this.pageState = {
+      ...this.commonPageState,
+    };
   }
 
   navigateBack(): void {
