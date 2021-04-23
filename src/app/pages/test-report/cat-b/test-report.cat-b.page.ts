a077870a47eb2f2ae61c369b23334f9f845ebc4d
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2021-04-23 08:55:27 +0100

    implement base page for test report (#132)

--- src/app/pages/test-report/cat-b/test-report.cat-b.page.ts
+++ src/app/pages/test-report/cat-b/test-report.cat-b.page.ts
@@ -1,20 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, Platform } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
 import { TestFlowPageNames } from '@pages/page-names.constants';
+import {
+  CommonTestReportPageState,
+  TestReportBasePageComponent,
+} from '@shared/classes/test-flow-base-pages/test-report/test-report-base-page';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+import { StoreModel } from '@shared/models/store.model';
+
+interface CatBTestReportPageState {}
+
+type TestReportPageState = CommonTestReportPageState & CatBTestReportPageState;
 
 @Component({
   selector: 'app-test-report-cat-b',
   templateUrl: './test-report.cat-b.page.html',
   styleUrls: ['./test-report.cat-b.page.scss'],
 })
-export class TestReportCatBPage implements OnInit {
+export class TestReportCatBPage extends TestReportBasePageComponent implements OnInit {
+
+  pageState: TestReportPageState;
 
   constructor(
     private navController: NavController,
     public routeByCat: RouteByCategoryProvider,
-  ) { }
+    store$: Store<StoreModel>,
+    platform: Platform,
+    authenticationProvider: AuthenticationProvider,
+    router: Router,
+  ) {
+    super(store$, platform, authenticationProvider, router);
+  }
 
   ngOnInit() {
   }
