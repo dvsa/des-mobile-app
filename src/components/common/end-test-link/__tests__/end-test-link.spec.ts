b7086d2b5b6d85a3d4de51825a13536755a9c575
Matthew Bell <mbell.dvsa@icloud.com>
2021-04-30 11:04:36 +0100

    amend end test link to use new route by cat provider

--- src/components/common/end-test-link/__tests__/end-test-link.spec.ts
+++ src/components/common/end-test-link/__tests__/end-test-link.spec.ts
@@ -5,20 +5,13 @@ import { configureTestSuite } from 'ng-bullet';
 import { Router } from '@angular/router';
 import { RouterMock } from '@mocks/angular-mocks/router-mock';
 import { ModalControllerMock } from '@mocks/ionic-mocks/modal-controller.mock';
-import {
-  CAT_B,
-  CAT_BE,
-  CAT_C,
-  CAT_D,
-  CAT_A_MOD1,
-  CAT_A_MOD2,
-  CAT_ADI_PART2,
-  CAT_CPC, CAT_HOME_TEST,
-} from '@pages/page-names.constants';
+import { TestFlowPageNames } from '@pages/page-names.constants';
+import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
+import { RouteByCategoryProviderMock } from '@providers/route-by-category/__mocks__/route-by-category.mock';
 import { AppModule } from '../../../../app/app.module';
 import { EndTestLinkComponent } from '../end-test-link';
 
-xdescribe('EndTestLinkComponent', () => {
+describe('EndTestLinkComponent', () => {
   let fixture: ComponentFixture<EndTestLinkComponent>;
   let component: EndTestLinkComponent;
   let modalController: ModalController;
@@ -31,6 +24,7 @@ xdescribe('EndTestLinkComponent', () => {
       providers: [
         { provide: ModalController, useClass: ModalControllerMock },
         { provide: Router, useClass: RouterMock },
+        { provide: RouteByCategoryProvider, useClass: RouteByCategoryProviderMock },
       ],
     });
   });
@@ -42,133 +36,26 @@ xdescribe('EndTestLinkComponent', () => {
     router = TestBed.inject(Router);
   }));
 
-  // describe('DOM', () => {
-  //   beforeEach(() => {
-  //     spyOn(modalController, 'create').and.returnValue(Promise.resolve({
-  //       present: () => Promise.resolve(),
-  //     } as HTMLIonModalElement));
-  //   });
-  //   describe('opening test termination confirmation modal', () => {
-  //     it('should open a modal for test termination', () => {
-  //       component.openEndTestModal();
-  //       expect(modalController.create).toHaveBeenCalled();
-  //       const { calls } = modalController.create as jasmine.Spy;
-  //       expect(calls.argsFor(0)[0].id).toBe('TerminateTestModal');
-  //     });
-  //     it('should pass the termination and cancellation callbacks to the modal creation', () => {
-  //     component.openEndTestModal();
-  //     expect(modalController.create).toHaveBeenCalled();
-  //     const { calls } = modalController.create as jasmine.Spy;
-  //     const navParams = calls.argsFor(0)[1];
-  //     expect(navParams.onCancel).toBe(component.onCancel);
-  //     expect(navParams.onTerminate).toBe(component.onTerminate);
-  //     });
-  //   });
-  // });
-
   describe('Class', () => {
     describe('onTerminate', () => {
       beforeEach(() => {
-        spyOn(component, 'navigateToOfficePage');
-        spyOn(router, 'navigate');
+        spyOn(component.routerByCategory, 'navigateToPage');
         component.terminateTestModal = {
           dismiss: () => Promise.resolve(true),
         } as HTMLIonModalElement;
         component.isDelegated = false;
+        component.category = TestCategory.BE;
       });
       it('should navigate straight to office when delegated', async () => {
         component.isDelegated = true;
         await component.onTerminate();
-        expect(component.navigateToOfficePage).toHaveBeenCalled();
+        expect(component.routerByCategory.navigateToPage)
+          .toHaveBeenCalledWith(TestFlowPageNames.OFFICE_PAGE, TestCategory.BE);
       });
-      it('should navigate to ADI debrief page', async () => {
-        component.category = TestCategory.ADI2;
+      it('should navigate to debrief page when not delegated', async () => {
         await component.onTerminate();
-        expect(router.navigate).toHaveBeenCalledWith([CAT_ADI_PART2.DEBRIEF_PAGE]);
-      });
-      it('should navigate to B debrief page', async () => {
-        component.category = TestCategory.B;
-        await component.onTerminate();
-        expect(router.navigate).toHaveBeenCalledWith([CAT_B.DEBRIEF_PAGE]);
-      });
-      it('should navigate to BE debrief page', async () => {
-        component.category = TestCategory.BE;
-        await component.onTerminate();
-        expect(router.navigate).toHaveBeenCalledWith([CAT_BE.DEBRIEF_PAGE]);
-      });
-      it('should navigate to C debrief page', async () => {
-        for (const category of [TestCategory.C, TestCategory.C1, TestCategory.CE, TestCategory.C1E]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_C.DEBRIEF_PAGE]);
-        }
-      });
-      it('should navigate to CPC debrief page', async () => {
-        for (const category of [TestCategory.CCPC, TestCategory.DCPC]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_CPC.DEBRIEF_PAGE]);
-        }
-      });
-      it('should navigate to D debrief page', async () => {
-        for (const category of [TestCategory.D, TestCategory.D1, TestCategory.DE, TestCategory.D1E]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_D.DEBRIEF_PAGE]);
-        }
-      });
-      it('should navigate to home test debrief page', async () => {
-        for (const category of [TestCategory.F, TestCategory.G, TestCategory.H, TestCategory.K]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_HOME_TEST.DEBRIEF_PAGE]);
-        }
-      });
-      it('should navigate to MOD1 debrief page', async () => {
-        for (const category of [TestCategory.EUA1M1, TestCategory.EUA2M1, TestCategory.EUAM1, TestCategory.EUAMM1]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_A_MOD1.DEBRIEF_PAGE]);
-        }
-      });
-      it('should navigate to MOD2 debrief page', async () => {
-        for (const category of [TestCategory.EUA1M2, TestCategory.EUA2M2, TestCategory.EUAM2, TestCategory.EUAMM2]) {
-          component.category = category;
-          await component.onTerminate();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_A_MOD2.DEBRIEF_PAGE]);
-        }
-      });
-    });
-
-    describe('navigateToOfficePage', () => {
-      beforeEach(() => {
-        spyOn(router, 'navigate');
-      });
-      it('should navigate to BE office page', async () => {
-        component.category = TestCategory.BE;
-        await component.navigateToOfficePage();
-        expect(router.navigate).toHaveBeenCalledWith([CAT_BE.OFFICE_PAGE]);
-      });
-      it('should navigate to C office page', async () => {
-        for (const category of [TestCategory.C, TestCategory.C1, TestCategory.CE, TestCategory.C1E]) {
-          component.category = category;
-          await component.navigateToOfficePage();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_C.OFFICE_PAGE]);
-        }
-      });
-      it('should navigate to CPC office page', async () => {
-        for (const category of [TestCategory.CCPC, TestCategory.DCPC]) {
-          component.category = category;
-          await component.navigateToOfficePage();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_CPC.OFFICE_PAGE]);
-        }
-      });
-      it('should navigate to D office page', async () => {
-        for (const category of [TestCategory.D, TestCategory.D1, TestCategory.DE, TestCategory.D1E]) {
-          component.category = category;
-          await component.navigateToOfficePage();
-          expect(router.navigate).toHaveBeenCalledWith([CAT_D.OFFICE_PAGE]);
-        }
+        expect(component.routerByCategory.navigateToPage)
+          .toHaveBeenCalledWith(TestFlowPageNames.DEBRIEF_PAGE, TestCategory.BE);
       });
     });
   });
--- src/components/common/end-test-link/end-test-link.ts
+++ src/components/common/end-test-link/end-test-link.ts
@@ -1,19 +1,10 @@
 import { Component, Input } from '@angular/core';
-import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
-import {
-  CAT_A_MOD1,
-  CAT_A_MOD2,
-  CAT_ADI_PART2,
-  CAT_B,
-  CAT_BE,
-  CAT_C,
-  CAT_CPC,
-  CAT_D,
-  CAT_HOME_TEST,
-} from '@pages/page-names.constants';
-import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { TestFlowPageNames, } from '@pages/page-names.constants';
 import { TerminateTestModal } from '@pages/terminate-test-modal/terminate-test-modal';
+import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
 
 @Component({
   selector: 'end-test-link',
@@ -35,19 +26,23 @@ export class EndTestLinkComponent {
 
   constructor(
     public modalController: ModalController,
-    public router: Router) {
+    public router: Router,
+    public routerByCategory: RouteByCategoryProvider,
+    ) {
   }
 
   openEndTestModal = async (): Promise<void> => {
     this.terminateTestModal = await this.modalController.create({
       id: 'TerminateTestModal',
       component: TerminateTestModal,
+      backdropDismiss: false,
+      showBackdrop: true,
       componentProps: {
         onCancel: this.onCancel,
         onTerminate: this.onTerminate,
         shouldAuthenticate: this.shouldAuthenticate,
       },
-      // cssClass: 'mes-modal-alert text-zoom-regular',
+      cssClass: 'mes-modal-alert text-zoom-regular',
     });
     await this.terminateTestModal.present();
   }
@@ -60,82 +55,10 @@ export class EndTestLinkComponent {
     await this.terminateTestModal.dismiss();
 
     if (this.isDelegated) {
-      await this.navigateToOfficePage();
+      await this.routerByCategory.navigateToPage(TestFlowPageNames.OFFICE_PAGE, this.category as TestCategory);
       return;
     }
-    switch (this.category) {
-      case TestCategory.ADI2:
-        await this.router.navigate([CAT_ADI_PART2.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.B:
-        await this.router.navigate([CAT_B.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.BE:
-        await this.router.navigate([CAT_BE.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.C:
-      case TestCategory.C1:
-      case TestCategory.CE:
-      case TestCategory.C1E:
-        await this.router.navigate([CAT_C.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.CCPC:
-      case TestCategory.DCPC:
-        await this.router.navigate([CAT_CPC.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.D:
-      case TestCategory.D1:
-      case TestCategory.DE:
-      case TestCategory.D1E:
-        await this.router.navigate([CAT_D.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.F:
-      case TestCategory.G:
-      case TestCategory.H:
-      case TestCategory.K:
-        await this.router.navigate([CAT_HOME_TEST.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.EUA1M1:
-      case TestCategory.EUA2M1:
-      case TestCategory.EUAM1:
-      case TestCategory.EUAMM1:
-        await this.router.navigate([CAT_A_MOD1.DEBRIEF_PAGE]);
-        break;
-      case TestCategory.EUA1M2:
-      case TestCategory.EUA2M2:
-      case TestCategory.EUAM2:
-      case TestCategory.EUAMM2:
-        await this.router.navigate([CAT_A_MOD2.DEBRIEF_PAGE]);
-        break;
-      default:
-        break;
-    }
-  };
-
-  navigateToOfficePage = async (): Promise<void> => {
-    switch (this.category) {
-      case TestCategory.BE:
-        await this.router.navigate([CAT_BE.OFFICE_PAGE]);
-        break;
-      case TestCategory.C:
-      case TestCategory.C1:
-      case TestCategory.CE:
-      case TestCategory.C1E:
-        await this.router.navigate([CAT_C.OFFICE_PAGE]);
-        break;
-      case TestCategory.CCPC:
-      case TestCategory.DCPC:
-        await this.router.navigate([CAT_CPC.OFFICE_PAGE]);
-        break;
-      case TestCategory.D:
-      case TestCategory.D1:
-      case TestCategory.DE:
-      case TestCategory.D1E:
-        await this.router.navigate([CAT_D.OFFICE_PAGE]);
-        break;
-      default:
-        break;
-    }
+    await this.routerByCategory.navigateToPage(TestFlowPageNames.DEBRIEF_PAGE, this.category as TestCategory);
   };
 
 }
