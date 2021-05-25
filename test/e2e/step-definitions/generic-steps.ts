973d4e22d57a2e0b0c4ebf8e7affa3c4cd5ab3b7
cai <cai.turner@dvsa.gov.uk>
2021-05-25 13:55:52 +0100

    removed unneeded code

--- test/e2e/step-definitions/generic-steps.ts
+++ test/e2e/step-definitions/generic-steps.ts
@@ -21,50 +21,6 @@ const fs = require('fs');
 
 this.testCategory = 'b';
 
-Before({ tags: '@catbe' }, () => {
-  this.testCategory = 'be';
-});
-
-Before({ tags: '@catc' }, () => {
-  this.testCategory = 'c';
-});
-
-Before({ tags: '@catc1' }, () => {
-  this.testCategory = 'c';
-});
-
-Before({ tags: '@catce' }, () => {
-  this.testCategory = 'c';
-});
-
-Before({ tags: '@catc1e' }, () => {
-  this.testCategory = 'ce';
-});
-
-Before({ tags: '@cata' }, () => {
-  this.testCategory = 'a-mod1';
-});
-
-Before({ tags: '@catm2' }, () => {
-  this.testCategory = 'a-mod2';
-});
-
-Before({ tags: '@catd' }, () => {
-  this.testCategory = 'd';
-});
-
-Before({ tags: '@catHome' }, () => {
-  this.testCategory = 'home-test';
-});
-
-Before({ tags: '@catADI2' }, () => {
-  this.testCategory = 'adi-part2';
-});
-
-Before({ tags: '@catcpc' }, () => {
-  this.testCategory = 'cpc';
-});
-
 // We need this much timeout for the login process to complete
 setDefaultTimeout(TEST_CONFIG.DEFAULT_TIMEOUT);
 
@@ -113,21 +69,3 @@ AfterAll(() => {
     });
   });
 });
-
-/// ///////////////////////////////////////// SHARED FUNCTIONS ////////////////////////////////////////////
-
-/**
- * Output the page source to a file - For debug purposes only
- * @param fileName the name of the file to output to
- */
-export const logPageSource = (fileName: string) => {
-  browser.getPageSource().then((pageSource) => {
-    // const fs = require('fs');
-    fs.writeFile(fileName, pageSource, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(`The page source was saved as ${fileName}`);
-    });
-  });
-};
