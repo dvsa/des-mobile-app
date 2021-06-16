837fe5bb0891008c4cc4efef3ff1331e10423f56
mattc <mculliford@aol.com>
2021-06-16 13:28:45 +0100

    port to tr base page

--- src/app/shared/classes/test-flow-base-pages/test-report/test-report-base-page.ts
+++ src/app/shared/classes/test-flow-base-pages/test-report/test-report-base-page.ts
@@ -1,6 +1,6 @@
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { Platform } from '@ionic/angular';
+import { merge, Observable, Subscription } from 'rxjs';
+import { ModalController, Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 import { StoreModel } from '@shared/models/store.model';
@@ -11,22 +11,82 @@ import { getCandidate } from '@store/tests/journal-data/common/candidate/candida
 import { AuthenticationProvider } from '@providers/authentication/authentication';
 
 import { PracticeableBasePageComponent } from '@shared/classes/practiceable-base-page';
+import { getTestReportState } from '@pages/test-report/test-report.reducer';
+import { isRemoveFaultMode, isSeriousMode, isDangerousMode } from '@pages/test-report/test-report.selector';
+import { getTestData } from '@store/tests/test-data/cat-b/test-data.reducer';
+import { CatBUniqueTypes } from '@dvsa/mes-test-schema/categories/B';
+import { hasManoeuvreBeenCompletedCatB } from '@store/tests/test-data/cat-b/test-data.cat-b.selector';
+import { getTestRequirementsCatB } from '@store/tests/test-data/cat-b/test-requirements/test-requirements.reducer';
+import { TestReportValidatorProvider } from '@providers/test-report-validator/test-report-validator';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { map } from 'rxjs/operators';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { legalRequirementsLabels } from '@shared/constants/legal-requirements/legal-requirements.constants';
+import {
+  CalculateTestResult,
+  TerminateTestFromTestReport,
+  TestReportViewDidEnter,
+} from '@pages/test-report/test-report.actions';
+import { Competencies, ExaminerActions, LegalRequirements } from '@store/tests/test-data/test-data.constants';
+import { OverlayCallback } from '@pages/test-report/test-report.model';
+import { ModalEvent } from '@pages/test-report/test-report.constants';
+import { CAT_B } from '@pages/page-names.constants';
+import { EtaInvalidModal } from '@pages/test-report/components/eta-invalid-modal/eta-invalid-modal';
+import { EndTestModal } from '@pages/test-report/components/end-test-modal/end-test-modal';
+import { LegalRequirementsModal } from
+  '@pages/test-report/components/legal-requirements-modal/legal-requirements-modal';
 
 export interface CommonTestReportPageState {
   candidateUntitledName$: Observable<string>;
+  isRemoveFaultMode$: Observable<boolean>;
+  isSeriousMode$: Observable<boolean>;
+  isDangerousMode$: Observable<boolean>;
+  manoeuvres$: Observable<boolean>;
+  testData$: Observable<CatBUniqueTypes.TestData>;
+  testRequirements$: Observable<CatBUniqueTypes.TestRequirements>;
 }
 
 export abstract class TestReportBasePageComponent extends PracticeableBasePageComponent {
 
   commonPageState: CommonTestReportPageState;
+  subscription: Subscription;
+  competencies = Competencies;
+  legalRequirements = LegalRequirements;
+  eta = ExaminerActions;
+  displayOverlay: boolean;
+
+  isRemoveFaultMode: boolean = false;
+  isSeriousMode: boolean = false;
+  isDangerousMode: boolean = false;
+  manoeuvresCompleted: boolean = false;
+  isTestReportValid: boolean = false;
+  isEtaValid: boolean = true;
+
+  missingLegalRequirements: legalRequirementsLabels[] = [];
+  modal: HTMLIonModalElement;
 
   protected constructor(
     platform: Platform,
     authenticationProvider: AuthenticationProvider,
     router: Router,
     store$: Store<StoreModel>,
+    private modalController: ModalController,
+    public testReportValidatorProvider: TestReportValidatorProvider,
+    public screenOrientation: ScreenOrientation,
+    public insomnia: Insomnia,
+    public statusBar: StatusBar,
   ) {
     super(platform, authenticationProvider, router, store$);
+  }
+
+  getCallback(): OverlayCallback {
+    return {
+      callbackMethod: () => {
+        this.toggleReportOverlay();
+      },
+    };
   }
 
   onInitialisation(): void {
@@ -41,7 +101,151 @@ export abstract class TestReportBasePageComponent extends PracticeableBasePageCo
         select(getCandidate),
         select(getUntitledCandidateName),
       ),
+      isRemoveFaultMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isRemoveFaultMode),
+      ),
+      isSeriousMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isSeriousMode),
+      ),
+      isDangerousMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isDangerousMode),
+      ),
+      testData$: currentTest$.pipe(
+        select(getTestData),
+      ),
+      manoeuvres$: currentTest$.pipe(
+        select(getTestData),
+        select(hasManoeuvreBeenCompletedCatB),
+      ),
+      testRequirements$: currentTest$.pipe(
+        select(getTestData),
+        select(getTestRequirementsCatB),
+      ),
     };
   }
+
+  async ionViewWillEnter(): Promise<void> {
+    // ionViewWillEnter lifecylce event used to ensure screen orientation is correct before page transition
+    if (super.isIos() && this.isPracticeMode) {
+      await this.screenOrientation.lock(
+        this.screenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY,
+      );
+      await this.insomnia.keepAwake();
+      this.statusBar.hide();
+    }
+  }
+
+  ionViewDidEnter(): void {
+    // it is possible that we come back to the page from the terminate screen
+    // so need to re-establish the subscription if it doesn't exists or is closed
+    if (!this.subscription || this.subscription.closed) {
+      this.setupSubscription();
+    }
+    this.store$.dispatch(TestReportViewDidEnter());
+  }
+
+  ionViewWillLeave() {
+    if (super.isIos() && this.isPracticeMode) {
+      this.statusBar.show();
+    }
+  }
+
+  toggleReportOverlay(): void {
+    this.displayOverlay = !this.displayOverlay;
+  }
+
+  setupSubscription() {
+    const {
+      candidateUntitledName$,
+      isRemoveFaultMode$,
+      isSeriousMode$,
+      isDangerousMode$,
+      manoeuvres$,
+      testData$,
+    } = this.commonPageState;
+
+    this.subscription = merge(
+      candidateUntitledName$,
+      isRemoveFaultMode$.pipe(map((result) => (this.isRemoveFaultMode = result))),
+      isSeriousMode$.pipe(map((result) => (this.isSeriousMode = result))),
+      isDangerousMode$.pipe(map((result) => (this.isDangerousMode = result))),
+      manoeuvres$.pipe(map((result) => (this.manoeuvresCompleted = result))),
+      testData$.pipe(map((data) => {
+        this.isTestReportValid = this.testReportValidatorProvider.isTestReportValid(data, TestCategory.B);
+        this.missingLegalRequirements = this.testReportValidatorProvider.getMissingLegalRequirements(
+          data,
+          TestCategory.B,
+        );
+        this.isEtaValid = this.testReportValidatorProvider.isETAValid(data, TestCategory.B);
+      })),
+    )
+      .subscribe();
+  }
+
+  cancelSubscription(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  onEndTestClick = async (): Promise<void> => {
+    const modalCssClass: string = 'mes-modal-alert text-zoom-regular';
+    if (!this.isTestReportValid) {
+      this.modal = await this.modalController.create({
+        component: LegalRequirementsModal,
+        componentProps: {
+          legalRequirements: this.missingLegalRequirements,
+        },
+        cssClass: modalCssClass,
+      });
+    } else if (!this.isEtaValid) {
+      this.modal = await this.modalController.create({
+        component: EtaInvalidModal,
+        cssClass: modalCssClass,
+      });
+    } else {
+      this.modal = await this.modalController.create({
+        component: EndTestModal,
+        cssClass: modalCssClass,
+      });
+    }
+
+    await this.modal.present();
+    const { data } = await this.modal.onWillDismiss();
+    if (data) { await this.onModalDismiss(data); }
+  };
+
+  onModalDismiss = async (event: ModalEvent): Promise<void> => {
+    // eslint-disable-next-line default-case
+    switch (event) {
+      case ModalEvent.CONTINUE:
+        this.store$.dispatch(CalculateTestResult());
+        await this.router.navigate([CAT_B.DEBRIEF_PAGE]);
+        break;
+      case ModalEvent.TERMINATE:
+        this.store$.dispatch(TerminateTestFromTestReport());
+        await this.router.navigate([CAT_B.DEBRIEF_PAGE]);
+        break;
+      default:
+        break;
+    }
+  };
+
+  onCancel = async (): Promise<void> => {
+    await this.modal.dismiss();
+  };
+
+  onContinue = async (): Promise<void> => {
+    await this.modal.dismiss();
+    await this.router.navigate([CAT_B.DEBRIEF_PAGE]);
+  };
+
+  onTerminate = async (): Promise<void> => {
+    await this.modal.dismiss();
+    await this.router.navigate([CAT_B.DEBRIEF_PAGE]);
+  };
 
 }
