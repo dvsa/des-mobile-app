5ecf141ea7d15a4b19fbeffb3032c0cf145a2bc3
Andrew Setterfield <andrew.setterfield@dvsa.gov.uk>
2021-06-28 12:29:58 +0100

    implemented post debrief holding screen

--- src/app/pages/post-debrief-holding/__tests__/post-debrief-holding.page.spec.ts
+++ src/app/pages/post-debrief-holding/__tests__/post-debrief-holding.page.spec.ts
@@ -1,31 +1,77 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule, NavController } from '@ionic/angular';
-import { NavMock } from '@mocks/angular-mocks/nav-mock';
+import {
+  async, ComponentFixture, fakeAsync, TestBed, tick,
+} from '@angular/core/testing';
+import { Platform } from '@ionic/angular';
+
+import { PlatformMock } from 'ionic-mocks';
+import { Router } from '@angular/router';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+import { AuthenticationProviderMock } from '@providers/authentication/__mocks__/authentication.mock';
+import { Store, StoreModule } from '@ngrx/store';
+import { StoreModel } from '@shared/models/store.model';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppModule } from '@app/app.module';
+import { MockComponent } from 'ng-mocks';
+import { PracticeModeBanner } from '@components/common/practice-mode-banner/practice-mode-banner';
+import { configureTestSuite } from 'ng-bullet';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
 import { RouteByCategoryProviderMock } from '@providers/route-by-category/__mocks__/route-by-category.mock';
-
+import { TestFlowPageNames } from '@pages/page-names.constants';
 import { PostDebriefHoldingPage } from '../post-debrief-holding.page';
 
 describe('PostDebriefHoldingPage', () => {
-  let component: PostDebriefHoldingPage;
   let fixture: ComponentFixture<PostDebriefHoldingPage>;
+  let component: PostDebriefHoldingPage;
+  let store$: Store<StoreModel>;
+  let routeByCat: RouteByCategoryProvider;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
-      declarations: [PostDebriefHoldingPage],
-      imports: [IonicModule.forRoot()],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        PostDebriefHoldingPage,
+        MockComponent(PracticeModeBanner),
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        AppModule,
+        StoreModule.forFeature('tests', () => ({})),
+      ],
       providers: [
-        { provide: NavController, useClass: NavMock },
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
         { provide: RouteByCategoryProvider, useClass: RouteByCategoryProviderMock },
       ],
-    }).compileComponents();
+    });
+  });
 
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(PostDebriefHoldingPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store$ = TestBed.inject(Store);
+    routeByCat = TestBed.inject(RouteByCategoryProvider);
+    spyOn(store$, 'dispatch');
+    // fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Class', () => {
+    it('should create component', () => {
+      expect(component)
+        .toBeTruthy();
+    });
+
+    describe('continueButton', () => {
+      it('should call navigateToPage function', fakeAsync(async () => {
+        spyOn(routeByCat, 'navigateToPage');
+        await component.continueButton();
+        tick();
+        expect(routeByCat.navigateToPage).toHaveBeenCalledWith(
+          TestFlowPageNames.NON_PASS_FINALISATION_PAGE,
+        );
+      }));
+    });
   });
 });
