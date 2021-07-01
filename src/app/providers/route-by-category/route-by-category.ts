87b56695f30f848c7ed72d7b311d77287c6e5c5c
mattc <mculliford@aol.com>
2021-07-01 14:07:18 +0100

    remove alerts

--- src/app/providers/route-by-category/route-by-category.ts
+++ src/app/providers/route-by-category/route-by-category.ts
@@ -12,8 +12,6 @@ export class RouteByCategoryProvider {
 
   async navigateToPage(page: PageNameKeys, category?: TestCategory): Promise<void> {
     const categoryPage: string = category ? getPageNameByCategoryAndKey(category, page) : page;
-    alert(category);
-    alert(categoryPage);
     await this.router.navigate([categoryPage]);
   }
 
