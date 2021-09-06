52d6720400703d476f7c0506c2666290cc33a82a
Matt Culliford <mculliford@aol.com>
2021-09-06 16:39:59 +0100

    fix effects and actions..migrate spec file

---
+++ src/app/pages/back-to-office/__tests__/back-to-office.analytics.effects.spec.ts
@@ -0,0 +1,199 @@
+import { TestBed } from '@angular/core/testing';
+import { ReplaySubject } from 'rxjs';
+import { StoreModule, Store } from '@ngrx/store';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { AnalyticsProvider } from '@providers/analytics/analytics';
+import { AnalyticsProviderMock } from '@providers/analytics/__mocks__/analytics.mock';
+import {
+  AnalyticsScreenNames,
+  AnalyticsEventCategories,
+  AnalyticsDimensionIndices,
+  AnalyticsEvents,
+} from '@providers/analytics/analytics.model';
+import { AnalyticRecorded } from '@providers/analytics/analytics.actions';
+import { StoreModel } from '@shared/models/store.model';
+import * as testsActions from '@store/tests/tests.actions';
+import { testsReducer } from '@store//tests/tests.reducer';
+import { PopulateCandidateDetails } from '@store/tests/journal-data/common/candidate/candidate.actions';
+import { Application } from '@dvsa/mes-journal-schema';
+import { end2endPracticeSlotId } from '@shared/mocks/test-slot-ids.mock';
+import { ActivityCodes } from '@shared/models/activity-codes';
+import * as applicationReferenceActions
+  from '@store/tests/journal-data/common/application-reference/application-reference.actions';
+import * as activityCodeActions from '@store//tests/activity-code/activity-code.actions';
+import { candidateMock } from '@store/tests/__mocks__/tests.mock';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { configureTestSuite } from 'ng-bullet';
+import { BackToOfficeAnalyticsEffects } from '../back-to-office.analytics.effects';
+import * as fakeJournalActions from '../../fake-journal/fake-journal.actions';
+import * as backToOfficeActions from '../back-to-office.actions';
+
+describe('Back To Office Analytics Effects', () => {
+
+  let effects: BackToOfficeAnalyticsEffects;
+  let analyticsProviderMock;
+  let actions$: any;
+  let store$: Store<StoreModel>;
+  const screenName = AnalyticsScreenNames.BACK_TO_OFFICE;
+  const screenNamePracticeMode = `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsScreenNames.BACK_TO_OFFICE}`;
+  const mockApplication: Application = {
+    applicationId: 123456,
+    bookingSequence: 78,
+    checkDigit: 9,
+  };
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          tests: testsReducer,
+        }),
+      ],
+      providers: [
+        BackToOfficeAnalyticsEffects,
+        { provide: AnalyticsProvider, useClass: AnalyticsProviderMock },
+        provideMockActions(() => actions$),
+        Store,
+      ],
+    });
+  });
+
+  beforeEach(() => {
+    actions$ = new ReplaySubject(1);
+    effects = TestBed.inject(BackToOfficeAnalyticsEffects);
+    analyticsProviderMock = TestBed.inject(AnalyticsProvider);
+    store$ = TestBed.inject(Store);
+  });
+
+  describe('backToOfficeViewDidEnter', () => {
+    it('should call setCurrentPage', (done) => {
+      // ARRANGE
+      store$.dispatch(testsActions.StartTest(123, TestCategory.B));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(backToOfficeActions.BackToOfficeViewDidEnter());
+      // ASSERT
+      effects.backToOfficeViewDidEnter$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.setCurrentPage)
+          .toHaveBeenCalledWith(screenName);
+        done();
+      });
+    });
+    it('should call setCurrentPage with practice mode prefix', (done) => {
+      // ARRANGE
+      store$.dispatch(fakeJournalActions.StartE2EPracticeTest(end2endPracticeSlotId));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(backToOfficeActions.BackToOfficeViewDidEnter());
+      // ASSERT
+      effects.backToOfficeViewDidEnter$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.setCurrentPage)
+          .toHaveBeenCalledWith(screenNamePracticeMode);
+        done();
+      });
+    });
+
+  });
+
+  describe('deferWriteUpEffect', () => {
+    it('should call logEvent with pass page and addCustomDimension', (done) => {
+      // ARRANGE
+      store$.dispatch(testsActions.StartTest(123, TestCategory.B));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      store$.dispatch(activityCodeActions.SetActivityCode(ActivityCodes.PASS));
+      store$.dispatch(applicationReferenceActions.PopulateApplicationReference(mockApplication));
+      // ACT
+      actions$.next(backToOfficeActions.DeferWriteUp());
+      // ASSERT
+      effects.deferWriteUpEffect$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.CANDIDATE_ID, '1');
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.APPLICATION_REFERENCE, '123456789');
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.BACK_TO_OFFICE,
+            AnalyticsEvents.DEFER_WRITE_UP,
+            'pass',
+          );
+        done();
+      });
+    });
+    it('should call logEvent with fail page and addCustomDimension', (done) => {
+      // ARRANGE
+      store$.dispatch(testsActions.StartTest(123, TestCategory.B));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      store$.dispatch(activityCodeActions.SetActivityCode(ActivityCodes.FAIL));
+      store$.dispatch(applicationReferenceActions.PopulateApplicationReference(mockApplication));
+      // ACT
+      actions$.next(backToOfficeActions.DeferWriteUp());
+      // ASSERT
+      effects.deferWriteUpEffect$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.CANDIDATE_ID, '1');
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.APPLICATION_REFERENCE, '123456789');
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.BACK_TO_OFFICE,
+            AnalyticsEvents.DEFER_WRITE_UP,
+            'fail',
+          );
+        done();
+      });
+    });
+    it('should call logEvent with pass page, practice mode prefix and addCustomDimension', (done) => {
+      // ARRANGE
+      store$.dispatch(fakeJournalActions.StartE2EPracticeTest(end2endPracticeSlotId));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      store$.dispatch(activityCodeActions.SetActivityCode(ActivityCodes.PASS));
+      store$.dispatch(applicationReferenceActions.PopulateApplicationReference(mockApplication));
+      // ACT
+      actions$.next(backToOfficeActions.DeferWriteUp());
+      // ASSERT
+      effects.deferWriteUpEffect$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.CANDIDATE_ID, '1');
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.APPLICATION_REFERENCE, '123456789');
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsEventCategories.BACK_TO_OFFICE}`,
+            `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsEvents.DEFER_WRITE_UP}`,
+            'pass',
+          );
+        done();
+      });
+    });
+    it('should call logEvent with fail page, practice mode prefix and addCustomDimension', (done) => {
+      // ARRANGE
+      store$.dispatch(fakeJournalActions.StartE2EPracticeTest(end2endPracticeSlotId));
+      store$.dispatch(PopulateCandidateDetails(candidateMock));
+      store$.dispatch(activityCodeActions.SetActivityCode(ActivityCodes.FAIL));
+      store$.dispatch(applicationReferenceActions.PopulateApplicationReference(mockApplication));
+      // ACT
+      actions$.next(backToOfficeActions.DeferWriteUp());
+      // ASSERT
+      effects.deferWriteUpEffect$.subscribe((result) => {
+        expect(result.type).toEqual(AnalyticRecorded.type);
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.CANDIDATE_ID, '1');
+        expect(analyticsProviderMock.addCustomDimension)
+          .toHaveBeenCalledWith(AnalyticsDimensionIndices.APPLICATION_REFERENCE, '123456789');
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsEventCategories.BACK_TO_OFFICE}`,
+            `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsEvents.DEFER_WRITE_UP}`,
+            'fail',
+          );
+        done();
+      });
+    });
+  });
+
+});
--- src/app/pages/back-to-office/back-to-office.analytics.effects.ts
+++ src/app/pages/back-to-office/back-to-office.analytics.effects.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import {
-  Actions, createEffect, ofType,
-} from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { switchMap, withLatestFrom, concatMap } from 'rxjs/operators';
+import { AnalyticsProvider } from '@providers/analytics/analytics';
+import {
+  AnalyticsScreenNames, AnalyticsDimensionIndices, AnalyticsEventCategories, AnalyticsEvents,
+} from '@providers/analytics/analytics.model';
 import { Store, select } from '@ngrx/store';
+import { StoreModel } from '@shared/models/store.model';
 import { getCurrentTest, isPassed, getJournalData } from '@store/tests/tests.selector';
 import { getTests } from '@store/tests/tests.reducer';
+import { formatAnalyticsText } from '@shared/helpers/format-analytics-text';
 import { TestsModel } from '@store/tests/tests.model';
+import { AnalyticRecorded } from '@providers/analytics/analytics.actions';
 import { getCandidate } from '@store/tests/journal-data/common/candidate/candidate.reducer';
 import { getCandidateId } from '@store/tests/journal-data/common/candidate/candidate.selector';
 import {
@@ -16,13 +21,6 @@ import {
 import {
   getApplicationNumber,
 } from '@store/tests/journal-data/common/application-reference/application-reference.selector';
-import { AnalyticRecorded } from '@providers/analytics/analytics.actions';
-import { formatAnalyticsText } from '@shared/helpers/format-analytics-text';
-import { StoreModel } from '@shared/models/store.model';
-import {
-  AnalyticsScreenNames, AnalyticsDimensionIndices, AnalyticsEventCategories, AnalyticsEvents,
-} from '@providers/analytics/analytics.model';
-import { AnalyticsProvider } from '@providers/analytics/analytics';
 import {
   BackToOfficeViewDidEnter,
   DeferWriteUp,
@@ -46,56 +44,52 @@ export class BackToOfficeAnalyticsEffects {
         ),
       ),
     )),
-    switchMap(([, tests]: [ReturnType <typeof BackToOfficeViewDidEnter>, TestsModel]) => {
+    switchMap(([, tests]: [ReturnType<typeof BackToOfficeViewDidEnter>, TestsModel]) => {
       const screenName = formatAnalyticsText(AnalyticsScreenNames.BACK_TO_OFFICE, tests);
       this.analytics.setCurrentPage(screenName);
       return of(AnalyticRecorded());
     }),
   ));
 
-  deferWriteUpEffect$= createEffect(() => {
-    return this.actions$.pipe(
-      ofType(DeferWriteUp),
-      concatMap((action) => of(action)
-        .pipe(
-          withLatestFrom(
-            this.store$.pipe(
-              select(getTests),
-            ),
-            this.store$.pipe(
-              select(getTests),
-              select(getCurrentTest),
-              select(isPassed),
-            ),
-            this.store$.pipe(
-              select(getTests),
-              select(getCurrentTest),
-              select(getJournalData),
-              select(getCandidate),
-              select(getCandidateId),
-            ),
-            this.store$.pipe(
-              select(getTests),
-              select(getCurrentTest),
-              select(getJournalData),
-              select(getApplicationReference),
-              select(getApplicationNumber),
-            ),
-          ),
-        )),
+  deferWriteUpEffect$ = createEffect(() => this.actions$.pipe(
+    ofType(DeferWriteUp),
+    concatMap((action) => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(isPassed),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(getJournalData),
+          select(getCandidate),
+          select(getCandidateId),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(getJournalData),
+          select(getApplicationReference),
+          select(getApplicationNumber),
+        ),
+      ),
+    )),
+    switchMap(([, tests, isTestPassed, candidateId, applicationReference]:
+    [ReturnType<typeof DeferWriteUp>, TestsModel, boolean, number, string]) => {
+      this.analytics.addCustomDimension(AnalyticsDimensionIndices.CANDIDATE_ID, `${candidateId}`);
+      this.analytics.addCustomDimension(AnalyticsDimensionIndices.APPLICATION_REFERENCE, applicationReference);
 
-      switchMap(([, tests, candidateId, applicationReference]:
-      [ReturnType<typeof DeferWriteUp>, TestsModel, boolean, number, string]) => {
-        this.analytics.addCustomDimension(AnalyticsDimensionIndices.CANDIDATE_ID, `${candidateId}`);
-        this.analytics.addCustomDimension(AnalyticsDimensionIndices.APPLICATION_REFERENCE, applicationReference);
-
-        this.analytics.logEvent(
-          formatAnalyticsText(AnalyticsEventCategories.BACK_TO_OFFICE, tests),
-          formatAnalyticsText(AnalyticsEvents.DEFER_WRITE_UP, tests),
-          isPassed ? 'pass' : 'fail',
-        );
-        return of(AnalyticRecorded());
-      }),
-    );
-  });
+      this.analytics.logEvent(
+        formatAnalyticsText(AnalyticsEventCategories.BACK_TO_OFFICE, tests),
+        formatAnalyticsText(AnalyticsEvents.DEFER_WRITE_UP, tests),
+        isTestPassed ? 'pass' : 'fail',
+      );
+      return of(AnalyticRecorded());
+    }),
+  ));
 }
