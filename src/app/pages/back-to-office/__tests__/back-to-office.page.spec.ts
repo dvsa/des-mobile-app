959f33bbe4b862fe7ca87ebc2b0d068b0d6627da
Matt Culliford <mculliford@aol.com>
2021-09-09 12:00:55 +0100

    fix issues with back-to-office page

---
+++ src/app/pages/back-to-office/__tests__/back-to-office.page.spec.ts
@@ -0,0 +1,170 @@
+import { ComponentFixture, waitForAsync, TestBed } from '@angular/core/testing';
+import {
+  IonicModule, NavParams, Config, Platform,
+} from '@ionic/angular';
+import {
+  NavParamsMock, ConfigMock, PlatformMock,
+} from 'ionic-mocks';
+
+import { AppModule } from 'src/app/app.module';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+import { AuthenticationProviderMock } from '@providers/authentication/__mocks__/authentication.mock';
+import { DateTimeProvider } from '@providers/date-time/date-time';
+import { DateTimeProviderMock } from '@providers/date-time/__mocks__/date-time.mock';
+import { StoreModule, Store } from '@ngrx/store';
+import { StoreModel } from '@shared/models/store.model';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+import { DeviceProvider } from '@providers/device/device';
+import { DeviceProviderMock } from '@providers/device/__mocks__/device.mock';
+import { InsomniaMock } from '@shared/mocks/insomnia.mock';
+import { ScreenOrientationMock } from '@shared/mocks/screen-orientation.mock';
+import { MockComponent } from 'ng-mocks';
+import { PracticeModeBanner } from '@components/common/practice-mode-banner/practice-mode-banner';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { configureTestSuite } from 'ng-bullet';
+import { Router } from '@angular/router';
+import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
+import { RouteByCategoryProviderMock } from '@providers/route-by-category/__mocks__/route-by-category.mock';
+import { BackToOfficePage } from '../back-to-office.page';
+
+describe('BackToOfficePage', () => {
+  let fixture: ComponentFixture<BackToOfficePage>;
+  let component: BackToOfficePage;
+  let store$: Store<StoreModel>;
+  let screenOrientation: ScreenOrientation;
+  let insomnia: Insomnia;
+  let deviceProvider: DeviceProvider;
+  let router: Router;
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        BackToOfficePage,
+        MockComponent(PracticeModeBanner),
+      ],
+      imports: [
+        IonicModule,
+        AppModule,
+        StoreModule.forRoot({
+          tests: () => ({
+            currentTest: {
+              slotId: '123',
+            },
+            testStatus: {},
+            startedTests: {
+              123: {
+                vehicleDetails: {},
+                accompaniment: {},
+                testData: {
+                  dangerousFaults: {},
+                  drivingFaults: {},
+                  manoeuvres: {},
+                  seriousFaults: {},
+                  testRequirements: {},
+                  ETA: {},
+                  eco: {},
+                  vehicleChecks: {
+                    showMeQuestion: {
+                      code: 'S3',
+                      description: '',
+                      outcome: '',
+                    },
+                    tellMeQuestion: {
+                      code: '',
+                      description: '',
+                      outcome: '',
+                    },
+                  },
+                  eyesightTest: {},
+                },
+                activityCode: '28',
+                journalData: {
+                  candidate: {
+                    candidateName: 'Joe Bloggs',
+                    driverNumber: '123',
+                  },
+                },
+                rekey: false,
+              },
+            },
+          }),
+        }),
+      ],
+      providers: [
+        { provide: NavParams, useFactory: () => NavParamsMock.instance() },
+        { provide: Config, useFactory: () => ConfigMock.instance() },
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+        { provide: DateTimeProvider, useClass: DateTimeProviderMock },
+        { provide: ScreenOrientation, useClass: ScreenOrientationMock },
+        { provide: Insomnia, useClass: InsomniaMock },
+        { provide: DeviceProvider, useClass: DeviceProviderMock },
+        { provide: RouteByCategoryProvider, useClass: RouteByCategoryProviderMock },
+      ],
+    });
+  });
+
+  beforeEach(waitForAsync(() => {
+    fixture = TestBed.createComponent(BackToOfficePage);
+    component = fixture.componentInstance;
+    screenOrientation = TestBed.inject(ScreenOrientation);
+    insomnia = TestBed.inject(Insomnia);
+    deviceProvider = TestBed.inject(DeviceProvider);
+    store$ = TestBed.inject(Store);
+    router = TestBed.inject(Router);
+    spyOn(store$, 'dispatch');
+    spyOn(router, 'navigate');
+    spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+  }));
+
+  describe('Class', () => {
+    describe('ionViewDidEnter', () => {
+      it('should disable test inhibitions when not in practice mode', async (done) => {
+        component.isPracticeMode = true;
+        await component.ionViewDidEnter();
+        expect(deviceProvider.disableSingleAppMode).not.toHaveBeenCalled();
+        expect(screenOrientation.unlock).toHaveBeenCalled();
+        expect(insomnia.allowSleepAgain).toHaveBeenCalled();
+        done();
+      });
+      it('should disable test inhibitions when not in practice mode', async (done) => {
+        component.isPracticeMode = false;
+        await component.ionViewDidEnter();
+        expect(deviceProvider.disableSingleAppMode).toHaveBeenCalled();
+        expect(screenOrientation.unlock).toHaveBeenCalled();
+        expect(insomnia.allowSleepAgain).toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+
+  describe('goToJournal', () => {
+    it('should call the popTo method in the navcontroller if not in practice mode', () => {
+      component.goToJournal();
+      expect(router.navigate).toHaveBeenCalled();
+    });
+    // @TODO enable once practice mode added (MES-6867)
+    xit('should call the popTo method in the navcontroller if in practice mode', async (done) => {
+      component.isPracticeMode = true;
+      spyOn(component.routeByCategoryProvider, 'navigateToPage');
+      await component.goToJournal();
+      expect(component.routeByCategoryProvider.navigateToPage).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  describe('DOM', () => {
+    it('should show the return to journal button when not a rekey', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.bottom-button'))).toBeDefined();
+    });
+    it('should hide the return to journal button when this is a rekey', () => {
+      fixture.detectChanges();
+      component.pageState.isRekey$ = of(true);
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('.bottom-button'))).toBeNull();
+    });
+  });
+});
--- src/app/pages/back-to-office/back-to-office.page.ts
+++ src/app/pages/back-to-office/back-to-office.page.ts
@@ -1,36 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, Platform } from '@ionic/angular';
-import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
-import { CAT_B, JOURNAL_PAGE } from '@pages/page-names.constants';
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { PracticeableBasePageComponent } from '@shared/classes/practiceable-base-page';
 import { AuthenticationProvider } from '@providers/authentication/authentication';
-import { Router } from '@angular/router';
 import { Store, select } from '@ngrx/store';
 import { StoreModel } from '@shared/models/store.model';
-import { Observable } from 'rxjs';
+import { BackToOfficeViewDidEnter, DeferWriteUp } from '@pages/back-to-office/back-to-office.actions';
+import { DeviceProvider } from '@providers/device/device';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
+import { merge, Observable, Subscription } from 'rxjs';
 import { getTests } from '@store/tests/tests.reducer';
 import { getCurrentTest } from '@store/tests/tests.selector';
 import { getRekeyIndicator } from '@store/tests/rekey/rekey.reducer';
 import { isRekey } from '@store/tests/rekey/rekey.selector';
+import { Router } from '@angular/router';
+import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
+import { JOURNAL_PAGE, TestFlowPageNames } from '@pages/page-names.constants';
+import { CategoryCode } from '@dvsa/mes-test-schema/categories/common';
+import { getTestCategory } from '@store/tests/category/category.reducer';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { map } from 'rxjs/operators';
 
 interface BackToOfficePageState {
   isRekey$: Observable<boolean>;
+  testCategory$: Observable<CategoryCode>;
 }
 
 @Component({
-  selector: 'app-back-to-office',
-  templateUrl: './back-to-office.page.html',
-  styleUrls: ['./back-to-office.page.scss'],
+  selector: '.back-to-office-page',
+  templateUrl: 'back-to-office.page.html',
+  styleUrls: ['back-to-office.page.scss'],
 })
-export class BackToOfficePage extends PracticeableBasePageComponent implements OnInit {
+export class BackToOfficePage extends PracticeableBasePageComponent {
   pageState: BackToOfficePageState;
+  testCategory: TestCategory;
+  isRekey: boolean;
+  merged$: Observable<string | boolean>;
+  subscription: Subscription;
+
   constructor(
-    platform: Platform,
-    authenticationProvider: AuthenticationProvider,
-    router: Router,
     store$: Store<StoreModel>,
-    public navController: NavController,
-    public routeByCat: RouteByCategoryProvider,
+    private deviceProvider: DeviceProvider,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+    public screenOrientation: ScreenOrientation,
+    public insomnia: Insomnia,
+    public router: Router,
+    public routeByCategoryProvider: RouteByCategoryProvider,
   ) {
     super(platform, authenticationProvider, router, store$);
   }
@@ -44,13 +60,57 @@ export class BackToOfficePage extends PracticeableBasePageComponent implements O
         select(getRekeyIndicator),
         select(isRekey),
       ),
+      testCategory$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getTestCategory),
+      ),
     };
+
+    const {
+      testCategory$,
+      isRekey$,
+    } = this.pageState;
+
+    this.merged$ = merge(
+      testCategory$.pipe(map((value) => this.testCategory = (value as TestCategory))),
+      isRekey$.pipe(map((value) => this.isRekey = value)),
+    );
+
+    this.subscription = this.merged$.subscribe();
   }
 
-  async navigateOffice(): Promise<void> {
-    await this.router.navigate([CAT_B.OFFICE_PAGE]);
+  async ionViewDidEnter(): Promise<void> {
+    if (super.isIos()) {
+      this.screenOrientation.unlock();
+      await this.insomnia.allowSleepAgain();
+
+      if (!this.isPracticeMode) {
+        await this.deviceProvider.disableSingleAppMode();
+      }
+    }
+
+    this.store$.dispatch(BackToOfficeViewDidEnter());
   }
-  async navigateJournal(): Promise<void> {
-    await this.router.navigate([JOURNAL_PAGE]);
+
+  ionViewDidLeave() {
+    super.ionViewDidLeave();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  async goToJournal(): Promise<void> {
+    if (this.isPracticeMode) {
+      this.exitPracticeMode();
+      return;
+    }
+    this.store$.dispatch(DeferWriteUp());
+    await this.router.navigate([JOURNAL_PAGE], { replaceUrl: true });
+  }
+
+  async goToOfficePage() {
+    await this.routeByCategoryProvider.navigateToPage(TestFlowPageNames.OFFICE_PAGE,
+      this.testCategory);
   }
 }
