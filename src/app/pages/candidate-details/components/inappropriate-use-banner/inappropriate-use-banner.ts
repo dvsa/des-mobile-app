2a7e9c96622dd44d637e4ee7e7e1758dc7303c2c
gittins7 <45790769+gittins7@users.noreply.github.com>
2021-10-07 12:31:50 +0100

    MES-7268 inappropriate use banner (#306)
    * Created and implemented component for inappropriate use banner to be used on candidate details

---
+++ src/app/pages/candidate-details/components/inappropriate-use-banner/inappropriate-use-banner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inappropriate-use-banner',
+  templateUrl: 'inappropriate-use-banner.html',
+  styleUrls: ['inappropriate-use-banner.scss'],
+})
+export class InappropriateUseBannerComponent {
+}
