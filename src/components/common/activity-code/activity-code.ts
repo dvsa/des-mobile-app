12cd6680a886cf3c795d6cf81a39c44f50e11db0
Matthew Bell <mbell.dvsa@icloud.com>
2021-12-07 16:30:12 +0000

    tidy PR

--- src/components/common/activity-code/activity-code.ts
+++ src/components/common/activity-code/activity-code.ts
@@ -1,11 +1,9 @@
 import {
-  ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output,
+  ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output,
 } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivityCode } from '@dvsa/mes-test-schema/categories/common';
 import { ActivityCodeModel } from '@shared/constants/activity-code/activity-code.constants';
-import { AlertController, ModalController } from '@ionic/angular';
-import { get } from 'lodash';
+import { ModalController } from '@ionic/angular';
 import { ModalActivityCodeListComponent } from '@components/common/modal-activity-code-list/modal-activity-code-list';
 import { ActivityCodeModalEvent } from '@components/common/activity-code/acitivity-code-modal-event';
 
@@ -14,7 +12,7 @@ import { ActivityCodeModalEvent } from '@components/common/activity-code/acitivi
   templateUrl: 'activity-code.html',
   styleUrls: ['activity-code.scss'],
 })
-export class ActivityCodeComponent implements OnChanges, OnInit {
+export class ActivityCodeComponent implements OnChanges {
 
   @Input()
   activityCodeModel: ActivityCodeModel;
@@ -33,18 +31,12 @@ export class ActivityCodeComponent implements OnChanges, OnInit {
 
   private formControl: FormControl;
   static readonly fieldName: string = 'activityCode';
-  private static readonly ionSelectOptionClass: string = 'alert-radio-label sc-ion-alert-ios';
   activityCodeListModal: HTMLIonModalElement;
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
-    private alertController: AlertController,
     public modalController: ModalController,
   ) {
-  }
-
-  ngOnInit(): void {
-    this.initialiseSelectClickEvent();
   }
 
   ngAfterViewChecked() {
@@ -71,49 +63,11 @@ export class ActivityCodeComponent implements OnChanges, OnInit {
     return this.disabled || (this.activityCodeModel && parseInt(this.activityCodeModel.activityCode, 10) < 4);
   }
 
-  isOptionDisabled(activityCode: ActivityCode): boolean {
-    if (parseInt(activityCode, 10) < 4) {
-      return true;
-    }
-    return false;
-  }
-
-  initialiseSelectClickEvent(): void {
-    // create dom click event
-    document.addEventListener('click', async (event) => {
-      // check if the element you are clicking has the same class as the ion-select-option
-      const isSelectOptionClick: boolean = event['path'][0].className === ActivityCodeComponent.ionSelectOptionClass;
-
-      if (isSelectOptionClick) {
-        // obtain text from element
-        const activityCodeValue: string = get(event, 'srcElement.innerText', null);
-
-        if (typeof activityCodeValue !== 'string') return;
-
-        this.emitSelection(activityCodeValue);
-
-        await this.dismissSelect();
-      }
-    });
-  }
-
-  private emitSelection = (activityCodeValue: string): void => {
-    // split string at every space which should be in format 'activityCode - description'
-    const [activityCode, , description] = activityCodeValue.split(' ');
-
-    // dispatch change event with new model
-    this.activityCodeChanged({ activityCode, description } as ActivityCodeModel);
-  };
-
-  private dismissSelect = async (): Promise<void> => {
-  // get a handle on the presented ion-select
-    const alert: HTMLIonAlertElement = await this.alertController.getTop();
-
-    // dismiss if present
-    if (alert) await alert.dismiss();
-  };
-
   openActivityCodeListModal = async (): Promise<void> => {
+    if (this.isSelectDisabled()) {
+      return;
+    }
+
     const activityCodeModal: HTMLIonModalElement = await this.modalController.create({
       id: 'ActivityCodeListModal',
       cssClass: 'activity-code-modal text-zoom-regular',
