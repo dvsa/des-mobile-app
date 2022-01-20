bbf17933e290787b7e3cd7c2392beb2b56cf79db
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2022-01-20 08:51:12 +0000

    Add behaviou map; Hide VRN button; (#435)

--- src/app/shared/helpers/behaviour-map-by-category.ts
+++ src/app/shared/helpers/behaviour-map-by-category.ts
@@ -2,6 +2,7 @@ import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/
 import { OutcomeBehaviourMapping } from '@providers/outcome-behaviour-map/outcome-behaviour-map.model';
 import { behaviourMap as CatBBehaviourMap } from '@pages/office/office-behaviour-map';
 import { behaviourMap as CatBEBehaviourMap } from '@pages/office/office-behaviour-map.cat-be';
+import { behaviourMap as CatCBehaviourMap } from '@pages/office/office-behaviour-map.cat-c';
 
 export const getBehaviourMapByCategory = (testCategory: TestCategory): OutcomeBehaviourMapping => {
   switch (testCategory) {
@@ -9,6 +10,11 @@ export const getBehaviourMapByCategory = (testCategory: TestCategory): OutcomeBe
       return CatBBehaviourMap;
     case TestCategory.BE:
       return CatBEBehaviourMap;
+    case TestCategory.C:
+    case TestCategory.C1:
+    case TestCategory.CE:
+    case TestCategory.C1E:
+      return CatCBehaviourMap;
     default:
       console.error(`Missing behaviour map for category: ${testCategory}`);
       return null;
