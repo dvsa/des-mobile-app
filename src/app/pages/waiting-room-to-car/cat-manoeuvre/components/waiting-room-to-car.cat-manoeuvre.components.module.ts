338e3e751835e98834734a09e7c3db7115da0e75
Matthew Bell <mbell.dvsa@icloud.com>
2022-02-09 21:14:58 +0000

    formatting correction

--- src/app/pages/waiting-room-to-car/cat-manoeuvre/components/waiting-room-to-car.cat-manoeuvre.components.module.ts
+++ src/app/pages/waiting-room-to-car/cat-manoeuvre/components/waiting-room-to-car.cat-manoeuvre.components.module.ts
@@ -4,19 +4,19 @@ import { ComponentsModule } from '@components/common/common-components.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DimensionsComponent } from '@pages/waiting-room-to-car/cat-manoeuvre/components/dimensions/dimensions';
-import {PipesModule} from '@shared/pipes/pipes.module';
+import { PipesModule } from '@shared/pipes/pipes.module';
 
 @NgModule({
   declarations: [
     DimensionsComponent,
   ],
-    imports: [
-        IonicModule,
-        ComponentsModule,
-        ReactiveFormsModule,
-        CommonModule,
-        PipesModule,
-    ],
+  imports: [
+    IonicModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    PipesModule,
+  ],
   exports: [
     DimensionsComponent,
   ],
