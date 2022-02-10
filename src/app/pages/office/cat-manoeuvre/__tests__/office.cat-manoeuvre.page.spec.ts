0d52022c0547e0db6737ff6367024235c2633dfa
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2022-02-10 14:02:35 +0000

    implement provisional office page (#479)

--- src/app/pages/office/cat-manoeuvre/__tests__/office.cat-manoeuvre.page.spec.ts
+++ src/app/pages/office/cat-manoeuvre/__tests__/office.cat-manoeuvre.page.spec.ts
@@ -1,34 +1,202 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule, NavController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { NavMock } from '@mocks/angular-mocks/nav-mock';
-import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
-import { RouteByCategoryProviderMock } from '@providers/route-by-category/__mocks__/route-by-category.mock';
-
-import { OfficeCatManoeuvrePage } from '../office.cat-manoeuvre.page';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {
+  IonicModule,
+  NavController,
+  Platform,
+  ToastController, ModalController,
+} from '@ionic/angular';
+import {
+  PlatformMock,
+} from 'ionic-mocks';
+import { NavControllerMock } from '@shared/mocks/nav-controller.mock';
+import { Store, StoreModule } from '@ngrx/store';
+import { MockComponent } from 'ng-mocks';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { configureTestSuite } from 'ng-bullet';
+import { ComponentsModule } from '@components/common/common-components.module';
+import { AppModule } from 'src/app/app.module';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+import { AuthenticationProviderMock } from '@providers/authentication/__mocks__/authentication.mock';
+import { StoreModel } from '@shared/models/store.model';
+import { FaultCountProvider } from '@providers/fault-count/fault-count';
+import { FaultSummaryProvider } from '@providers/fault-summary/fault-summary';
+import { WeatherConditionProvider } from '@providers/weather-conditions/weather-condition';
+import { QuestionProvider } from '@providers/question/question';
+import { QuestionProviderMock } from '@providers/question/__mocks__/question.mock';
+import { OutcomeBehaviourMapProvider } from '@providers/outcome-behaviour-map/outcome-behaviour-map';
+import { OutcomeBehaviourMapProviderMock } from '@providers/outcome-behaviour-map/__mocks__/outcome-behaviour-map.mock';
+import { ModalControllerMock } from '@mocks/ionic-mocks/modal-controller.mock';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivityCodeModel, ActivityCodeDescription } from '@shared/constants/activity-code/activity-code.constants';
+import { ActivityCodes } from '@shared/models/activity-codes';
+import { ActivityCodeComponent } from '@components/common/activity-code/activity-code';
+import { By } from '@angular/platform-browser';
+import { Competencies, ExaminerActions } from '@store/tests/test-data/test-data.constants';
+import { ToggleETA } from '@store/tests/test-data/common/eta/eta.actions';
+import { TogglePlanningEco } from '@store/tests/test-data/common/eco/eco.actions';
+import { AddDangerousFault } from '@store/tests/test-data/common/dangerous-faults/dangerous-faults.actions';
+import { AddSeriousFault } from '@store/tests/test-data/common/serious-faults/serious-faults.actions';
+import { ToastControllerMock } from '@shared/mocks/toast-controller.mock';
+import { TrueLikenessComponent } from '@pages/office/components/true-likeness/true-likeness';
+import { OfficeCatManoeuvrePage } from '@pages/office/cat-manoeuvre/office.cat-manoeuvre.page';
+import { DateOfTest } from '../../components/date-of-test/date-of-test';
+import { CandidateSectionComponent } from '../../components/candidate-section/candidate-section';
+import { FaultCommentCardComponent } from '../../components/fault-comment-card/fault-comment-card';
+import { IndependentDrivingComponent } from '../../components/independent-driving/independent-driving';
+import { IdentificationComponent } from '../../components/identification/identification';
+import { AdditionalInformationComponent } from '../../components/additional-information/additional-information';
+import { WeatherConditionsComponent } from '../../components/weather-conditions/weather-conditions';
+import { CandidateDescriptionComponent } from '../../components/candidate-description/candidate-description';
+import { RouteNumberComponent } from '../../components/route-number/route-number';
 
 describe('OfficeCatManoeuvrePage', () => {
-  let component: OfficeCatManoeuvrePage;
   let fixture: ComponentFixture<OfficeCatManoeuvrePage>;
-  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
+  let component: OfficeCatManoeuvrePage;
+  let store$: Store<StoreModel>;
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        OfficeCatManoeuvrePage,
+        MockComponent(RouteNumberComponent),
+        MockComponent(CandidateDescriptionComponent),
+        MockComponent(IdentificationComponent),
+        MockComponent(WeatherConditionsComponent),
+        MockComponent(AdditionalInformationComponent),
+        MockComponent(IndependentDrivingComponent),
+        MockComponent(FaultCommentCardComponent),
+        MockComponent(CandidateSectionComponent),
+        MockComponent(DateOfTest),
+        MockComponent(TrueLikenessComponent),
+      ],
+      imports: [
+        IonicModule,
+        AppModule,
+        ComponentsModule,
+        StoreModule.forRoot({
+          tests: () => ({
+            currentTest: {
+              slotId: '123',
+            },
+            testStatus: {},
+            startedTests: {
+              123: {
+                category: TestCategory.CM,
+                vehicleDetails: {},
+                accompaniment: {},
+                testData: {
+                  dangerousFaults: {},
+                  drivingFaults: {},
+                  manoeuvres: {},
+                  seriousFaults: {},
+                  testRequirements: {},
+                  ETA: {},
+                  eco: {},
+                },
+                activityCode: '28',
+                journalData: {
+                  candidate: {
+                    candidateName: 'Joe Bloggs',
+                    driverNumber: '123',
+                  },
+                },
+                rekey: false,
+              },
+            },
+          }),
+        }),
+        ReactiveFormsModule,
+      ],
+      providers: [
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+        { provide: NavController, useClass: NavControllerMock },
+        { provide: ToastController, useClass: ToastControllerMock },
+        { provide: ModalController, useClass: ModalControllerMock },
+        { provide: OutcomeBehaviourMapProvider, useClass: OutcomeBehaviourMapProviderMock },
+        WeatherConditionProvider,
+        { provide: QuestionProvider, useClass: QuestionProviderMock },
+        { provide: FaultSummaryProvider, useClass: FaultSummaryProvider },
+        { provide: FaultCountProvider, useClass: FaultCountProvider },
+      ],
+    });
+  });
 
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [OfficeCatManoeuvrePage],
-      imports: [IonicModule.forRoot()],
-      providers: [
-        { provide: NavController, useClass: NavMock },
-        { provide: RouteByCategoryProvider, useClass: RouteByCategoryProviderMock },
-        { provide: Router, useValue: routerSpy },
-      ],
-    }).compileComponents();
-
     fixture = TestBed.createComponent(OfficeCatManoeuvrePage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store$ = TestBed.inject(Store);
+    spyOn(store$, 'dispatch');
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('DOM', () => {
+    it('should pass the selected activity code to the activity code subcomponent', () => {
+      const activityCodeModel: ActivityCodeModel = {
+        activityCode: ActivityCodes.ACCIDENT,
+        description: ActivityCodeDescription.ACCIDENT,
+      };
+      fixture.detectChanges();
+      const activityCodeElement = fixture.debugElement.query(By.css('activity-code'))
+        .componentInstance as ActivityCodeComponent;
+      expect(activityCodeElement.activityCodeModel).toEqual(activityCodeModel);
+    });
+    it('should hide ETA faults container if there are none', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#ETA'))).toBeNull();
+    });
+    it('should display ETA faults container if there are any', () => {
+      store$.dispatch(ToggleETA(ExaminerActions.verbal));
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#ETA'))).toBeDefined();
+    });
+    it('should hide eco faults container if there are none', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#eco'))).toBeNull();
+    });
+    it('should display eco faults container if there are any', () => {
+      store$.dispatch(TogglePlanningEco());
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#eco'))).toBeDefined();
+    });
+    it('should display eta fault details if there are any', () => {
+      store$.dispatch(ToggleETA(ExaminerActions.verbal));
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#etaFaults'))).toBeDefined();
+    });
+    it('should display eco fault details if there are any', () => {
+      store$.dispatch(TogglePlanningEco());
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#ecoFaults'))).toBeDefined();
+    });
+    it('should not display dangerous fault comment textbox if there are not any', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#dangerousFaultComment'))).toBeNull();
+    });
+    it('should display dangerous fault comment textbox if there are any', () => {
+      store$.dispatch(AddDangerousFault(Competencies.judgementCrossing));
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#dangerousFaultComment'))).toBeDefined();
+    });
+    it('should not display serious fault comment textbox if there are not any', () => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#seriousFaultComment'))).toBeNull();
+    });
+    it('should display serious fault comment textbox if there are any', () => {
+      store$.dispatch(AddSeriousFault(Competencies.judgementOvertaking));
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#seriousFaultComment'))).toBeDefined();
+    });
+
+    describe('deferring the write up', () => {
+      it('should dispatch an action to persist tests + pop navstack to root when pressing save and continue', () => {
+        spyOn(component, 'popToRoot');
+        fixture.detectChanges();
+        const saveAndContinueButton = fixture.debugElement.query(By.css('#office-save-button'));
+        saveAndContinueButton.triggerEventHandler('click', null);
+        fixture.detectChanges();
+        expect(component.popToRoot).toHaveBeenCalled();
+      });
+    });
   });
+
 });
--- src/app/pages/office/cat-manoeuvre/office.cat-manoeuvre.page.ts
+++ src/app/pages/office/cat-manoeuvre/office.cat-manoeuvre.page.ts
@@ -1,35 +1,184 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import {
+  ModalController, NavController, Platform, ToastController,
+} from '@ionic/angular';
 import { Router } from '@angular/router';
-import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
-import { JOURNAL_PAGE, TestFlowPageNames } from '../../page-names.constants';
+import { merge, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { FormGroup } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { startsWith } from 'lodash';
+
+import {
+  CommonOfficePageState,
+  OfficeBasePageComponent,
+} from '@shared/classes/test-flow-base-pages/office/office-base-page';
+import { AuthenticationProvider } from '@providers/authentication/authentication';
+import { StoreModel } from '@shared/models/store.model';
+import { OutcomeBehaviourMapProvider } from '@providers/outcome-behaviour-map/outcome-behaviour-map';
+import { WeatherConditionProvider } from '@providers/weather-conditions/weather-condition';
+import { FaultSummaryProvider } from '@providers/fault-summary/fault-summary';
+import { FaultCountProvider } from '@providers/fault-count/fault-count';
+import { behaviourMap } from '@pages/office/office-behaviour-map.cat-cm';
+import { getActivityCodeOptions } from '@shared/constants/activity-code/activity-code.constants';
+import { ExaminerRole } from '@providers/app-config/constants/examiner-role.constants';
+import { AppConfigProvider } from '@providers/app-config/app-config';
+import { getTests } from '@store/tests/tests.reducer';
+import { getCurrentTest } from '@store/tests/tests.selector';
+import { TestOutcome } from '@store/tests/tests.constants';
+import { Language } from '@store/tests/communication-preferences/communication-preferences.model';
+import { CommentSource, FaultSummary } from '@shared/models/fault-marking.model';
+import { PassCertificateNumberChanged } from '@store/tests/pass-completion/pass-completion.actions';
+import { PassCertificateNumberReceived } from '@store/tests/post-test-declarations/post-test-declarations.actions';
+import { getCommunicationPreference } from '@store/tests/communication-preferences/communication-preferences.reducer';
+import { getConductedLanguage } from '@store/tests/communication-preferences/communication-preferences.selector';
+import { getDelegatedTestIndicator } from '@store/tests/delegated-test/delegated-test.reducer';
+import { isDelegatedTest } from '@store/tests/delegated-test/delegated-test.selector';
+import { AddDangerousFaultComment } from '@store/tests/test-data/common/dangerous-faults/dangerous-faults.actions';
+import { CompetencyOutcome } from '@shared/models/competency-outcome';
+import { AddManoeuvreComment } from '@store/tests/test-data/common/manoeuvres/manoeuvres.actions';
+import { AddSeriousFaultComment } from '@store/tests/test-data/common/serious-faults/serious-faults.actions';
+
+interface CatManoeuvreOfficePageState {
+  delegatedTest$: Observable<boolean>;
+  conductedLanguage$: Observable<string>;
+}
+type OfficePageState = CommonOfficePageState & CatManoeuvreOfficePageState;
 
 @Component({
   selector: '.office-cat-manoeuvre-page',
   templateUrl: './office.cat-manoeuvre.page.html',
   styleUrls: ['../office.page.scss'],
 })
-export class OfficeCatManoeuvrePage implements OnInit {
+export class OfficeCatManoeuvrePage extends OfficeBasePageComponent implements OnInit {
+
+  pageState: OfficePageState;
+  pageSubscription: Subscription;
+  form: FormGroup;
+  testOutcomeText: string;
+  conductedLanguage: string;
+  isDelegated: boolean;
 
   constructor(
-    private navController: NavController,
-    public routeByCat: RouteByCategoryProvider,
-    private router: Router,
-  ) { }
-
-  ngOnInit() {
+    platform: Platform,
+    authenticationProvider: AuthenticationProvider,
+    router: Router,
+    store$: Store<StoreModel>,
+    navController: NavController,
+    toastController: ToastController,
+    modalController: ModalController,
+    outcomeBehaviourProvider: OutcomeBehaviourMapProvider,
+    weatherConditionProvider: WeatherConditionProvider,
+    faultSummaryProvider: FaultSummaryProvider,
+    faultCountProvider: FaultCountProvider,
+    private appConfig: AppConfigProvider,
+  ) {
+    super(
+      platform,
+      authenticationProvider,
+      router,
+      store$,
+      navController,
+      toastController,
+      modalController,
+      outcomeBehaviourProvider,
+      weatherConditionProvider,
+      faultSummaryProvider,
+      faultCountProvider,
+    );
+    this.outcomeBehaviourProvider.setBehaviourMap(behaviourMap);
+    this.activityCodeOptions = getActivityCodeOptions(this.appConfig.getAppConfig().role === ExaminerRole.DLG);
   }
 
-  navigateBack(): void {
-    this.navController.back();
+  ngOnInit(): void {
+    super.onInitialisation();
+
+    const currentTest$ = this.store$.pipe(
+      select(getTests),
+      select(getCurrentTest),
+    );
+
+    this.pageState = {
+      ...this.commonPageState,
+      conductedLanguage$: currentTest$.pipe(
+        select(getCommunicationPreference),
+        select(getConductedLanguage),
+      ),
+      delegatedTest$: currentTest$.pipe(
+        select(getDelegatedTestIndicator),
+        select(isDelegatedTest),
+      ),
+    };
+
+    this.setupSubscription();
   }
 
-  async navigateForward(): Promise<void> {
-    await this.routeByCat.navigateToPage(TestFlowPageNames.REKEY_REASON_PAGE);
+  setupSubscription(): void {
+    super.setupSubscriptions();
+
+    const { testOutcomeText$, conductedLanguage$, delegatedTest$ } = this.pageState;
+
+    this.pageSubscription = merge(
+      conductedLanguage$.pipe(map((result) => this.conductedLanguage = result)),
+      testOutcomeText$.pipe(map((result) => this.testOutcomeText = result)),
+      delegatedTest$.pipe(map((result) => this.isDelegated = result)),
+    ).subscribe();
   }
 
-  async navigateJournal(): Promise<void> {
-    await this.router.navigate([JOURNAL_PAGE]);
+  ionViewDidLeave(): void {
+    super.ionViewDidLeave();
+
+    if (this.pageSubscription) {
+      this.pageSubscription.unsubscribe();
+    }
   }
 
+  isPass(): boolean {
+    return this.testOutcomeText === TestOutcome.Passed;
+  }
+
+  isWelsh(): boolean {
+    return this.conductedLanguage === Language.CYMRAEG;
+  }
+
+  passCertificateNumberChanged(passCertificateNumber: string): void {
+    this.store$.dispatch(PassCertificateNumberChanged(passCertificateNumber));
+    this.store$.dispatch(PassCertificateNumberReceived(this.form.get('passCertificateNumberCtrl').valid));
+  }
+
+  dangerousFaultCommentChanged(dangerousFaultComment: FaultSummary): void {
+    if (dangerousFaultComment.source === CommentSource.SIMPLE) {
+      this.store$.dispatch(
+        AddDangerousFaultComment(dangerousFaultComment.competencyIdentifier, dangerousFaultComment.comment),
+      );
+    } else if (startsWith(dangerousFaultComment.source, CommentSource.MANOEUVRES)) {
+      const segments = dangerousFaultComment.source.split('-');
+      const fieldName = segments[1];
+      const controlOrObservation = segments[2];
+      this.store$.dispatch(AddManoeuvreComment(
+        fieldName,
+        CompetencyOutcome.D,
+        controlOrObservation,
+        dangerousFaultComment.comment,
+      ));
+    }
+  }
+
+  seriousFaultCommentChanged(seriousFaultComment: FaultSummary): void {
+    if (seriousFaultComment.source === CommentSource.SIMPLE) {
+      this.store$.dispatch(
+        AddSeriousFaultComment(seriousFaultComment.competencyIdentifier, seriousFaultComment.comment),
+      );
+    } else if (startsWith(seriousFaultComment.source, CommentSource.MANOEUVRES)) {
+      const segments = seriousFaultComment.source.split('-');
+      const fieldName = segments[1];
+      const controlOrObservation = segments[2];
+      this.store$.dispatch(AddManoeuvreComment(
+        fieldName,
+        CompetencyOutcome.S,
+        controlOrObservation,
+        seriousFaultComment.comment,
+      ));
+    }
+  }
 }
