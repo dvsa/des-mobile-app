a0619a2b26e70cc4c51b38996a08ac9493a3ed21
Matthew Bell <mbell.dvsa@icloud.com>
2022-02-10 12:17:13 +0000

    implement provisional office page

--- src/app/pages/office/cat-manoeuvre/office.cat-manoeuvre.module.ts
+++ src/app/pages/office/cat-manoeuvre/office.cat-manoeuvre.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { EffectsModule } from '@ngrx/effects';
+
 import { OfficeAnalyticsEffects } from '@pages/office/office.analytics.effects';
 import { OfficeEffects } from '@pages/office/office.effects';
 import { ComponentsModule } from '@components/common/common-components.module';
 import { OfficeComponentsModule } from '@pages/office/components/office.components.module';
 import { FaultSummaryProvider } from '@providers/fault-summary/fault-summary';
+import { TestFinalisationComponentsModule } from '@components/test-finalisation/test-finalisation-components.module';
+import {
+  PassFinalisationComponentsModule,
+} from '@pages/pass-finalisation/components/pass-finalisation-components.module';
+
 import { OfficeCatManoeuvrePage } from './office.cat-manoeuvre.page';
 import { OfficeCatManoeuvrePageRoutingModule } from './office.cat-manoeuvre-routing.module';
 
@@ -25,6 +30,8 @@ import { OfficeCatManoeuvrePageRoutingModule } from './office.cat-manoeuvre-rout
     IonicModule,
     OfficeComponentsModule,
     OfficeCatManoeuvrePageRoutingModule,
+    TestFinalisationComponentsModule,
+    PassFinalisationComponentsModule,
   ],
   providers: [
     FaultSummaryProvider,
