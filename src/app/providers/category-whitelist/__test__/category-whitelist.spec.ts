2fa5cb673e0565c364f5fa64ecf01ee6e9cffabd
andrew <andrew.setterfield@dvsa.gov.uk>
2022-02-22 13:41:08 +0000

    restore category whitelist

--- src/app/providers/category-whitelist/__test__/category-whitelist.spec.ts
+++ src/app/providers/category-whitelist/__test__/category-whitelist.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { CategoryWhitelistProvider } from '../category-whitelist';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 describe('CategoryWhitelistProvider', () => {
   let provider: CategoryWhitelistProvider;
@@ -12,6 +13,16 @@ describe('CategoryWhitelistProvider', () => {
   describe('Should be correctly configured', () => {
     it('should be created successfully', () => {
       expect(provider).toBeTruthy();
+    });
+  });
+
+  describe('isWhiteListed', () => {
+    it('shout return true if category whitelisted', () => {
+      expect(provider.isWhiteListed(TestCategory.B)).toEqual(true);
+    });
+
+    it('shout return false if category blacklisted', () => {
+      expect(provider.isWhiteListed(TestCategory.A)).toEqual(false);
     });
   });
 
