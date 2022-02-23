514970e04285344b6afe788244fc1015bac385c6
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2022-02-23 08:10:42 +0000

    cherry pick - Reinstate whitelist (#522)
    * restore category whitelist
    
    * updated linting
    
    * add manoeucres cats
    
    Co-authored-by: Matthew Bell <mbell.dvsa@icloud.com>

--- src/app/providers/category-whitelist/__test__/category-whitelist.spec.ts
+++ src/app/providers/category-whitelist/__test__/category-whitelist.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import { CategoryWhitelistProvider } from '../category-whitelist';
 
 describe('CategoryWhitelistProvider', () => {
@@ -12,6 +13,16 @@ describe('CategoryWhitelistProvider', () => {
   describe('Should be correctly configured', () => {
     it('should be created successfully', () => {
       expect(provider).toBeTruthy();
+    });
+  });
+
+  describe('isWhiteListed', () => {
+    it('shout return true if category whitelisted', () => {
+      expect(provider.isWhiteListed(TestCategory.B)).toEqual(true);
+    });
+
+    it('shout return false if category blacklisted', () => {
+      expect(provider.isWhiteListed(TestCategory.A)).toEqual(false);
     });
   });
 
