0ac2f8ad1f1641e63ad6701eb84c0221d2d6d245
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2022-02-28 16:08:16 +0000

    Update c1 reducer to use cat c pass completion (#529)

--- src/store/tests/tests.cat-c1.reducer.ts
+++ src/store/tests/tests.cat-c1.reducer.ts
@@ -15,7 +15,7 @@ import { activityCodeReducer } from './activity-code/activity-code.reducer';
 import { journalDataCatCReducer } from './journal-data/cat-c/journal-data.cat-c.reducer';
 import { vehicleDetailsCatCReducer } from './vehicle-details/cat-c/vehicle-details.cat-c.reducer';
 import { testDataCatC1Reducer } from './test-data/cat-c/test-data.cat-c1.reducer';
-import { passCompletionReducer } from './pass-completion/pass-completion.reducer';
+import { passCompletionCatCReducer } from './pass-completion/cat-c/pass-completion.cat-c.reducer';
 import { delegatedTestReducer } from './delegated-test/delegated-test.reducer';
 import { changeMarkerReducer } from './change-marker/change-marker.reducer';
 import { testSummaryReducer } from './test-summary/test-summary.reducer';
@@ -39,7 +39,7 @@ export function testsCatC1Reducer(
       examinerConducted: examinerConductedReducer,
       examinerKeyed: examinerKeyedReducer,
       journalData: journalDataCatCReducer,
-      passCompletion: passCompletionReducer,
+      passCompletion: passCompletionCatCReducer,
       postTestDeclarations: postTestDeclarationsReducer,
       preTestDeclarations: preTestDeclarationsCatCReducer,
       rekey: rekeyReducer,
