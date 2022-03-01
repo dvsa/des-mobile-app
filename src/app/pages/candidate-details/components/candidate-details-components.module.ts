5c1fffb721023f75911744dcf9a0b8fe37a297cf
RLCorp <rhys.lewis@dvsa.gov.uk>
2022-03-01 11:52:38 +0000

    added banner to details card, moved banner to common components.

--- src/app/pages/candidate-details/components/candidate-details-components.module.ts
+++ src/app/pages/candidate-details/components/candidate-details-components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import {
   InappropriateUseBannerComponent,
-} from '@pages/candidate-details/components/inappropriate-use-banner/inappropriate-use-banner';
+} from '@components/common/inappropriate-use-banner/inappropriate-use-banner';
 import { IonicModule } from '@ionic/angular';
 
 @NgModule({
