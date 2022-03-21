770a0fdc8dedb22f70f7ddc7e958bbf9ae5a57e5
Ryan <ryan_gittins_33@hotmail.co.uk>
2022-03-21 15:46:55 +0000

    WIP: Migration of pass-finalisation for cat-home

--- src/app/pages/waiting-room-to-car/cat-home-test/components/waiting-room-to-car-.cat-home.components.module.ts
+++ src/app/pages/waiting-room-to-car/cat-home-test/components/waiting-room-to-car-.cat-home.components.module.ts
@@ -5,7 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {
   VehicleChecksModalCatHomeModule,
-} from '@pages/waiting-room-to-car/cat-home-test/components/vehicle-checks-modal/vehicle-checks-modal.cat-home.page.module';
+} from
+  '@pages/waiting-room-to-car/cat-home-test/components/vehicle-checks-modal/vehicle-checks-modal.cat-home.page.module';
 import {
   VehicleChecksCatHomeTestComponent,
 } from '@pages/waiting-room-to-car/cat-home-test/components/vehicle-checks/vehicle-checks';
