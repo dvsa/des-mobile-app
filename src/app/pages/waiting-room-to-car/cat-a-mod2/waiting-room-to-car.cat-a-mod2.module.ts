08d66ce98e318973c6ec763e14b1a3a55a8132b0
andrew <andrew.setterfield@dvsa.gov.uk>
2022-03-24 10:29:10 +0000

    WIP

--- src/app/pages/waiting-room-to-car/cat-a-mod2/waiting-room-to-car.cat-a-mod2.module.ts
+++ src/app/pages/waiting-room-to-car/cat-a-mod2/waiting-room-to-car.cat-a-mod2.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { WaitingRoomToCarCatAMod2PageRoutingModule } from './waiting-room-to-car.cat-a-mod2-routing.module';
 import { WaitingRoomToCarCatAMod2Page } from './waiting-room-to-car.cat-a-mod2.page';
+import { ComponentsModule } from '@components/common/common-components.module';
+import {
+  WaitingRoomToCarComponentsModule
+} from '@pages/waiting-room-to-car/components/waiting-room-to-car.components.module';
+import { EffectsModule } from '@ngrx/effects';
+import { WaitingRoomToCarAnalyticsEffects } from '@pages/waiting-room-to-car/waiting-room-to-car.analytics.effects';
 
 @NgModule({
   imports: [
@@ -12,6 +18,12 @@ import { WaitingRoomToCarCatAMod2Page } from './waiting-room-to-car.cat-a-mod2.p
     FormsModule,
     IonicModule,
     WaitingRoomToCarCatAMod2PageRoutingModule,
+    ReactiveFormsModule,
+    ComponentsModule,
+    WaitingRoomToCarComponentsModule,
+    EffectsModule.forFeature([
+      WaitingRoomToCarAnalyticsEffects,
+    ]),
   ],
   declarations: [WaitingRoomToCarCatAMod2Page],
 })
