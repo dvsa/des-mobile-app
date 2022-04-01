c69f5b18c168b3632605f38ab788cadc32d0b200
Matthew Bell <mbell.dvsa@icloud.com>
2022-04-01 13:49:28 +0100

    Get coverage on everything except page state

--- src/app/shared/classes/test-flow-base-pages/office/__tests__/office-base-page.spec.ts
+++ src/app/shared/classes/test-flow-base-pages/office/__tests__/office-base-page.spec.ts
@@ -1,6 +1,6 @@
 import {
   waitForAsync,
-  TestBed,
+  TestBed, fakeAsync, tick,
 } from '@angular/core/testing';
 import {
   ModalController,
@@ -31,21 +31,48 @@ import { OutcomeBehaviourMapProviderMock } from '@providers/outcome-behaviour-ma
 import { Identification, IndependentDriving, WeatherConditions } from '@dvsa/mes-test-schema/categories/common';
 import {
   AdditionalInformationChanged,
-  CandidateDescriptionChanged,
+  CandidateDescriptionChanged, D255No, D255Yes, DebriefUnWitnessed, DebriefWitnessed,
   IdentificationUsedChanged,
   IndependentDrivingTypeChanged,
-  RouteNumberChanged,
+  RouteNumberChanged, TrueLikenessToPhotoChanged,
   WeatherConditionsChanged,
 } from '@store/tests/test-summary/test-summary.actions';
 import { SetActivityCode } from '@store/tests/activity-code/activity-code.actions';
-import { CompleteTest, SavingWriteUpForLater } from '@pages/office/office.actions';
+import {
+  CompleteTest, OfficeValidationError, OfficeViewDidEnter, SavingWriteUpForLater, TestStartDateChanged,
+} from '@pages/office/office.actions';
 import { PersistTests } from '@store/tests/tests.actions';
 import { FaultSummaryProvider } from '@providers/fault-summary/fault-summary';
 import { FaultCountProvider } from '@providers/fault-count/fault-count';
 import { FinishTestModal } from '@pages/office/components/finish-test-modal/finish-test-modal';
+import {
+  ProvisionalLicenseNotReceived,
+  ProvisionalLicenseReceived,
+} from '@store/tests/pass-completion/pass-completion.actions';
+import {
+  DualControlsToggled, GearboxCategoryChanged, SchoolBikeToggled, SchoolCarToggled,
+} from '@store/tests/vehicle-details/vehicle-details.actions';
+import { HealthDeclarationAccepted } from '@store/tests/post-test-declarations/post-test-declarations.actions';
+import {
+  CandidateChoseToProceedWithTestInEnglish,
+  CandidateChoseToProceedWithTestInWelsh,
+} from '@store/tests/communication-preferences/communication-preferences.actions';
+import {
+  InstructorAccompanimentToggled,
+  InterpreterAccompanimentToggled, OtherAccompanimentToggled,
+  SupervisorAccompanimentToggled,
+} from '@store/tests/accompaniment/accompaniment.actions';
+import { CircuitTypeChanged } from '@store/tests/test-summary/cat-a-mod1/test-summary.cat-a-mod1.actions';
+import { TestFlowPageNames } from '@pages/page-names.constants';
+import { take } from 'rxjs/operators';
+import {
+  AbstractControl, FormControl, FormGroup, Validators,
+} from '@angular/forms';
+import { of, Subscription } from 'rxjs';
+import { SetStartDate } from '@store/tests/journal-data/common/test-slot-attributes/test-slot-attributes.actions';
 import { OfficeBasePageComponent } from '../office-base-page';
 
-describe('OfficeBasePageComponent', () => {
+fdescribe('OfficeBasePageComponent', () => {
   let platform: Platform;
   let authenticationProvider: AuthenticationProvider;
   let router: Router;
@@ -139,7 +166,15 @@ describe('OfficeBasePageComponent', () => {
     it('should resolve state variables', () => {
       basePageComponent.onInitialisation();
       basePageComponent.commonPageState.activityCode$
+        .pipe(take(1))
         .subscribe((res: ActivityCodeModel) => expect(res.activityCode).toEqual('1'));
+    });
+  });
+
+  describe('ionViewDidEnter', () => {
+    it('should dispatch OfficeViewDidEnter', () => {
+      basePageComponent.ionViewDidEnter();
+      expect(store$.dispatch).toHaveBeenCalledWith(OfficeViewDidEnter());
     });
   });
 
@@ -159,9 +194,34 @@ describe('OfficeBasePageComponent', () => {
   });
 
   describe('activityCodeChanged', () => {
+    beforeEach(() => {
+      basePageComponent.form.addControl('showMeQuestion', new FormControl('', []));
+      spyOn(basePageComponent.form.controls['showMeQuestion'], 'setValue');
+    });
     it('should dispatch a SetActivityCode action with the activity code', () => {
+      basePageComponent.form.controls = { showMeQuestion: null };
       basePageComponent.activityCodeChanged(activityCodeModelList[0]);
       expect(store$.dispatch).toHaveBeenCalledWith(SetActivityCode(activityCodeModelList[0].activityCode));
+    });
+    it('should setValue of showMeQuestion to {}', () => {
+      basePageComponent.form.controls = {
+        showMeQuestion: {
+          ...basePageComponent.form.controls['showMeQuestion'],
+          value: { code: 'N/A' },
+        } as AbstractControl,
+      };
+      basePageComponent.activityCodeChanged(activityCodeModelList[0]);
+      expect(basePageComponent.form.controls['showMeQuestion'].setValue).toHaveBeenCalledWith({});
+    });
+    it('should not setValue of showMeQuestion when value is not N/A', () => {
+      basePageComponent.form.controls = {
+        showMeQuestion: {
+          ...basePageComponent.form.controls['showMeQuestion'],
+          value: { code: 'A1' },
+        } as AbstractControl,
+      };
+      basePageComponent.activityCodeChanged(activityCodeModelList[0]);
+      expect(basePageComponent.form.controls['showMeQuestion'].setValue).not.toHaveBeenCalled();
     });
   });
 
@@ -209,6 +269,58 @@ describe('OfficeBasePageComponent', () => {
     });
   });
 
+  describe('setupSubscriptions', () => {
+    it('should assign store values to variables', () => {
+      basePageComponent.commonPageState = {
+        ...basePageComponent.commonPageState,
+        startDateTime$: of('2022-01-01T12:00:00'),
+      };
+      basePageComponent.setupSubscriptions();
+      expect(basePageComponent.startDateTime).toEqual('2022-01-01T12:00:00');
+    });
+  });
+
+  describe('ionViewDidLeave', () => {
+    it('should unsubscribe when subscription', () => {
+      basePageComponent.subscription = new Subscription();
+      spyOn(basePageComponent.subscription, 'unsubscribe');
+      basePageComponent.ionViewDidLeave();
+      expect(basePageComponent.subscription.unsubscribe).toHaveBeenCalled();
+    });
+  });
+
+  describe('onSubmit', () => {
+    beforeEach(() => {
+      spyOn(basePageComponent, 'showFinishTestModal');
+    });
+    it('should call through to showFinishTestModal when form is valid', async () => {
+      spyOn(basePageComponent, 'isFormValid').and.returnValue(Promise.resolve(true));
+      await basePageComponent.onSubmit();
+      expect(basePageComponent.showFinishTestModal).toHaveBeenCalled();
+    });
+    it('should not call through to showFinishTestModal when form is invalid', async () => {
+      spyOn(basePageComponent, 'isFormValid').and.returnValue(Promise.resolve(false));
+      await basePageComponent.onSubmit();
+      expect(basePageComponent.showFinishTestModal).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('goToReasonForRekey', () => {
+    beforeEach(() => {
+      spyOn(router, 'navigate');
+    });
+    it('should call through to router.navigate when form is valid', async () => {
+      spyOn(basePageComponent, 'isFormValid').and.returnValue(Promise.resolve(true));
+      await basePageComponent.goToReasonForRekey();
+      expect(router.navigate).toHaveBeenCalledWith([TestFlowPageNames.REKEY_REASON_PAGE]);
+    });
+    it('should not call through to router.navigate when form is invalid', async () => {
+      spyOn(basePageComponent, 'isFormValid').and.returnValue(Promise.resolve(false));
+      await basePageComponent.goToReasonForRekey();
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
+  });
+
   describe('additionalInformationChanged', () => {
     it('should dispatch an Additional Information change action with the new value', () => {
       const info: string = 'Nothing more to say.';
@@ -227,6 +339,14 @@ describe('OfficeBasePageComponent', () => {
     });
   });
 
+  describe('dateOfTestChanged', () => {
+    it('should ', () => {
+      basePageComponent.startDateTime = '2022-01-01T12:00:00';
+      basePageComponent.dateOfTestChanged('2022-01-02');
+      expect(store$.dispatch).toHaveBeenCalledWith(TestStartDateChanged('2022-01-01T12:00:00', '2022-01-02T12:00:00'));
+      expect(store$.dispatch).toHaveBeenCalledWith(SetStartDate('2022-01-02T12:00:00'));
+    });
+  });
   describe('routeNumberChanged', () => {
     it('should dispatch a Route change action with the new value', () => {
       const route: number = 17;
@@ -235,7 +355,6 @@ describe('OfficeBasePageComponent', () => {
       expect(store$.dispatch).toHaveBeenCalledWith(RouteNumberChanged(route));
     });
   });
-
   describe('independentDrivingChanged', () => {
     it('should dispatch an Independent Driving change action with the new value', () => {
       const drivingAid: IndependentDriving = 'Diagram';
@@ -244,21 +363,126 @@ describe('OfficeBasePageComponent', () => {
       expect(store$.dispatch).toHaveBeenCalledWith(IndependentDrivingTypeChanged(drivingAid));
     });
   });
-
+  describe('trueLikenessToPhotoChanged', () => {
+    it('should dispatch TrueLikenessToPhotoChanged with true', () => {
+      basePageComponent.trueLikenessToPhotoChanged(true);
+      expect(store$.dispatch).toHaveBeenCalledWith(TrueLikenessToPhotoChanged(true));
+    });
+  });
+  describe('provisionalLicenseReceived', () => {
+    it('should dispatch ProvisionalLicenseReceived', () => {
+      basePageComponent.provisionalLicenseReceived();
+      expect(store$.dispatch).toHaveBeenCalledWith(ProvisionalLicenseReceived());
+    });
+  });
+  describe('provisionalLicenseNotReceived', () => {
+    it('should dispatch ProvisionalLicenseNotReceived', () => {
+      basePageComponent.provisionalLicenseNotReceived();
+      expect(store$.dispatch).toHaveBeenCalledWith(ProvisionalLicenseNotReceived());
+    });
+  });
+  describe('transmissionChanged', () => {
+    it('should dispatch GearboxCategoryChanged with Manual', () => {
+      basePageComponent.transmissionChanged('Manual');
+      expect(store$.dispatch).toHaveBeenCalledWith(GearboxCategoryChanged('Manual'));
+    });
+  });
+  describe('healthDeclarationChanged', () => {
+    it('should dispatch HealthDeclarationAccepted with false', () => {
+      basePageComponent.healthDeclarationChanged(false);
+      expect(store$.dispatch).toHaveBeenCalledWith(HealthDeclarationAccepted(false));
+    });
+  });
+  describe('d255Changed', () => {
+    [
+      { outcome: true, action: D255Yes },
+      { outcome: false, action: D255No },
+    ].forEach(({ action, outcome }) => {
+      it(`should dispatch D255${outcome ? 'Yes' : 'No'}`, () => {
+        basePageComponent.d255Changed(outcome);
+        expect(store$.dispatch).toHaveBeenCalledWith(action());
+      });
+    });
+  });
+  describe('isWelshChanged', () => {
+    it('should dispatch CandidateChoseToProceedWithTestInWelsh with Cymraeg', () => {
+      basePageComponent.isWelshChanged(true);
+      expect(store$.dispatch).toHaveBeenCalledWith(CandidateChoseToProceedWithTestInWelsh('Cymraeg'));
+    });
+    it('should dispatch CandidateChoseToProceedWithTestInEnglish with English', () => {
+      basePageComponent.isWelshChanged(false);
+      expect(store$.dispatch).toHaveBeenCalledWith(CandidateChoseToProceedWithTestInEnglish('English'));
+    });
+  });
+  describe('instructorAccompanimentToggled', () => {
+    it('should dispatch InstructorAccompanimentToggled', () => {
+      basePageComponent.instructorAccompanimentToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(InstructorAccompanimentToggled());
+    });
+  });
+  describe('supervisorAccompanimentToggled', () => {
+    it('should dispatch SupervisorAccompanimentToggled', () => {
+      basePageComponent.supervisorAccompanimentToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(SupervisorAccompanimentToggled());
+    });
+  });
+  describe('interpreterAccompanimentToggled', () => {
+    it('should dispatch InterpreterAccompanimentToggled', () => {
+      basePageComponent.interpreterAccompanimentToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(InterpreterAccompanimentToggled());
+    });
+  });
+  describe('schoolBikeToggled', () => {
+    it('should dispatch SchoolBikeToggled', () => {
+      basePageComponent.schoolBikeToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(SchoolBikeToggled());
+    });
+  });
+  describe('otherAccompanimentToggled', () => {
+    it('should dispatch OtherAccompanimentToggled', () => {
+      basePageComponent.otherAccompanimentToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(OtherAccompanimentToggled());
+    });
+  });
+  describe('dualControlsToggled', () => {
+    it('should dispatch DualControlsToggled', () => {
+      basePageComponent.dualControlsToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(DualControlsToggled());
+    });
+  });
+  describe('schoolCarToggled', () => {
+    it('should dispatch SchoolCarToggled', () => {
+      basePageComponent.schoolCarToggled();
+      expect(store$.dispatch).toHaveBeenCalledWith(SchoolCarToggled());
+    });
+  });
+  describe('circuitChanged', () => {
+    it('should dispatch CircuitTypeChanged with Left', () => {
+      basePageComponent.circuitChanged('Left');
+      expect(store$.dispatch).toHaveBeenCalledWith(CircuitTypeChanged('Left'));
+    });
+  });
+  describe('debriefWitnessedChanged', () => {
+    [
+      { witnessed: true, action: DebriefWitnessed },
+      { witnessed: false, action: DebriefUnWitnessed },
+    ].forEach(({ action, witnessed }) => {
+      it(`should dispatch action with ${witnessed}`, () => {
+        basePageComponent.debriefWitnessedChanged(witnessed);
+        expect(store$.dispatch).toHaveBeenCalledWith(action());
+      });
+    });
+  });
   describe('identificationChanged', () => {
     it('should dispatch an Identification change action with the new value', () => {
       const idType: Identification = 'Passport';
       basePageComponent.identificationChanged(idType);
-
       expect(store$.dispatch).toHaveBeenCalledWith(IdentificationUsedChanged(idType));
     });
   });
-
   describe('showFinishTestModal', async () => {
     it('should create a modal', async () => {
-      spyOn(basePageComponent.modalController, 'create')
-        .and
-        .callThrough();
+      spyOn(basePageComponent.modalController, 'create').and.callThrough();
       await basePageComponent.showFinishTestModal();
       expect(basePageComponent.modalController.create).toHaveBeenCalledWith({
         id: 'FinishTestModal',
@@ -271,6 +495,27 @@ describe('OfficeBasePageComponent', () => {
         },
       });
     });
+  });
+  describe('isFormValid', () => {
+    it('should return true if form is valid', async () => {
+      await basePageComponent.isFormValid();
+      expect(basePageComponent.form.valid).toEqual(true);
+    });
+
+    it('should dispatch the appropriate ValidationError actions', fakeAsync(() => {
+      basePageComponent.form = new FormGroup({
+        requiredControl1: new FormControl(null, [Validators.required]),
+        requiredControl2: new FormControl(null, [Validators.required]),
+        notRequiredControl: new FormControl(null),
+      });
+
+      basePageComponent.isFormValid();
+      tick();
+      expect(store$.dispatch).toHaveBeenCalledWith(OfficeValidationError('requiredControl1 is blank'));
+      expect(store$.dispatch).toHaveBeenCalledWith(OfficeValidationError('requiredControl2 is blank'));
+      expect(store$.dispatch).not.toHaveBeenCalledWith(OfficeValidationError('notRequiredControl is blank'));
+      expect(basePageComponent.toast).not.toBeUndefined();
+    }));
   });
 
 });
