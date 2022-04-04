d2edff24567af1ea642af121a6b65f72d04d7df0
RLCorp <88314925+RLCorp@users.noreply.github.com>
2022-04-04 16:08:29 +0100

    mes-7105 bug fixes (#598)
    * Various bug fixes
    
    * Detach change detection in View test result to stop components re-rendering which was giving the appearance of bouncing
    
    Co-authored-by: Matthew Bell <mbell.dvsa@icloud.com>

--- src/app/pages/view-test-result/view-test-result.page.ts
+++ src/app/pages/view-test-result/view-test-result.page.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component, Input, OnInit,
+} from '@angular/core';
 import { ModalController, Platform } from '@ionic/angular';
 import { BasePageComponent } from '@shared/classes/base-page';
 import { AuthenticationProvider } from '@providers/authentication/authentication';
@@ -66,6 +69,7 @@ export class ViewTestResultPage extends BasePageComponent implements OnInit {
     private loadingProvider: LoadingProvider,
     private faultCountProvider: FaultCountProvider,
     private faultSummaryProvider: FaultSummaryProvider,
+    private ref: ChangeDetectorRef,
   ) {
     super(platform, authenticationProvider, router);
   }
@@ -107,7 +111,13 @@ export class ViewTestResultPage extends BasePageComponent implements OnInit {
   }
 
   handleLoadingUI = async (isLoading: boolean): Promise<void> => {
+    this.isLoading = isLoading;
+
     await this.loadingProvider.handleUILoading(isLoading, { spinner: 'circles' });
+
+    if (!isLoading) {
+      this.ref.detach();
+    }
   };
 
   getTestDetails(): TestDetailsModel {
@@ -201,20 +211,29 @@ export class ViewTestResultPage extends BasePageComponent implements OnInit {
   showDebriefCommonCard = (): boolean => isAnyOf(this.testCategory, [
     TestCategory.B, // Cat B
     TestCategory.BE, // Cat BE
-    TestCategory.EUAMM1, TestCategory.EUA1M1, TestCategory.EUA2M1, TestCategory.EUAM1,
-    TestCategory.C, TestCategory.C1, TestCategory.C1E, TestCategory.CE, // Cat C
-    TestCategory.D, TestCategory.D1, TestCategory.D1E, TestCategory.DE, // Cat D
+    TestCategory.EUAMM1, TestCategory.EUA1M1, TestCategory.EUA2M1, TestCategory.EUAM1, // Cat A Mod 1
+    TestCategory.EUAMM2, TestCategory.EUA1M2, TestCategory.EUA2M2, TestCategory.EUAM2, // Cat A Mod 2
+    TestCategory.C, TestCategory.C1, TestCategory.C1E, TestCategory.CE, // Cat C3B
+    TestCategory.CM, TestCategory.C1M, TestCategory.C1EM, TestCategory.CEM, // Cat C3A
+    TestCategory.D, TestCategory.D1, TestCategory.D1E, TestCategory.DE, // Cat D3B
+    TestCategory.DM, TestCategory.D1M, TestCategory.D1EM, TestCategory.DEM, // Cat D3A
     TestCategory.F, TestCategory.G, TestCategory.H, TestCategory.K, // Cat Home
+    TestCategory.ADI2, // ADI
+    TestCategory.CCPC, TestCategory.DCPC,
   ]);
 
   showVehicleDetailsCommonCard: () => boolean = () => isAnyOf(this.testCategory, [
     TestCategory.B, // Cat B
     TestCategory.BE, // Cat BE
+    TestCategory.EUAMM1, TestCategory.EUA1M1, TestCategory.EUA2M1, TestCategory.EUAM1, // Cat A Mod 1
+    TestCategory.EUAMM2, TestCategory.EUA1M2, TestCategory.EUA2M2, TestCategory.EUAM2, // Cat A Mod 2
     TestCategory.C, TestCategory.C1, TestCategory.C1E, TestCategory.CE, // Cat C 3B
     TestCategory.CM, TestCategory.C1M, TestCategory.C1EM, TestCategory.CEM, // Cat C 3A
     TestCategory.D, TestCategory.D1, TestCategory.D1E, TestCategory.DE, // Cat D 3B
     TestCategory.DM, TestCategory.D1M, TestCategory.D1EM, TestCategory.DEM, // Cat D 3A
     TestCategory.F, TestCategory.G, TestCategory.H, TestCategory.K, // Cat Home
+    TestCategory.ADI2, // ADI
+    TestCategory.CCPC, TestCategory.DCPC,
   ]);
 
   getDrivingFaultSumCount(): number {
