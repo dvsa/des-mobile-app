82d694ad4df684457bdf3571cd9355b524f30f47
Ryan <ryan_gittins_33@hotmail.co.uk>
2022-04-06 10:00:53 +0100

    reverted incorrect deletion

---
+++ src/store/tests/test-summary/cat-a-mod2/test-summary.cat-a-mod2.selector.ts
@@ -0,0 +1,6 @@
+import {
+  TestSummary,
+  ModeOfTransport,
+} from '@dvsa/mes-test-schema/categories/AM2';
+
+export const getModeOfTransport = (ts: TestSummary): ModeOfTransport => ts.modeOfTransport;
