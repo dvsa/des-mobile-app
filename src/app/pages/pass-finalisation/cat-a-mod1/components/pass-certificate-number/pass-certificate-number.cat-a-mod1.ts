99b68a512a577b090f721c7430b534c13d52bd53
Matthew Bell <mbell.dvsa@icloud.com>
2022-05-24 10:53:18 +0100

    Fix validation on mod1 pass cert

--- src/app/pages/pass-finalisation/cat-a-mod1/components/pass-certificate-number/pass-certificate-number.cat-a-mod1.ts
+++ src/app/pages/pass-finalisation/cat-a-mod1/components/pass-certificate-number/pass-certificate-number.cat-a-mod1.ts
@@ -3,9 +3,6 @@ import {
 } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {
-  PASS_CERTIFICATE_LENGTH_A_MOD1,
-} from '@providers/pass-certificate-validation/pass-certificate-validation.constants';
-import {
   getByteCount,
   getPassCertificateAMOD1Validator,
 } from '@shared/constants/field-validators/field-validators';
@@ -35,8 +32,6 @@ export class PassCertificateNumberCatAMod1Component implements OnChanges {
   ngOnChanges(): void {
     if (!this.formControl) {
       this.formControl = new FormControl(null, [
-        Validators.maxLength(PASS_CERTIFICATE_LENGTH_A_MOD1),
-        Validators.minLength(PASS_CERTIFICATE_LENGTH_A_MOD1),
         Validators.pattern(this.passCertificateAMOD1Validator.pattern),
         Validators.required,
       ]);
@@ -47,7 +42,6 @@ export class PassCertificateNumberCatAMod1Component implements OnChanges {
   }
 
   passCertificateNumberChanged(passCertificateNumber: string): void {
-    this.formControl.updateValueAndValidity();
     const actualLength: number = getByteCount(passCertificateNumber);
     const permittedLength: number = this.passCertificateAMOD1Validator.maxByteLength;
     const validFormat: boolean = this.passCertificateAMOD1Validator.pattern.test(passCertificateNumber);
