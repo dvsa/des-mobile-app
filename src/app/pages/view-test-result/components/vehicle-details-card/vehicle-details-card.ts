25f6db1e0863e8c3bf53c76aa4dc0fbab312858a
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2022-07-04 13:06:27 +0100

    CP- MES-8108 (#741)
    * Translate boolean to string
    
    * check explicit value
    
    * add bike check

--- src/app/pages/view-test-result/components/vehicle-details-card/vehicle-details-card.ts
+++ src/app/pages/view-test-result/components/vehicle-details-card/vehicle-details-card.ts
@@ -11,6 +11,7 @@ import { CatC1EUniqueTypes } from '@dvsa/mes-test-schema/categories/C1E';
 import * as CatAMod1Types from '@dvsa/mes-test-schema/categories/AM1';
 import { CatBUniqueTypes } from '@dvsa/mes-test-schema/categories/B';
 import { flattenArray } from '@pages/view-test-result/view-test-result-helpers';
+import { isAnyOf } from '@shared/helpers/simplifiers';
 
 @Component({
   selector: 'vehicle-details-card',
@@ -112,6 +113,13 @@ export class VehicleDetailsCardComponent {
 
   public isADI2 = (): boolean => this.category === TestCategory.ADI2;
 
+  public isBike = (): boolean => isAnyOf(this.category, [
+    // Cat Mod1
+    TestCategory.EUA1M1, TestCategory.EUA2M1, TestCategory.EUAM1, TestCategory.EUAMM1,
+    // Cat Mod2
+    TestCategory.EUA1M2, TestCategory.EUA2M2, TestCategory.EUAM2, TestCategory.EUAMM2,
+  ]);
+
   public get instructorRegistrationNumber(): number {
     return get(this.instructorDetails, 'registrationNumber');
   }
@@ -140,8 +148,11 @@ export class VehicleDetailsCardComponent {
     return get(this.data, 'numberOfSeats', '?').toString();
   }
 
-  public get schoolBike(): boolean {
-    return get(this.data, 'schoolBike');
+  public get schoolBike(): string {
+    if (!this.isBike()) {
+      return null;
+    }
+    return get(this.data, 'schoolBike') ? 'Yes' : 'No';
   }
 
   public get trainerPRN(): number {
