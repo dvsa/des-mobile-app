c047224708edba76f2a24cc308e10c27b5bf44ea
RLCorp <rhys.lewis@dvsa.gov.uk>
2022-07-07 12:50:34 +0100

    WIP: Adding ADI3 Pass finalisation page

--- src/app/pages/test-report/cat-adi-part3/test-report.cat-adi-part3.page.ts
+++ src/app/pages/test-report/cat-adi-part3/test-report.cat-adi-part3.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { TestFlowPageNames } from '@pages/page-names.constants';
 import { Router } from '@angular/router';
+import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 @Component({
   selector: 'app-test-report-cat-adi3',
@@ -13,6 +15,7 @@ export class TestReportCatADI3Page implements OnInit {
   constructor(
     private navController: NavController,
     private router: Router,
+    private routeByCat: RouteByCategoryProvider,
   ) { }
 
   ngOnInit() {
@@ -20,6 +23,9 @@ export class TestReportCatADI3Page implements OnInit {
 
   async navigateForward(): Promise<void> {
     await this.router.navigate([TestFlowPageNames.DEBRIEF_PAGE]);
+  }
+  async navigateToPass(): Promise<void> {
+    await this.routeByCat.navigateToPage(TestFlowPageNames.PASS_FINALISATION_PAGE, TestCategory.ADI3);
   }
 
 }
