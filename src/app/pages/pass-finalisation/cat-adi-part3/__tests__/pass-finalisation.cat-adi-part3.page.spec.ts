bb694c47de9d8d550cd921610e409a2aa518f422
RLCorp <rhys.lewis@dvsa.gov.uk>
2022-07-14 16:41:14 +0100

    Added finished Finalised Outcome Page for ADI 3

--- src/app/pages/pass-finalisation/cat-adi-part3/__tests__/pass-finalisation.cat-adi-part3.page.spec.ts
+++ src/app/pages/pass-finalisation/cat-adi-part3/__tests__/pass-finalisation.cat-adi-part3.page.spec.ts
@@ -1,4 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture, TestBed, waitForAsync,
+} from '@angular/core/testing';
 import { NavController, Platform, IonicModule } from '@ionic/angular';
 import { NavControllerMock, PlatformMock } from 'ionic-mocks';
 import { Router } from '@angular/router';
@@ -6,53 +8,86 @@ import { AuthenticationProvider } from '@providers/authentication/authentication
 import { AuthenticationProviderMock } from '@providers/authentication/__mocks__/authentication.mock';
 import { OutcomeBehaviourMapProvider } from '@providers/outcome-behaviour-map/outcome-behaviour-map';
 import { Store } from '@ngrx/store';
-import { RouteByCategoryProvider } from '@providers/route-by-category/route-by-category';
-import { RouteByCategoryProviderMock } from '@providers/route-by-category/__mocks__/route-by-category.mock';
+import { configureTestSuite } from 'ng-bullet';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MockComponent } from 'ng-mocks';
+import { PracticeModeBanner } from '@components/common/practice-mode-banner/practice-mode-banner';
+import { DebriefWitnessedComponent } from '@components/test-finalisation/debrief-witnessed/debrief-witnessed';
+import { FinalisationHeaderComponent } from '@components/test-finalisation/finalisation-header/finalisation-header';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppModule } from '@app/app.module';
+import {
+  FurtherDevelopmentComponent,
+} from '@pages/pass-finalisation/cat-adi-part3/components/further-development/further-development.component';
+import { StoreModel } from '@shared/models/store.model';
+import {
+  ReasonForNoAdviceGivenChanged,
+  SeekFurtherDevelopmentChanged,
+} from '@store/tests/test-data/cat-adi-part3/review/review.actions';
 import { PassFinalisationCatADIPart3Page } from '../pass-finalisation.cat-adi-part3.page';
 
 fdescribe('PassFinalisation.CatAdiPart3.Page.HtmlPage', () => {
   let component: PassFinalisationCatADIPart3Page;
   let fixture: ComponentFixture<PassFinalisationCatADIPart3Page>;
+  let store$: Store<StoreModel>;
   const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl', 'navigate']);
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
-      declarations: [PassFinalisationCatADIPart3Page],
-      imports: [IonicModule.forRoot()],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      declarations: [
+        PassFinalisationCatADIPart3Page,
+        MockComponent(PracticeModeBanner),
+        MockComponent(FinalisationHeaderComponent),
+        MockComponent(DebriefWitnessedComponent),
+        MockComponent(FurtherDevelopmentComponent),
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        IonicModule,
+        AppModule,
+      ],
       providers: [
         {
           provide: Platform,
           useFactory: () => PlatformMock.instance(),
         },
-        {
-          provide: AuthenticationProvider,
-          useClass: AuthenticationProviderMock,
-        },
-        {
-          provide: Router,
-          useValue: routerSpy,
-        },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+        { provide: Router, useValue: routerSpy },
         { provide: Store },
-        {
-          provide: RouteByCategoryProvider,
-          useClass: RouteByCategoryProviderMock,
-        },
-        {
-          provide: NavController,
-          useClass: NavControllerMock,
-        },
+        { provide: NavController, useClass: NavControllerMock },
         OutcomeBehaviourMapProvider,
       ],
-    })
-      .compileComponents();
+    });
+  });
 
+  beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(PassFinalisationCatADIPart3Page);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store$ = TestBed.inject(Store);
+    spyOn(store$, 'dispatch');
   }));
 
-  it('should create', () => {
-    expect(component)
-      .toBeTruthy();
+  describe('class', () => {
+    it('should create', () => {
+      expect(component)
+        .toBeTruthy();
+    });
+
+    describe('furtherDevelopmentChanged', () => {
+      it('should dispatch SeekFurtherDevelopmentChanged using the parameter given ', () => {
+        component.furtherDevelopmentChanged(true);
+        expect(store$.dispatch)
+          .toHaveBeenCalledWith(SeekFurtherDevelopmentChanged(true));
+      });
+    });
+
+    describe('adviceReasonChanged', () => {
+      it('should dispatch ReasonForNoAdviceGivenChanged using the parameter given ', () => {
+        component.adviceReasonChanged('test');
+        expect(store$.dispatch)
+          .toHaveBeenCalledWith(ReasonForNoAdviceGivenChanged('test'));
+      });
+    });
   });
 });
