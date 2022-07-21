4e674b2cdf6474ca978f8e892dd7e8db9df7f152
andrew <andrew.setterfield@dvsa.gov.uk>
2022-07-21 11:12:36 +0100

    addition logic to change privacy message based upon test type

--- src/app/pages/communication/components/privacy-notice/privacy-notice.ts
+++ src/app/pages/communication/components/privacy-notice/privacy-notice.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { configureI18N } from '@shared/helpers/translation.helpers';
 import { Language } from '@store/tests/communication-preferences/communication-preferences.model';
+import { CategoryCode } from '@dvsa/mes-test-schema/categories/common';
 
 @Component({
   selector: 'privacy-notice',
@@ -14,12 +15,30 @@ export class PrivacyNoticeComponent implements OnInit {
   language: Language;
 
   @Input()
-  isRider: boolean = false;
+  category: CategoryCode;
 
   constructor(private translate: TranslateService) { }
 
   ngOnInit() : void {
     configureI18N(this.language, this.translate);
+  }
+
+  categorySpecificTestContext(cat: CategoryCode): string {
+    switch (cat) {
+      case 'ADI3':
+        return 'ADI';
+      case 'EUA1M1':
+      case 'EUA1M2':
+      case 'EUA2M1':
+      case 'EUA2M2':
+      case 'EUAM1':
+      case 'EUAM2':
+      case 'EUAMM1':
+      case 'EUAMM2':
+        return 'common.riding';
+      default:
+        return 'common.driving';
+    }
   }
 
 }
