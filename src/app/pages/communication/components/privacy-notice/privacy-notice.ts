eda073e4cccf72441ce6939be51b6d1086a8d145
andrew <andrew.setterfield@dvsa.gov.uk>
2022-07-21 11:51:31 +0100

    added enums

--- src/app/pages/communication/components/privacy-notice/privacy-notice.ts
+++ src/app/pages/communication/components/privacy-notice/privacy-notice.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { configureI18N } from '@shared/helpers/translation.helpers';
 import { Language } from '@store/tests/communication-preferences/communication-preferences.model';
 import { CategoryCode } from '@dvsa/mes-test-schema/categories/common';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 @Component({
   selector: 'privacy-notice',
@@ -25,16 +26,16 @@ export class PrivacyNoticeComponent implements OnInit {
 
   categorySpecificTestContext(cat: CategoryCode): string {
     switch (cat) {
-      case 'ADI3':
+      case TestCategory.ADI3:
         return 'ADI';
-      case 'EUA1M1':
-      case 'EUA1M2':
-      case 'EUA2M1':
-      case 'EUA2M2':
-      case 'EUAM1':
-      case 'EUAM2':
-      case 'EUAMM1':
-      case 'EUAMM2':
+      case TestCategory.EUA1M1:
+      case TestCategory.EUA1M2:
+      case TestCategory.EUA2M1:
+      case TestCategory.EUA2M2:
+      case TestCategory.EUAM1:
+      case TestCategory.EUAM2:
+      case TestCategory.EUAMM1:
+      case TestCategory.EUAMM2:
         return 'common.riding';
       default:
         return 'common.driving';
