a86e83b854fbd46fb486ad1f4c2ef9a258b19fb5
gittins7 <45790769+gittins7@users.noreply.github.com>
2022-07-25 14:22:47 +0100

    Changed theme string concatenation to no longer use & (#772)

--- src/components/common/adi3-debrief-card/adi3-debrief-card.ts
+++ src/components/common/adi3-debrief-card/adi3-debrief-card.ts
@@ -42,8 +42,9 @@ export class Adi3DebriefCard implements OnInit {
   ngOnInit(): void {
     const formattedLessonThemeValues = this.lessonTheme.lessonThemes
       .map((theme) => lessonThemeValues[theme]);
-    this.lessonThemeValueStr = `${formattedLessonThemeValues.slice(0, -1)
-      .join(', ')} & ${this.lessonTheme.other ? this.lessonTheme.other : formattedLessonThemeValues.slice(-1)}`;
+    this.lessonThemeValueStr = this.lessonTheme.other
+      ? `${formattedLessonThemeValues.slice(0, -1).join(', ')}, ${this.lessonTheme.other}`
+      : formattedLessonThemeValues.join(', ');
   }
 
   displayGradeDescription(): string {
