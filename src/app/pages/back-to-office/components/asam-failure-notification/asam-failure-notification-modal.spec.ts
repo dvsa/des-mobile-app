0b62eb010c30180e843a05a8d93d42a607736d7d
andrew <andrew.setterfield@dvsa.gov.uk>
2022-08-10 14:41:49 +0100

    WIP

--- src/app/pages/back-to-office/components/asam-failure-notification/asam-failure-notification-modal.spec.ts
+++ src/app/pages/back-to-office/components/asam-failure-notification/asam-failure-notification-modal.spec.ts
@@ -1,24 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { AsamFailureNotificationComponent } from './asam-failure-notification-modal';
-
-describe('AsamFailureNotificationComponent', () => {
-  let component: AsamFailureNotificationComponent;
-  let fixture: ComponentFixture<AsamFailureNotificationComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AsamFailureNotificationComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(AsamFailureNotificationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { IonicModule } from '@ionic/angular';
+//
+// import { AsamFailureNotificationModal } from './asam-failure-notification-modal';
+//
+// describe('AsamFailureNotificationComponent', () => {
+//   let component: AsamFailureNotificationModal;
+//   let fixture: ComponentFixture<AsamFailureNotificationModal>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [AsamFailureNotificationModal],
+//       imports: [IonicModule.forRoot()],
+//     })
+//       .compileComponents();
+//
+//     fixture = TestBed.createComponent(AsamFailureNotificationModal);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   }));
+//
+//   it('should create', () => {
+//     expect(component)
+//       .toBeTruthy();
+//   });
+// });
