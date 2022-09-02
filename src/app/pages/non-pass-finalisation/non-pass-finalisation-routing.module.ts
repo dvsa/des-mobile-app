6a58e27b9593442bcb7a308546506874344df625
Ryan Gittins <45790769+gittins7@users.noreply.github.com>
2022-09-02 12:56:54 +0100

    Cp mes 8184 (#842)
    * WIP: tests left to fix
    
    * WIP: tests
    
    * Update mock for snapshot
    
    * removed focus from test
    
    * styling change for activity code modal
    
    * added input for isAdi3 to office page utilisation of activity code component
    
    Co-authored-by: Matthew Bell <mbell.dvsa@icloud.com>

--- src/app/pages/non-pass-finalisation/non-pass-finalisation-routing.module.ts
+++ src/app/pages/non-pass-finalisation/non-pass-finalisation-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {
     path: '',
     component: NonPassFinalisationPage,
-    resolve: { behaviourMap: NonPassFinalisationResolver },
+    resolve: { nonPassData: NonPassFinalisationResolver },
   },
 ];
 
