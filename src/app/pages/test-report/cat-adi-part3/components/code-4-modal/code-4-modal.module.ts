f0b106eaf2061258644ecab0bcc1f2b273eb3142
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2022-09-16 10:24:04 +0100

    reinstate modal (#865)

--- src/app/pages/test-report/cat-adi-part3/components/code-4-modal/code-4-modal.module.ts
+++ src/app/pages/test-report/cat-adi-part3/components/code-4-modal/code-4-modal.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Code4Modal } from '@pages/test-report/cat-adi-part3/components/code-4-modal/code-4-modal';
+import { ComponentsModule } from '@components/common/common-components.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { Code4Modal } from '@pages/test-report/cat-adi-part3/components/code-4-m
   imports: [
     IonicModule,
     CommonModule,
+    ComponentsModule,
   ],
 })
 export class Code4ModalModule {}
