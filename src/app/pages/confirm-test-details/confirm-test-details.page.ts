c5a4477555970a4fa59244e67415faf96e5def7a
Matthew Bell <mbell.dvsa@icloud.com>
2022-09-29 11:41:55 +0100

    Linting fixes

--- src/app/pages/confirm-test-details/confirm-test-details.page.ts
+++ src/app/pages/confirm-test-details/confirm-test-details.page.ts
@@ -5,7 +5,9 @@ import { select, Store } from '@ngrx/store';
 import { StoreModel } from '@shared/models/store.model';
 import { GearboxCategory } from '@dvsa/mes-test-schema/categories/common';
 import { getTests } from '@store/tests/tests.reducer';
-import { getActivityCode, getCurrentTest, getJournalData, getTestOutcomeText } from '@store/tests/tests.selector';
+import {
+  getActivityCode, getCurrentTest, getJournalData, getTestOutcomeText,
+} from '@store/tests/tests.selector';
 import { getCandidate } from '@store/tests/journal-data/common/candidate/candidate.reducer';
 import {
   getCandidateName,
