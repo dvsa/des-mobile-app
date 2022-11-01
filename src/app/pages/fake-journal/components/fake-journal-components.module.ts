735e2af285e2091e91f5bd0c3b7dfb3dcaf0721e
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2022-11-01 11:19:54 +0000

    Deleted fake test slot

--- src/app/pages/fake-journal/components/fake-journal-components.module.ts
+++ src/app/pages/fake-journal/components/fake-journal-components.module.ts
@@ -3,25 +3,15 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TestSlotComponentsModule } from '@components/test-slot/test-slot-components.module';
 import { PreviewModeModalModule } from '@pages/fake-journal/components/preview-mode-modal/preview-mode-modal.module';
-import { FakeTestSlotComponent } from './fake-test-slot/fake-test-slot';
 import { JournalComponentsModule } from '../../journal/components/journal-components.module';
 
 @NgModule({
-  declarations: [
-    FakeTestSlotComponent,
-  ],
   imports: [
     CommonModule,
     IonicModule,
     JournalComponentsModule,
     TestSlotComponentsModule,
     PreviewModeModalModule,
-  ],
-  entryComponents: [
-    FakeTestSlotComponent,
-  ],
-  exports: [
-    FakeTestSlotComponent,
   ],
 })
 export class FakeJournalComponentsModule {
