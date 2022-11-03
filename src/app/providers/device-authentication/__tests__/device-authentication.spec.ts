e626ac404e79e229717485805cb5d4cd6e59120b
Matthew Bell <mbell.dvsa@icloud.com>
2022-11-03 14:03:25 +0000

    Refactor provider and update specs

--- src/app/providers/device-authentication/__tests__/device-authentication.spec.ts
+++ src/app/providers/device-authentication/__tests__/device-authentication.spec.ts
@@ -4,6 +4,7 @@ import { configureTestSuite } from 'ng-bullet';
 import { PlatformMock } from '@mocks/ionic-mocks/platform-mock';
 import { TestersEnvironmentFile } from '@environments/models/environment.model';
 import { environment } from '@environments/environment';
+import { NativeBiometricMock } from '@mocks/@capacitor/biometrics';
 import { DeviceAuthenticationProvider } from '../device-authentication';
 import { AppConfigProvider } from '../../app-config/app-config';
 import { AppConfigProviderMock } from '../../app-config/__mocks__/app-config.mock';
@@ -11,6 +12,7 @@ import { AppConfig } from '../../app-config/app-config.model';
 
 describe('DeviceAuthenticationProvider', () => {
   let deviceAuthenticationProvider: DeviceAuthenticationProvider;
+  let platform: Platform;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -24,32 +26,49 @@ describe('DeviceAuthenticationProvider', () => {
 
   beforeEach(() => {
     deviceAuthenticationProvider = TestBed.inject(DeviceAuthenticationProvider);
+    platform = TestBed.inject(Platform);
+
+    spyOn(platform, 'ready').and.returnValue(Promise.resolve(''));
+    spyOn(platform, 'is').and.returnValue(true);
+    spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+    (environment as unknown as TestersEnvironmentFile).isTest = false;
+
+    spyOn(NativeBiometricMock, 'isAvailable');
+
+    (NativeBiometricMock.isAvailable as any)
+      .withArgs({ key: 'useFallback' })
+      .and.returnValue(Promise.resolve({ value: { available: true } }));
   });
 
   describe('triggerLockScreen', () => {
-    it('should return true when successfully authenticated', async () => {
-      const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toBe(true);
+    it('should call through to verifyIdentity when available', async () => {
+      try {
+        await deviceAuthenticationProvider.triggerLockScreen();
+        expect(NativeBiometricMock.isAvailable).toHaveBeenCalledWith({ useFallback: true });
+        expect(NativeBiometricMock.verifyIdentity).toHaveBeenCalled();
+      } catch (err) {
+        console.error(err);
+      }
     });
 
-    it('should resolve immediately if the examiner role check passes', async () => {
+    it('should resolve to false if the examiner role check is checked and is a DLG user', async () => {
       spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
       const result = await deviceAuthenticationProvider.triggerLockScreen();
       expect(result).toBe(false);
     });
 
     it('should resolve to false if environment file has isTest set to true', async () => {
-      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
       (environment as unknown as TestersEnvironmentFile).isTest = true;
       const result = await deviceAuthenticationProvider.triggerLockScreen();
       expect(result).toEqual(false);
     });
 
-    it('should resolve to true if environment file has isTest set to true', async () => {
-      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+    it('should resolve to false if environment file has isTest set to true', async () => {
+      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
       (environment as unknown as TestersEnvironmentFile).isTest = false;
       const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toEqual(true);
+      expect(result).toEqual(false);
     });
   });
 
--- src/app/providers/device-authentication/device-authentication.ts
+++ src/app/providers/device-authentication/device-authentication.ts
@@ -1,32 +1,49 @@
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { TestersEnvironmentFile } from '@environments/models/environment.model';
+import { NativeBiometric } from 'capacitor-native-biometric';
+import { Platform } from '@ionic/angular';
 import { AppConfigProvider } from '../app-config/app-config';
 import { ExaminerRole } from '../app-config/constants/examiner-role.constants';
-import { NativeBiometric } from "capacitor-native-biometric";
 
 @Injectable()
 export class DeviceAuthenticationProvider {
 
   constructor(
+    private platform: Platform,
     public appConfig: AppConfigProvider,
   ) { }
 
-  async performBiometricVerification(): Promise<boolean> {
-    // fallback to passcode if biometric authentication unavailable
-    const result = await NativeBiometric.isAvailable({useFallback: true});
-    if(!result.isAvailable) throw new Error('biometrics not available');
-    return await NativeBiometric.verifyIdentity({
-      reason: "Please authenticate",
-      useFallback: true,
-    })
-  }
-
   triggerLockScreen = async (): Promise<boolean> => {
-    if (this.appConfig.getAppConfig().role === ExaminerRole.DLG
-        || (environment as unknown as TestersEnvironmentFile)?.isTest) {
-      return Promise.resolve(false);
+    try {
+      await this.platform.ready();
+
+      if (this.shouldBypassDeviceAuth()) {
+        return false;
+      }
+
+      return await this.performBiometricVerification();
+    } catch (err) {
+      throw new Error(err);
     }
-    return this.performBiometricVerification();
+  };
+
+  private shouldBypassDeviceAuth = (): boolean => {
+    return (
+      !this.platform.is('cordova')
+        || this.appConfig.getAppConfig()?.role === ExaminerRole.DLG
+        || (environment as unknown as TestersEnvironmentFile)?.isTest
+    );
+  };
+
+  private performBiometricVerification = async (): Promise<boolean> => {
+    // fallback to passcode if biometric authentication unavailable
+    const result = await NativeBiometric.isAvailable({ useFallback: true });
+    if (!result.isAvailable) throw new Error('Biometrics not available');
+
+    return NativeBiometric.verifyIdentity({
+      reason: 'Please authenticate',
+      useFallback: true,
+    });
   };
 }
