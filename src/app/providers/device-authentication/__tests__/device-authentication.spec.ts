3baceb0790e2d2024180aad8839259c90440cb79
Matthew Culliford <matthew.culliford@dvsa.gov.uk>
2022-11-03 11:48:22 +0000

    replace passcode / fingerprint plugin

--- src/app/providers/device-authentication/__tests__/device-authentication.spec.ts
+++ src/app/providers/device-authentication/__tests__/device-authentication.spec.ts
@@ -4,13 +4,19 @@ import { configureTestSuite } from 'ng-bullet';
 import { PlatformMock } from '@mocks/ionic-mocks/platform-mock';
 import { TestersEnvironmentFile } from '@environments/models/environment.model';
 import { environment } from '@environments/environment';
+import { NativeBiometricMock } from '@mocks/@capacitor/biometrics';
 import { DeviceAuthenticationProvider } from '../device-authentication';
 import { AppConfigProvider } from '../../app-config/app-config';
 import { AppConfigProviderMock } from '../../app-config/__mocks__/app-config.mock';
 import { AppConfig } from '../../app-config/app-config.model';
+import { LoadingProvider } from '@providers/loader/loader';
+import { DeviceProvider } from '@providers/device/device';
+import { DeviceProviderMock } from '@providers/device/__mocks__/device.mock';
 
 describe('DeviceAuthenticationProvider', () => {
   let deviceAuthenticationProvider: DeviceAuthenticationProvider;
+  let platform: Platform;
+  let loadingProvider: LoadingProvider;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -18,38 +24,62 @@ describe('DeviceAuthenticationProvider', () => {
         DeviceAuthenticationProvider,
         { provide: Platform, useClass: PlatformMock },
         { provide: AppConfigProvider, useClass: AppConfigProviderMock },
+        { provide: DeviceProvider, useClass: DeviceProviderMock },
+        LoadingProvider,
       ],
     });
   });
 
   beforeEach(() => {
     deviceAuthenticationProvider = TestBed.inject(DeviceAuthenticationProvider);
+    platform = TestBed.inject(Platform);
+    loadingProvider = TestBed.inject(LoadingProvider);
+
+    spyOn(loadingProvider, 'handleUILoading').and.returnValue(Promise.resolve());
+    spyOn(platform, 'ready').and.returnValue(Promise.resolve(''));
+    spyOn(platform, 'is').and.returnValue(true);
+    spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+    (environment as unknown as TestersEnvironmentFile).isTest = false;
+
+    spyOn(NativeBiometricMock, 'isAvailable');
+
+    (NativeBiometricMock.isAvailable as any)
+      .withArgs({ key: 'useFallback' })
+      .and.returnValue(Promise.resolve({ value: { available: true } }));
   });
 
   describe('triggerLockScreen', () => {
-    it('should return true when successfully authenticated', async () => {
-      const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toBe(true);
+    it('should call through to verifyIdentity when available', async () => {
+      try {
+        await deviceAuthenticationProvider.triggerLockScreen();
+        expect(NativeBiometricMock.isAvailable).toHaveBeenCalledWith({ useFallback: true });
+        expect(NativeBiometricMock.verifyIdentity).toHaveBeenCalled();
+      } catch (err) {
+        console.error(err);
+      }
     });
 
-    it('should resolve immediately if the examiner role check passes', async () => {
+    it('should not trigger lock screen if the examiner role check is checked and is a DLG user', async () => {
       spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
-      const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toBe(false);
+      spyOn(NativeBiometricMock, 'verifyIdentity');
+      await deviceAuthenticationProvider.triggerLockScreen();
+      expect(NativeBiometricMock.verifyIdentity).not.toHaveBeenCalled();
     });
 
-    it('should resolve to false if environment file has isTest set to true', async () => {
-      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+    it('should not trigger lock screen if environment file has isTest set to true', async () => {
+      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
       (environment as unknown as TestersEnvironmentFile).isTest = true;
-      const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toEqual(false);
+      spyOn(NativeBiometricMock, 'verifyIdentity');
+      await deviceAuthenticationProvider.triggerLockScreen();
+      expect(NativeBiometricMock.verifyIdentity).not.toHaveBeenCalled();
     });
 
-    it('should resolve to true if environment file has isTest set to true', async () => {
-      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DE' } as AppConfig);
+    it('should not trigger lock screen if environment file has isTest set to true', async () => {
+      spyOn(deviceAuthenticationProvider.appConfig, 'getAppConfig').and.returnValue({ role: 'DLG' } as AppConfig);
       (environment as unknown as TestersEnvironmentFile).isTest = false;
-      const result = await deviceAuthenticationProvider.triggerLockScreen();
-      expect(result).toEqual(true);
+      spyOn(NativeBiometricMock, 'verifyIdentity');
+      await deviceAuthenticationProvider.triggerLockScreen();
+      expect(NativeBiometricMock.verifyIdentity).not.toHaveBeenCalled();
     });
   });
 
--- src/app/providers/device-authentication/device-authentication.ts
+++ src/app/providers/device-authentication/device-authentication.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { environment } from '@environments/environment';
 import { TestersEnvironmentFile } from '@environments/models/environment.model';
+import { NativeBiometric } from 'capacitor-native-biometric';
+import { Platform } from '@ionic/angular';
+import { DeviceProvider } from '@providers/device/device';
+import { LoadingProvider } from '@providers/loader/loader';
 import { AppConfigProvider } from '../app-config/app-config';
 import { ExaminerRole } from '../app-config/constants/examiner-role.constants';
-
-declare let cordova: any;
 
 @Injectable()
 export class DeviceAuthenticationProvider {
@@ -13,37 +14,52 @@ export class DeviceAuthenticationProvider {
   constructor(
     private platform: Platform,
     public appConfig: AppConfigProvider,
+    private deviceProvider: DeviceProvider,
+    private loadingProvider: LoadingProvider,
   ) { }
 
-  triggerLockScreen = async (): Promise<boolean> => {
-    if (this.appConfig.getAppConfig().role === ExaminerRole.DLG
-        || (environment as unknown as TestersEnvironmentFile)?.isTest) {
-      return Promise.resolve(false);
+  triggerLockScreen = async (): Promise<void> => {
+    try {
+      await this.platform.ready();
+
+      if (this.shouldBypassDeviceAuth()) {
+        return;
+      }
+
+      return await this.performBiometricVerification();
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  private shouldBypassDeviceAuth = (): boolean => {
+    return (
+      !this.platform.is('cordova')
+        || this.appConfig.getAppConfig()?.role === ExaminerRole.DLG
+        || (environment as unknown as TestersEnvironmentFile)?.isTest
+    );
+  };
+
+  private performBiometricVerification = async (): Promise<void> => {
+    const deviceType: string = this.deviceProvider.getDeviceType();
+    const isASAMEnabled: boolean = await this.deviceProvider.checkSingleAppMode();
+    // handle bug caused by accessing touch id while ASAM enabled in iPad 8th only
+    const shouldToggleASAM: boolean = isASAMEnabled && this.deviceProvider.is8thGenDevice(deviceType);
+    if (shouldToggleASAM) {
+      await this.deviceProvider.disableSingleAppMode();
     }
 
-    return new Promise<boolean>((resolve, reject) => {
-
-      this.platform.ready().then(() => {
-
-        if (!this.platform.is('cordova')) {
-          return resolve(true);
-        }
-        if (cordova && cordova.plugins && cordova.plugins.DeviceAuthentication) {
-
-          cordova.plugins.DeviceAuthentication.runAuthentication(
-            'Please enter your passcode',
-            (successful: boolean) => {
-              return successful ? resolve(true) : reject(new Error('device authentication failed'));
-            },
-            (error) => {
-              return reject(new Error(error));
-            },
-          );
-
-        } else {
-          return reject(new Error('no device authentication plugin found'));
-        }
+    try {
+      await NativeBiometric.verifyIdentity({
+        reason: 'Please authenticate',
+        useFallback: true, // fallback to passcode if biometric authentication unavailable
       });
-    });
+    } finally {
+      if (shouldToggleASAM) {
+        await this.loadingProvider.handleUILoading(true);
+        await this.deviceProvider.enableSingleAppMode();
+        await this.loadingProvider.handleUILoading(false);
+      }
+    }
   };
 }
