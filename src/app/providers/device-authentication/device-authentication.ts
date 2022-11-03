424e0834e90da2699538be8a1915277a5169861e
Matthew Culliford <matthew.culliford@dvsa.gov.uk>
2022-11-03 11:48:22 +0000

    replace passcode / fingerprint plugin

--- src/app/providers/device-authentication/device-authentication.ts
+++ src/app/providers/device-authentication/device-authentication.ts
@@ -1,49 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Platform } from '@ionic/angular';
 import { environment } from '@environments/environment';
 import { TestersEnvironmentFile } from '@environments/models/environment.model';
 import { AppConfigProvider } from '../app-config/app-config';
 import { ExaminerRole } from '../app-config/constants/examiner-role.constants';
-
-declare let cordova: any;
+import { NativeBiometric } from "capacitor-native-biometric";
 
 @Injectable()
 export class DeviceAuthenticationProvider {
 
   constructor(
-    private platform: Platform,
     public appConfig: AppConfigProvider,
   ) { }
+
+  async performBiometricVerification(): Promise<boolean> {
+    // fallback to passcode if biometric authentication unavailable
+    const result = await NativeBiometric.isAvailable({useFallback: true});
+    if(!result.isAvailable) throw new Error('biometrics not available');
+    return await NativeBiometric.verifyIdentity({
+      reason: "Please authenticate",
+      useFallback: true,
+    })
+  }
 
   triggerLockScreen = async (): Promise<boolean> => {
     if (this.appConfig.getAppConfig().role === ExaminerRole.DLG
         || (environment as unknown as TestersEnvironmentFile)?.isTest) {
       return Promise.resolve(false);
     }
-
-    return new Promise<boolean>((resolve, reject) => {
-
-      this.platform.ready().then(() => {
-
-        if (!this.platform.is('cordova')) {
-          return resolve(true);
-        }
-        if (cordova && cordova.plugins && cordova.plugins.DeviceAuthentication) {
-
-          cordova.plugins.DeviceAuthentication.runAuthentication(
-            'Please enter your passcode',
-            (successful: boolean) => {
-              return successful ? resolve(true) : reject(new Error('device authentication failed'));
-            },
-            (error) => {
-              return reject(new Error(error));
-            },
-          );
-
-        } else {
-          return reject(new Error('no device authentication plugin found'));
-        }
-      });
-    });
+    return this.performBiometricVerification();
   };
 }
