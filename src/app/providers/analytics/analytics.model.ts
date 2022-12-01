55885ccf91bb8653cfb1d70e544f3ff4bd5cefbe
andrewsetterfield <andrew.setterfield@dvsa.gov.uk>
2022-12-01 11:43:17 +0000

    added analytics

--- src/app/providers/analytics/analytics.model.ts
+++ src/app/providers/analytics/analytics.model.ts
@@ -137,6 +137,7 @@ export enum AnalyticsEvents {
   TOGGLE_LICENSE_RECEIVED = 'toggle licence received',
   D255 = 'set D255',
   GEARBOX_CATEGORY_CHANGED = 'set transmission',
+  MOT_STATUS_CHANGED = 'set mot status',
   LANGUAGE_CHANGED = 'language changed',
   CBT_CHANGED = 'cbt changed',
   BIKE_CATEGORY_CHANGED = 'bike category changed',
