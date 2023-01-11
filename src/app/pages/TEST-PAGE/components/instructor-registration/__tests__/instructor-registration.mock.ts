cd0478092343f3a3e84d925293c499e1f0eb6181
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-01-11 14:03:19 +0000

    WIP: testing dark mode functionality

---
+++ src/app/pages/TEST-PAGE/components/instructor-registration/__tests__/instructor-registration.mock.ts
@@ -0,0 +1,29 @@
+export const mockValidInstructorRegistrationNumber = {
+  target: {
+    value: '1234567',
+  },
+};
+
+export const mockInvalidInstructorRegistrationNumber = {
+  target: {
+    value: '12£45H7',
+  },
+};
+
+export const mockLeadingZeroRegistrationNumber = {
+  target: {
+    value: '04567',
+  },
+};
+
+export const mockOnlyZeroRegistrationNumber = {
+  target: {
+    value: '0',
+  },
+};
+
+export const mockBlankInstructorRegistrationNumber = {
+  target: {
+    value: '',
+  },
+};
---
+++ src/app/pages/TEST-PAGE/components/tell-me-question-outcome/tell-me-question-outcome.ts
@@ -0,0 +1,44 @@
+import {
+  Component, Input, Output, EventEmitter, OnChanges,
+} from '@angular/core';
+import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'tell-me-question-outcome-TEST',
+  templateUrl: 'tell-me-question-outcome.html',
+})
+export class TellMeQuestionOutcomeComponent implements OnChanges {
+
+  @Input()
+  tellMeQuestionOutcome: string;
+
+  @Input()
+  formGroup: UntypedFormGroup;
+
+  @Input()
+  tellMeQuestionSelected: boolean;
+
+  @Output()
+  tellMeQuestionOutcomeChange = new EventEmitter<string>();
+
+  formControl: UntypedFormControl;
+
+  ngOnChanges(): void {
+    if (!this.formControl) {
+      this.formControl = new UntypedFormControl('', [Validators.required]);
+      this.formGroup.addControl('tellMeQuestionOutcome', this.formControl);
+    }
+    this.formControl.patchValue(this.tellMeQuestionOutcome);
+  }
+
+  tellMeQuestionOutcomeChanged(questionOutcome: string): void {
+    if (this.formControl.valid) {
+      this.tellMeQuestionOutcomeChange.emit(questionOutcome);
+    }
+  }
+
+  get invalid(): boolean {
+    return !this.formControl.valid && this.formControl.dirty;
+  }
+
+}
