0a274bbce54a0dd5187df5e036fd94f4fd9ca234
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-03-02 10:32:48 +0000

    WIP: Unit Tests

--- src/app/providers/question/__tests__/question.spec.ts
+++ src/app/providers/question/__tests__/question.spec.ts
@@ -15,6 +15,14 @@ import showMeQuestionsVocationalTrailerConstants
   from '@shared/constants/show-me-questions/show-me-questions.vocational-trailer.constants';
 import safetyQuestionsCatDConstants
   from '@shared/constants/safety-questions.cat-d.constants';
+import tellMeQuestionsCatHomeTestConstants
+  from '@shared/constants/tell-me-questions/tell-me-questions.cat-home-test.constants';
+import showMeQuestionsCatHomeTestConstants
+  from '@shared/constants/show-me-questions/show-me-questions.cat-home-test.constants';
+import showMeQuestionsCatAdiPart2Constants
+  from '@shared/constants/show-me-questions/show-me-questions.cat-adi-part2.constants';
+import balanceQuestionsCatAMod2Constants from '@shared/constants/balance-questions.cat-a-mod2.constants';
+import safetyQuestionsCatAMod2Constants from '@shared/constants/safety-questions.cat-a-mod2.constants';
 import { QuestionProvider } from '../question';
 
 describe('question provider', () => {
@@ -58,6 +66,18 @@ describe('question provider', () => {
     it('should return the correct questions for a category D1E test', () => {
       expect(questionProvider.getTellMeQuestions(TestCategory.D1E)).toEqual(tellMeQuestionsVocationalTrailerConstants);
     });
+    it('should return the correct questions for a category K test', () => {
+      expect(questionProvider.getTellMeQuestions(TestCategory.K)).toEqual(tellMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category F test', () => {
+      expect(questionProvider.getTellMeQuestions(TestCategory.F)).toEqual(tellMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category G test', () => {
+      expect(questionProvider.getTellMeQuestions(TestCategory.G)).toEqual(tellMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category H test', () => {
+      expect(questionProvider.getTellMeQuestions(TestCategory.H)).toEqual(tellMeQuestionsCatHomeTestConstants);
+    });
     it('should return no questions for a non-supported category', () => {
       expect(questionProvider.getTellMeQuestions(TestCategory.B1)).toEqual([]);
     });
@@ -91,8 +111,59 @@ describe('question provider', () => {
     it('should return the correct questions for a category D1E test', () => {
       expect(questionProvider.getShowMeQuestions(TestCategory.D1E)).toEqual(showMeQuestionsVocationalTrailerConstants);
     });
+    it('should return the correct questions for a category K test', () => {
+      expect(questionProvider.getShowMeQuestions(TestCategory.K)).toEqual(showMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category F test', () => {
+      expect(questionProvider.getShowMeQuestions(TestCategory.F)).toEqual(showMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category G test', () => {
+      expect(questionProvider.getShowMeQuestions(TestCategory.G)).toEqual(showMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category H test', () => {
+      expect(questionProvider.getShowMeQuestions(TestCategory.H)).toEqual(showMeQuestionsCatHomeTestConstants);
+    });
+    it('should return the correct questions for a category ADI2 test', () => {
+      expect(questionProvider.getShowMeQuestions(TestCategory.ADI2)).toEqual(showMeQuestionsCatAdiPart2Constants);
+    });
     it('should return no questions for a non-supported category', () => {
       expect(questionProvider.getShowMeQuestions(TestCategory.B1)).toEqual([]);
+    });
+  });
+
+  describe('getSafetyQuestions', () => {
+    it('should return the correct questions for a category EUA1M2 test', () => {
+      expect(questionProvider.getSafetyQuestions(TestCategory.EUA1M2)).toEqual(safetyQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUA2M2 test', () => {
+      expect(questionProvider.getSafetyQuestions(TestCategory.EUA2M2)).toEqual(safetyQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUA2M2 test', () => {
+      expect(questionProvider.getSafetyQuestions(TestCategory.EUAM2)).toEqual(safetyQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUAMM2 test', () => {
+      expect(questionProvider.getSafetyQuestions(TestCategory.EUAMM2)).toEqual(safetyQuestionsCatAMod2Constants);
+    });
+    it('should return the an empty array for an invalid test', () => {
+      expect(questionProvider.getSafetyQuestions(TestCategory.B1)).toEqual([]);
+    });
+  });
+
+  describe('getBalanceQuestions', () => {
+    it('should return the correct questions for a category EUA1M2 test', () => {
+      expect(questionProvider.getBalanceQuestions(TestCategory.EUA1M2)).toEqual(balanceQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUA2M2 test', () => {
+      expect(questionProvider.getBalanceQuestions(TestCategory.EUA2M2)).toEqual(balanceQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUA2M2 test', () => {
+      expect(questionProvider.getBalanceQuestions(TestCategory.EUAM2)).toEqual(balanceQuestionsCatAMod2Constants);
+    });
+    it('should return the correct questions for a category EUAMM2 test', () => {
+      expect(questionProvider.getBalanceQuestions(TestCategory.EUAMM2)).toEqual(balanceQuestionsCatAMod2Constants);
+    });
+    it('should return the an empty array for an invalid test', () => {
+      expect(questionProvider.getBalanceQuestions(TestCategory.B1)).toEqual([]);
     });
   });
 
