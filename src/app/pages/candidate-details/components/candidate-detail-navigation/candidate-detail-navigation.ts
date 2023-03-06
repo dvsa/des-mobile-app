a6b7a9707a56c919be3d712405cccd1a79770ac2
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-03-06 12:27:37 +0000

    Added candidate details navigation

---
+++ src/app/pages/candidate-details/components/candidate-detail-navigation/candidate-detail-navigation.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'candidate-detail-navigation',
+  templateUrl: 'candidate-detail-navigation.html',
+  styleUrls: ['candidate-detail-navigation.scss'],
+})
+export class CandidateDetailNavigationComponent {
+
+  @Input()
+  name: string;
+  @Input()
+  date: string;
+  @Input()
+  time: string;
+  @Input()
+  canNavigateToPreviousCandidate: boolean;
+  @Input()
+  canNavigateToNextCandidate: boolean;
+
+  @Output()
+  previousCandidateClicked = new EventEmitter();
+  @Output()
+  nextCandidateClicked = new EventEmitter();
+
+  onPreviousCandidateClick(): void {
+    this.previousCandidateClicked.emit();
+  }
+
+  onNextCandidateClick(): void {
+    this.nextCandidateClicked.emit();
+  }
+}
--- src/app/pages/candidate-details/components/candidate-details-components.module.ts
+++ src/app/pages/candidate-details/components/candidate-details-components.module.ts
@@ -4,18 +4,25 @@ import {
 } from '@components/common/inappropriate-use-banner/inappropriate-use-banner';
 import { IonicModule } from '@ionic/angular';
 import { FitMarkerBannerComponent } from '@components/common/fit-marker-banner/fit-marker-banner';
+import {
+  CandidateDetailNavigationComponent,
+} from '@pages/candidate-details/components/candidate-detail-navigation/candidate-detail-navigation';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     InappropriateUseBannerComponent,
     FitMarkerBannerComponent,
+    CandidateDetailNavigationComponent,
   ],
   imports: [
     IonicModule,
+    CommonModule,
   ],
   exports: [
     InappropriateUseBannerComponent,
     FitMarkerBannerComponent,
+    CandidateDetailNavigationComponent,
   ],
 })
 export class CandidateDetailsComponentsModule { }
