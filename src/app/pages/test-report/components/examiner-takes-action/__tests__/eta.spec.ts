ca763268eff7d89ec5f2164467eb802d2bf50fb0
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-03-30 13:55:49 +0100

    Unit tests

--- src/app/pages/test-report/components/examiner-takes-action/__tests__/eta.spec.ts
+++ src/app/pages/test-report/components/examiner-takes-action/__tests__/eta.spec.ts
@@ -6,10 +6,26 @@ import { StoreModel } from '@shared/models/store.model';
 import { ToggleETA } from '@store/tests/test-data/common/eta/eta.actions';
 import { ExaminerActions } from '@store/tests/test-data/test-data.constants';
 import { MockComponent } from 'ng-mocks';
+import { provideMockStore } from '@ngrx/store/testing';
+import { TestData } from '@dvsa/mes-test-schema/categories/common';
 import { CompetencyButtonComponent } from '../../competency-button/competency-button';
 import { EtaComponent } from '../eta';
 
 describe('EtaComponent', () => {
+
+  const initialState = {
+    tests: {
+      currentTest: {
+        slotId: '123',
+      },
+      startedTests: {
+        123: {
+          testData: { ETA: { physical: true, verbal: false } },
+        },
+      },
+    },
+  } as TestData;
+
   let fixture: ComponentFixture<EtaComponent>;
   let component: EtaComponent;
   let store$: Store<StoreModel>;
@@ -23,6 +39,9 @@ describe('EtaComponent', () => {
       imports: [
         IonicModule,
         StoreModule.forRoot({ tests: testsReducer }),
+      ],
+      providers: [
+        provideMockStore({ initialState }),
       ],
     });
 
@@ -43,6 +62,24 @@ describe('EtaComponent', () => {
       component.eta = ExaminerActions.physical;
       component.toggleETA();
       expect(store$.dispatch).toHaveBeenCalledWith(ToggleETA(ExaminerActions.physical));
+    });
+  });
+  describe('ngOnInit', () => {
+    it('should resolve state variables', () => {
+      component.eta = ExaminerActions.physical;
+      component.ngOnInit();
+
+      component.componentState.actionTaken$.subscribe((val) => {
+        expect(val).toEqual(true);
+      });
+    });
+  });
+  describe('toggleETA', () => {
+    it('should dispatch ToggleETA with eta', () => {
+      component.eta = ExaminerActions.physical;
+      component.toggleETA();
+
+      expect(component['store$'].dispatch).toHaveBeenCalledWith(ToggleETA(ExaminerActions.physical));
     });
   });
 });
