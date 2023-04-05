032fffe55f6227f88b8f9a565d2655cfc562e663
Matthew Bell <mbell.dvsa@icloud.com>
2023-04-05 09:20:53 +0100

    Update sonar update script

--- buildScripts/sonar-properties-update.sh
+++ buildScripts/sonar-properties-update.sh
@@ -10,4 +10,4 @@ echo "
 APP_VERSION=$(xmllint -xpath 'string(//*[local-name()="widget"]/@version)' config.xml)
 
 # Update the project version by replacing the <version> with the value inside config.xml
-sed -i "" "s/<version>/${APP_VERSION}/g" sonar-project.properties
+sed -i "" "s/^sonar.projectVersion=.*/sonar.projectVersion=${APP_VERSION}/" sonar-project.properties
