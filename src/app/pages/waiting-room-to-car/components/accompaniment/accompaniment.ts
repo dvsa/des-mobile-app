b08949ebadf4f02d54a4387c702c05c120d33aa9
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-05-05 09:37:52 +0100

    Keeping up to date

--- src/app/pages/waiting-room-to-car/components/accompaniment/accompaniment.ts
+++ src/app/pages/waiting-room-to-car/components/accompaniment/accompaniment.ts
@@ -22,8 +22,6 @@ export class AccompanimentComponent implements OnChanges {
   accompanimentChange = new EventEmitter();
 
   formControl: UntypedFormControl;
-  @Input() style?: number;
-
   ngOnChanges(): void {
     if (!this.formControl) {
       this.formControl = new UntypedFormControl(null);
