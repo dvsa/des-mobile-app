4fd1960bf7dacc8b023cce9b7e5965e987accd15
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-05-18 08:21:45 +0100

    WIP: Replaced more headers with new component

--- src/components/common/terminate-test-modal/terminate-test-modal.module.ts
+++ src/components/common/terminate-test-modal/terminate-test-modal.module.ts
@@ -3,11 +3,13 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 
 import { TerminateTestModal } from '@components/common/terminate-test-modal/terminate-test-modal';
+import { HeaderComponentModule } from '@components/common/header-component/header-component.module';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    HeaderComponentModule,
   ],
   declarations: [TerminateTestModal],
 })
