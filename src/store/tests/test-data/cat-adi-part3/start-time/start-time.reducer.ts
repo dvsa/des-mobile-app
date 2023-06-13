0db3453228d8bfad8f114bca4c3f09627a41cbdd
Matthew Bell <mbell.dvsa@icloud.com>
2023-06-13 14:56:37 +0100

    Hydrate store with start/end time

--- src/store/tests/test-data/cat-adi-part3/start-time/start-time.reducer.ts
+++ src/store/tests/test-data/cat-adi-part3/start-time/start-time.reducer.ts
@@ -1,9 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
+import * as moment from 'moment';
 import * as startTimeActionTypes from './start-time.actions';
 
 const initialState: string = null;
 
+const timeFormat = 'YYYY-MM-DDTHH:mm';
+
 export const startTimeReducer = createReducer(
   initialState,
-  on(startTimeActionTypes.StartTimeChanged, (_, { time }) => time),
+  on(startTimeActionTypes.StartTimeChanged, (_, { time }) => moment(time)
+    .format(timeFormat)),
 );
