fed841c383a837272150681b82d30b7a91bf5ad6
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-06-15 08:45:34 +0100

    Fixed test

--- src/app/pages/pass-certificates/__tests__/pass-certificates.page.spec.ts
+++ src/app/pages/pass-certificates/__tests__/pass-certificates.page.spec.ts
@@ -1,24 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { Store } from '@ngrx/store';
-import { MockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { PassCertificatesPage } from '../pass-certificates.page';
 
 describe('PassCertificatesPage', () => {
   let component: PassCertificatesPage;
   let fixture: ComponentFixture<PassCertificatesPage>;
+  let store$: MockStore;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PassCertificatesPage],
       providers: [
         { provide: Store, useClass: MockStore },
+        provideMockStore({ }),
       ],
     });
     fixture = TestBed.createComponent(PassCertificatesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    store$ = TestBed.inject(MockStore);
   }));
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(store$).toBeTruthy();
   });
 });
