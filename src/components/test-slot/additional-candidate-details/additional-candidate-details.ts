2a4e50e07bbcbe94ec244694ba3b6bdfbc41e841
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-07-25 15:37:32 +0100

    Removed attempt number for completed tests

--- src/components/test-slot/additional-candidate-details/additional-candidate-details.ts
+++ src/components/test-slot/additional-candidate-details/additional-candidate-details.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TestStatus } from '@store/tests/test-status/test-status.model';
 
 @Component({
   selector: 'additional-candidate-details',
@@ -10,5 +11,12 @@ export class AdditionalCandidateDetailsComponent {
   prn?: number = 0;
 
   @Input()
+  testStatus: TestStatus;
+
+  @Input()
   attempts?: number = 0;
+
+  isCompleted(): boolean {
+    return [TestStatus.Completed, TestStatus.Submitted].includes(this.testStatus);
+  }
 }
