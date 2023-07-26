981b04ec2c61808e3e34ac75c5d0c6b61cee277e
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2023-07-26 08:19:02 +0100

    update terminology of test slots (#1262)

--- src/app/shared/models/slot-types.ts
+++ src/app/shared/models/slot-types.ts
@@ -1,7 +1,7 @@
 export enum SlotTypes {
   STANDARD_TEST = 'Standard Test',
-  SINGLE_SLOT_SPECIAL_NEEDS = 'Single Slot (Special Needs)',
+  SINGLE_SLOT_SPECIAL_NEEDS = 'Single Slot (Additional Needs)',
   EXTENDED_TEST = 'Extended Test',
-  EXTENDED_TEST_SPECIAL_NEEDS = 'Extended Test Special Needs',
+  EXTENDED_TEST_SPECIAL_NEEDS = 'Extended Test Additional Needs',
   SPECIAL_NEEDS_EXTRA_TIME = 'Extra Time Needed',
 }
