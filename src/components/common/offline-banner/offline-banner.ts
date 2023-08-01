74fed1fa38477b24dc8eab62cfe35b5dc700d06a
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-08-01 15:41:50 +0100

    Updated offline banner to utilise logic more in line with the rest of the app.

--- src/components/common/offline-banner/offline-banner.ts
+++ src/components/common/offline-banner/offline-banner.ts
@@ -1,19 +1,22 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component, Input, OnChanges, SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'offline-banner',
   templateUrl: 'offline-banner.html',
   styleUrls: ['offline-banner.scss'],
 })
-export class OfflineBannerComponent {
+export class OfflineBannerComponent implements OnChanges {
 
   @Input()
   isOffline: boolean;
 
   hasBeenOffline: boolean = false;
 
-  hasBeenOfflineSet() {
-    this.hasBeenOffline = true;
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.isOffline?.currentValue === true) {
+      this.hasBeenOffline = true;
+    }
   }
-
 }
