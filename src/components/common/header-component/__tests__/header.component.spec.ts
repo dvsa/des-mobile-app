80eefef51968df12c1a7df9ca8644de687f3bcf5
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-08-08 15:31:51 +0100

    Fixed tests

--- src/components/common/header-component/__tests__/header.component.spec.ts
+++ src/components/common/header-component/__tests__/header.component.spec.ts
@@ -1,30 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { AppComponent } from '@app/app.component';
-import { provideMockStore } from '@ngrx/store/testing';
-import { SlotProvider } from '@providers/slot/slot';
-import { SlotProviderMock } from '@providers/slot/__mocks__/slot.mock';
-import { DeviceProviderMock } from '@providers/device/__mocks__/device.mock';
-import { DateTimeProvider } from '@providers/date-time/date-time';
-import { DateTimeProviderMock } from '@providers/date-time/__mocks__/date-time.mock';
-import { AuthenticationProvider } from '@providers/authentication/authentication';
-import { AuthenticationProviderMock } from '@providers/authentication/__mocks__/authentication.mock';
-import { SecureStorage } from '@awesome-cordova-plugins/secure-storage/ngx';
-import { SecureStorageMock } from '@mocks/ionic-mocks/secure-storage.mock';
-import { DataStoreProvider } from '@providers/data-store/data-store';
-import { DataStoreProviderMock } from '@providers/data-store/__mocks__/data-store.mock';
-import { NetworkStateProvider } from '@providers/network-state/network-state';
-import { NetworkStateProviderMock } from '@providers/network-state/__mocks__/network-state.mock';
-import { TranslateService } from '@ngx-translate/core';
-import { translateServiceMock } from '@shared/helpers/__mocks__/translate.mock';
-import { AppInfoProvider } from '@providers/app-info/app-info';
-import { AppInfoProviderMock } from '@providers/app-info/__mocks__/app-info.mock';
-import { AppConfigProvider } from '@providers/app-config/app-config';
-import { AppConfigProviderMock } from '@providers/app-config/__mocks__/app-config.mock';
-import { DeviceProvider } from '@providers/device/device';
 import { AccessibilityService } from '@providers/accessibility/accessibility.service';
 import { AccessibilityServiceMock } from '@providers/accessibility/__mocks__/accessibility-service.mock';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header.component';
 
 describe('HeaderComponent', () => {
@@ -34,21 +13,9 @@ describe('HeaderComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      imports: [IonicModule.forRoot()],
+      imports: [IonicModule.forRoot(), CommonModule],
       providers: [
-        { provide: AppComponent, useClass: AppComponent },
-        { provide: SlotProvider, useClass: SlotProviderMock },
-        { provide: DateTimeProvider, useClass: DateTimeProviderMock },
-        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
-        { provide: SecureStorage, useClass: SecureStorageMock },
-        { provide: DataStoreProvider, useClass: DataStoreProviderMock },
-        { provide: NetworkStateProvider, useClass: NetworkStateProviderMock },
-        { provide: TranslateService, useValue: translateServiceMock },
-        { provide: AppInfoProvider, useClass: AppInfoProviderMock },
-        { provide: AppConfigProvider, useClass: AppConfigProviderMock },
-        { provide: DeviceProvider, useClass: DeviceProviderMock },
         { provide: AccessibilityService, useClass: AccessibilityServiceMock },
-        provideMockStore({}),
       ],
     }).compileComponents();
 
