0b2548b6968bce30b16d82e46d35d58df53fd1a0
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2023-08-15 15:47:44 +0100

    re-add provider to test

--- src/app/pages/candidate-details/__tests__/candidate-details.page.spec.ts
+++ src/app/pages/candidate-details/__tests__/candidate-details.page.spec.ts
@@ -31,6 +31,8 @@ import { JournalModel } from '@store/journal/journal.model';
 import { StoreModel } from '@shared/models/store.model';
 import { TestStatus } from '@store/tests/test-status/test-status.model';
 import { SlotProvider } from '@providers/slot/slot';
+import { AppConfigProvider } from '@providers/app-config/app-config';
+import { AppConfigProviderMock } from '@providers/app-config/__mocks__/app-config.mock';
 import { CandidateDetailsPage } from '../candidate-details.page';
 
 describe('CandidateDetailsPage', () => {
@@ -99,6 +101,7 @@ describe('CandidateDetailsPage', () => {
         { provide: NavParams, useValue: mockNavParams },
         { provide: Router, useClass: RouterMock },
         { provide: SlotProvider, useClass: SlotProvider },
+        { provide: AppConfigProvider, useClass: AppConfigProviderMock },
         provideMockStore({ initialState }),
       ],
     });
