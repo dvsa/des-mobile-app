df151504ff3dbb13ee13f4eba540fd7f03b0cc35
Rhys Lewis <88314925+RLCorp@users.noreply.github.com>
2023-08-22 16:26:40 +0100

    Journal header (#1324)
    * Added correct headers to Journal and its supporting components
    
    * Fixed tests
    
    * Fixed button issues

--- src/components/test-slot/language/__tests__/language.spec.ts
+++ src/components/test-slot/language/__tests__/language.spec.ts
@@ -38,14 +38,14 @@ describe('LanguageComponent', () => {
       it('should render text when the language is Welsh', () => {
         component.welshLanguage = true;
         fixture.detectChanges();
-        const renderedText = fixture.debugElement.query(By.css('h6.language-padding'))
+        const renderedText = fixture.debugElement.query(By.css('ion-text.language-padding'))
           .nativeElement;
         expect(renderedText.textContent).toBe('Cymraeg');
       });
       it('should not render text when the language is not Welsh', () => {
         component.welshLanguage = false;
         fixture.detectChanges();
-        const renderedText = fixture.debugElement.queryAll(By.css('h6.language-padding'));
+        const renderedText = fixture.debugElement.queryAll(By.css('ion-text.language-padding'));
         expect(renderedText.length).toBe(0);
       });
     });
