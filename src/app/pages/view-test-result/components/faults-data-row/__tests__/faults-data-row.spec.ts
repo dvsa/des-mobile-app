27c2c180db72bf4536d566cd16826e4d68067f32
Matthew Bell <mbell.dvsa@icloud.com>
2023-09-15 11:35:59 +0100

    Update specs to use TestBed

--- src/app/pages/view-test-result/components/faults-data-row/__tests__/faults-data-row.spec.ts
+++ src/app/pages/view-test-result/components/faults-data-row/__tests__/faults-data-row.spec.ts
@@ -1,16 +1,42 @@
 import { FaultSummary } from '@shared/models/fault-marking.model';
 import { FaultsDataRowComponent } from '../faults-data-row';
 import { AccessibilityService } from '@providers/accessibility/accessibility.service';
-import { MobileAccessibility } from '@awesome-cordova-plugins/mobile-accessibility/ngx';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from '@components/common/common-components.module';
+import { DangerousFaultBadgeComponent } from '@components/common/dangerous-fault-badge/dangerous-fault-badge';
+import { SeriousFaultBadgeComponent } from '@components/common/serious-fault-badge/serious-fault-badge';
+import { DrivingFaultsBadgeComponent } from '@components/common/driving-faults-badge/driving-faults-badge';
+import { AccessibilityServiceMock } from '@providers/accessibility/__mocks__/accessibility-service.mock';
 
 describe('FaultsDataRowComponent', () => {
-  describe('defaultSettings', () => {
-    it('should have 15 as minDrivingFaultCount when the component is created', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
+  let fixture: ComponentFixture<FaultsDataRowComponent>;
+  let component: FaultsDataRowComponent;
 
-      expect(component.minDrivingFaultCount).toBe(15);
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        FaultsDataRowComponent,
+        MockComponent(DangerousFaultBadgeComponent),
+        MockComponent(SeriousFaultBadgeComponent),
+        MockComponent(DrivingFaultsBadgeComponent),
+      ],
+      imports: [
+        IonicModule,
+        ComponentsModule,
+      ],
+      providers: [
+        {
+          provide: AccessibilityService,
+          useClass: AccessibilityServiceMock,
+        },
+      ],
     });
-  });
+
+    fixture = TestBed.createComponent(FaultsDataRowComponent);
+    component = fixture.componentInstance;
+  }));
 
   describe('showFaultComment', () => {
     const faultSummary: FaultSummary = {
@@ -21,25 +47,23 @@ describe('FaultsDataRowComponent', () => {
     };
 
     it('should return false when drivingFaultCount is less than minDrivingFaultCount', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
-
       component.drivingFaultCount = 5;
       component.minDrivingFaultCount = 6;
 
       const result = component.showFaultComment(faultSummary);
 
-      expect(result).toBe(false);
+      expect(result)
+        .toBe(false);
     });
 
     it('should return false when drivingFaultCount is equal to minDrivingFaultCount', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
-
       component.drivingFaultCount = 6;
       component.minDrivingFaultCount = 6;
 
       const result = component.showFaultComment(faultSummary);
 
-      expect(result).toBe(false);
+      expect(result)
+        .toBe(false);
     });
 
     it('should return false when there are no comments', () => {
@@ -47,37 +71,36 @@ describe('FaultsDataRowComponent', () => {
         ...faultSummary,
         comment: undefined,
       };
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
 
       component.drivingFaultCount = 7;
       component.minDrivingFaultCount = 6;
 
       const result = component.showFaultComment(localFaultSummary);
 
-      expect(result).toBe(false);
+      expect(result)
+        .toBe(false);
     });
 
     it('should return true when drivingFaultCount is greater than minDrivingFaultCount and has comments', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
-
       component.drivingFaultCount = 7;
       component.minDrivingFaultCount = 6;
 
       const result = component.showFaultComment(faultSummary);
 
-      expect(result).toBe(true);
+      expect(result)
+        .toBe(true);
     });
   });
 
   describe('getDriverType', () => {
     it('should return riding when isRider is true', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
-      expect(component.getDriverType(true)).toBe('riding');
+      expect(component.getDriverType(true))
+        .toBe('riding');
     });
 
     it('should return driving when isRider is false', () => {
-      const component = new FaultsDataRowComponent(new AccessibilityService(new MobileAccessibility()));
-      expect(component.getDriverType(false)).toBe('driving');
+      expect(component.getDriverType(false))
+        .toBe('driving');
     });
   });
 });
