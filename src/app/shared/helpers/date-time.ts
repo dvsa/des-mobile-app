a5c1bda3289baba72ea3797bca5c5895e90d0375
Matthew Bell <mbell.dvsa@icloud.com>
2023-10-19 07:25:58 +0100

    Add date range logic

--- src/app/shared/helpers/date-time.ts
+++ src/app/shared/helpers/date-time.ts
@@ -7,6 +7,8 @@ export enum Duration {
   SECOND = 'second',
 }
 
+export type DateRange = 'today' | 'week' | 'fortnight';
+
 export class DateTime {
   moment: moment.Moment;
 
@@ -68,6 +70,27 @@ export class DateTime {
   isBefore(targetDate: DateTime | string | Date): boolean {
     const date = new DateTime(targetDate);
     return date.moment.diff(this.moment, Duration.SECOND) > 0;
+  }
+
+  isDuring(range: DateRange) {
+    let dateRange: moment.Moment = null;
+
+    switch (range) {
+      case 'today':
+        dateRange = moment(new Date())
+          .subtract(1, 'day');
+        break;
+      case 'week':
+        dateRange = moment(new Date())
+          .subtract(1, 'week');
+        break;
+      case 'fortnight':
+        dateRange = moment(new Date())
+          .subtract(2, 'week');
+        break;
+    }
+
+    return this.moment.isSameOrAfter(dateRange);
   }
 
   static today(): Date {
