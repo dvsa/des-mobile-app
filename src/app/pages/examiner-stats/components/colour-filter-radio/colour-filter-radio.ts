0db63d2d35eab0ec4fc3db87c8ed9ccfbec6cdaf
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-10-23 16:20:04 +0100

    WIP: Added 3rd colour scheme and data label keys

--- src/app/pages/examiner-stats/components/colour-filter-radio/colour-filter-radio.ts
+++ src/app/pages/examiner-stats/components/colour-filter-radio/colour-filter-radio.ts
@@ -1,30 +1,30 @@
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-import { FilterEnum } from '@pages/examiner-stats/examiner-stats.page';
+import { ColourEnum } from '@pages/examiner-stats/examiner-stats.page';
 
 @Component({
-  selector: 'view-filter-radio',
-  templateUrl: 'view-filter-radio.html',
+  selector: 'colour-filter-radio',
+  templateUrl: 'colour-filter-radio.html',
 })
-export class ViewFilterRadioComponent implements OnChanges {
+export class ColourFilterRadioComponent implements OnChanges {
 
   @Input()
   formGroup: UntypedFormGroup;
 
   @Output()
-  filterChange = new EventEmitter<FilterEnum>();
+  filterChange = new EventEmitter<ColourEnum>();
 
   public formControl: UntypedFormControl;
 
   ngOnChanges(): void {
     if (!this.formControl) {
       this.formControl = new UntypedFormControl('Both');
-      this.formGroup.addControl('displayFilter', this.formControl);
+      this.formGroup.addControl('colourFilter', this.formControl);
     }
-    this.formControl.patchValue('Both');
+    this.formControl.patchValue('Default');
   }
 
-  viewFilterChanged(viewFilter: FilterEnum): void {
+  viewFilterChanged(viewFilter: ColourEnum): void {
     if (this.formControl.valid) {
       this.filterChange.emit(viewFilter);
     }
