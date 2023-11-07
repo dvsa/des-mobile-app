3f1a60f54fc9e36b4c92cbe53378a3d52e7cb0ee
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-11-07 15:48:35 +0000

    WIP: redesigned page and added partial test coverage and new colour schemes

--- src/components/common/data-grid/__tests__/data-grid.spec.ts
+++ src/components/common/data-grid/__tests__/data-grid.spec.ts
@@ -1,116 +1,88 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { IonicModule } from '@ionic/angular';
-//
-// import { AppModule } from '@app/app.module';
-// import { UntypedFormGroup, Validators } from '@angular/forms';
-// import { ActivityCodeModel } from '@shared/constants/activity-code/activity-code.constants';
-// import { ActivityCodeModalEvent } from '@components/common/activity-code/acitivity-code-modal-event';
-// import { DataGridComponent } from '@components/common/data-grid/data-grid';
-//
-// describe('DataGridComponent', () => {
-//   let fixture: ComponentFixture<DataGridComponent>;
-//   let component: DataGridComponent;
-//
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [
-//         DataGridComponent,
-//       ],
-//       imports: [
-//         IonicModule,
-//         AppModule,
-//       ],
-//       providers: [
-//       ],
-//     });
-//
-//     fixture = TestBed.createComponent(DataGridComponent);
-//     component = fixture.componentInstance;
-//   }));
-//
-//   describe('Class', () => {
-//     describe('activityCodeChanged', () => {
-//       it('should emit activityCodeChange with passed value', () => {
-//         spyOn(component.activityCodeChange, 'emit');
-//         component.activityCodeChanged({ activityCode: '1' } as ActivityCodeModel);
-//
-//         expect(component.activityCodeChange.emit).toHaveBeenCalledWith({ activityCode: '1' } as ActivityCodeModel);
-//       });
-//     });
-//
-//     describe('onModalDismiss', () => {
-//       it('should call activityCodeChanged with data passed if event is SELECT_CODE', async () => {
-//         spyOn(component, 'activityCodeChanged');
-//         await component.onModalDismiss(
-//         { activityCode: '1' } as ActivityCodeModel, ActivityCodeModalEvent.SELECT_CODE);
-//
-//         expect(component.activityCodeChanged).toHaveBeenCalledWith({ activityCode: '1' } as ActivityCodeModel);
-//       });
-//       it('should not call activityCodeChanged with data passed if event is CANCEL', async () => {
-//         spyOn(component, 'activityCodeChanged');
-//         await component.onModalDismiss({ activityCode: '1' } as ActivityCodeModel, ActivityCodeModalEvent.CANCEL);
-//         expect(component.activityCodeChanged).not.toHaveBeenCalled();
-//       });
-//     });
-//
-//     describe('openActivityCodeListModal', () => {
-//       it('should call alertController.create if isSelectDisabled returns false', async () => {
-//         spyOn(component, 'isSelectDisabled').and.returnValue(false);
-//         spyOn(component.modalController, 'create').and.returnValue(Promise.resolve({
-//           present: () => Promise.resolve(),
-//           onWillDismiss: () => Promise.resolve({ data: true }),
-//         } as HTMLIonModalElement));
-//         await component.openActivityCodeListModal();
-//         expect(component.modalController.create).toHaveBeenCalled();
-//       });
-//     });
-//
-//     describe('invalid', () => {
-//       it('should return true if the formControl is invalid and dirty', () => {
-//         component['formControl'] = null;
-//         component.formGroup = new UntypedFormGroup({});
-//         component.ngOnChanges();
-//         component['formControl'].setValidators([Validators.required]);
-//
-//         component['formControl'].setValue(null);
-//         component['formControl'].markAsDirty();
-//
-//         expect(component.invalid).toBeTruthy();
-//       });
-//       it('should return false if the formControl is valid and dirty', () => {
-//         component['formControl'] = null;
-//         component.formGroup = new UntypedFormGroup({});
-//         component.ngOnChanges();
-//         component['formControl'].setValidators([Validators.required]);
-//
-//         component['formControl'].setValue(1);
-//         component['formControl'].markAsDirty();
-//
-//         expect(component.invalid).toBeFalsy();
-//       });
-//       it('should return false if the formControl is invalid and clean', () => {
-//         component['formControl'] = null;
-//         component.formGroup = new UntypedFormGroup({});
-//         component.ngOnChanges();
-//         component['formControl'].setValidators([Validators.required]);
-//
-//         component['formControl'].setValue(null);
-//         component['formControl'].markAsPristine();
-//
-//         expect(component.invalid).toBeFalsy();
-//       });
-//       it('should return false if the formControl is valid and clean', () => {
-//         component['formControl'] = null;
-//         component.formGroup = new UntypedFormGroup({});
-//         component.ngOnChanges();
-//         component['formControl'].setValidators([Validators.required]);
-//
-//         component['formControl'].setValue(1);
-//         component['formControl'].markAsPristine();
-//
-//         expect(component.invalid).toBeFalsy();
-//       });
-//     });
-//   });
-//
-// });
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { AppModule } from '@app/app.module';
+import { DataGridComponent } from '@components/common/data-grid/data-grid';
+
+fdescribe('DataGridComponent', () => {
+  let fixture: ComponentFixture<DataGridComponent>;
+  let component: DataGridComponent;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DataGridComponent,
+      ],
+      imports: [
+        IonicModule,
+        AppModule,
+      ],
+      providers: [
+      ],
+    });
+
+    fixture = TestBed.createComponent(DataGridComponent);
+    component = fixture.componentInstance;
+  }));
+
+  describe('Class', () => {
+    describe('ngOnInit', () => {
+      it('should call cropData if croppedRows is null and ' +
+          'rowCropCount && passedData are both present', () => {
+        component.croppedRows = null;
+        component.rowCropCount = 3;
+        component.passedData = [[1], [2], [3], [4], [5], [6]];
+        spyOn(component, 'cropData');
+
+        component.ngOnInit();
+
+        expect(component.cropData).toHaveBeenCalled();
+      });
+      it('should call loopColours if colourScheme is present and ' +
+          'finalColourArray is not present', () => {
+        component.croppedRows = null;
+        component.rowCropCount = 3;
+        component.passedData = [[1], [2], [3], [4], [5], [6]];
+
+        component.colourScheme = ['1', '2', '3', '4'];
+        component.finalColourArray = null;
+        spyOn(component, 'loopColours');
+
+        component.ngOnInit();
+
+        expect(component.loopColours).toHaveBeenCalled();
+      });
+    });
+    describe('ngOnChanges', () => {
+      it('should correctly crop an array into 2 depending on the parameter', () => {
+        component.croppedRows = null;
+        component.rowCropCount = 3;
+        component.passedData = [[1], [2], [3], [4], [5], [6]];
+
+        // component.ngOnChanges();
+
+        expect(component.croppedRows).toEqual( { preCrop: [[1], [2], [3]], postCrop: [[4], [5], [6]] });
+      });
+    });
+    describe('cropData', () => {
+      it('should correctly crop an array into 2 depending on the parameter', () => {
+        component.croppedRows = null;
+        component.rowCropCount = 3;
+        component.passedData = [[1], [2], [3], [4], [5], [6]];
+
+        component.cropData();
+
+        expect(component.croppedRows).toEqual( { preCrop: [[1], [2], [3]], postCrop: [[4], [5], [6]] });
+      });
+    });
+    describe('loopColours', () => {
+      it('should loop the colour array once if there ' +
+          'is more rows in the data grid than items in the colour array', () => {
+        component.passedData = [[1], [2], [3], [4], [5], [6]];
+        component.colourScheme = ['1', '2', '3', '4'];
+
+        expect(component.loopColours()).toEqual( ['1', '2', '3', '4', '1', '2', '3', '4']);
+      });
+    });
+  });
+
+});
