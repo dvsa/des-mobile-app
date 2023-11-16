4441ec3be09871fd77c23f2664c185dbe78b2197
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2023-11-16 15:52:16 +0000

    WIP: Updated privacy

--- src/app/providers/colour-contrast/colour-contrast.service.ts
+++ src/app/providers/colour-contrast/colour-contrast.service.ts
@@ -14,7 +14,7 @@ export class ColourContrastService {
       0.0722 * this.relativeLuminance(color[2]);
   }
 
-  hexToRgb(hex: string): [number, number, number] {
+  private hexToRgb(hex: string): [number, number, number] {
     let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? [parseInt(result[1], 16),
       parseInt(result[2], 16),
