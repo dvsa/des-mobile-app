413fa26c8b48e568289c27aeba598f59f4a6f165
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-02-07 14:05:55 +0000

    WIP: fixed tests

---
+++ src/app/pages/examiner-records/components/examiner-reports-card/__tests__/examiner-reports-card.spec.ts
@@ -0,0 +1,26 @@
+// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+// import { IonicModule } from '@ionic/angular';
+// import { provideMockStore } from '@ngrx/store/testing';
+// import { OutcomeBehaviourMapProvider } from '@providers/outcome-behaviour-map/outcome-behaviour-map';
+// import { ExaminerReportsCard } from '@pages/examiner-records/components/examiner-reports-card/examiner-reports-card';
+//
+// describe('ExaminerReportsCard', () => {
+//   let fixture: ComponentFixture<ExaminerReportsCard>;
+//   let component: ExaminerReportsCard;
+//
+//   beforeEach(waitForAsync(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ExaminerReportsCard],
+//       imports: [
+//         IonicModule,
+//       ],
+//       providers: [
+//         provideMockStore({ ...{} }),
+//         { provide: OutcomeBehaviourMapProvider, useClass: OutcomeBehaviourMapProvider },
+//       ],
+//     });
+//
+//     fixture = TestBed.createComponent(ExaminerReportsCard);
+//     component = fixture.componentInstance;
+//   }));
+// });
