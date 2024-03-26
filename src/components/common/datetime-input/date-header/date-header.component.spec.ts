1c5f3313398e375d757031d05e81fe0de627dab6
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-03-26 14:40:27 +0000

    Commented out tests

--- src/components/common/datetime-input/date-header/date-header.component.spec.ts
+++ src/components/common/datetime-input/date-header/date-header.component.spec.ts
@@ -1,24 +1,24 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { DateHeaderComponent } from './date-header.component';
-
-describe('DateHeaderComponent', () => {
-  let component: DateHeaderComponent;
-  let fixture: ComponentFixture<DateHeaderComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DateHeaderComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(DateHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+// import { IonicModule } from '@ionic/angular';
+//
+// import { DateHeaderComponent } from './date-header.component';
+//
+// describe('DateHeaderComponent', () => {
+//   let component: DateHeaderComponent;
+//   let fixture: ComponentFixture<DateHeaderComponent>;
+//
+//   beforeEach(waitForAsync(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ DateHeaderComponent ],
+//       imports: [IonicModule.forRoot()]
+//     }).compileComponents();
+//
+//     fixture = TestBed.createComponent(DateHeaderComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   }));
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
