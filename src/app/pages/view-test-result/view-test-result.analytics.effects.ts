a9ed452ae002dab5202bba883aa52d4b7b7e80aa
Rhys Lewis <88314925+RLCorp@users.noreply.github.com>
2024-03-28 09:22:24 +0000

    Added GA4 to view test result (#1514)

--- src/app/pages/view-test-result/view-test-result.analytics.effects.ts
+++ src/app/pages/view-test-result/view-test-result.analytics.effects.ts
@@ -19,8 +19,14 @@ export class ViewTestResultAnalyticsEffects {
   viewTestResultViewDidEnter$ = createEffect(() => this.actions$.pipe(
     ofType(ViewTestResultViewDidEnter),
     switchMap((action: ReturnType<typeof ViewTestResultViewDidEnter>) => {
+      // TODO - MES-9495 - remove old analytics
       this.analytics.addCustomDimension(AnalyticsDimensionIndices.APPLICATION_REFERENCE, action.applicationReference);
       this.analytics.setCurrentPage(AnalyticsScreenNames.VIEW_TEST_RESULT);
+
+      // GA4 Analytics
+      this.analytics.addGACustomDimension(AnalyticsDimensionIndices.APPLICATION_REFERENCE, action.applicationReference);
+      this.analytics.setGACurrentPage(AnalyticsScreenNames.VIEW_TEST_RESULT);
+
       return of(AnalyticRecorded());
     }),
   ));
