436ecd758e6c70e299713d563d25cccaa8c01308
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2024-04-04 12:37:08 +0100

    add GA4 analytics for practice mode journal

--- src/app/pages/fake-journal/fake-journal.analytics.effects.ts
+++ src/app/pages/fake-journal/fake-journal.analytics.effects.ts
@@ -6,7 +6,7 @@ import { switchMap } from 'rxjs/operators';
 import { AnalyticsProvider } from '@providers/analytics/analytics';
 import {
   AnalyticsEventCategories, AnalyticsEvents,
-  AnalyticsScreenNames,
+  AnalyticsScreenNames, GoogleAnalyticsEvents, GoogleAnalyticsEventsTitles,
 } from '@providers/analytics/analytics.model';
 import { AnalyticRecorded } from '@providers/analytics/analytics.actions';
 import { FakeJournalDidEnter, StartE2EPracticeTest } from '@pages/fake-journal/fake-journal.actions';
@@ -23,7 +23,11 @@ export class FakeJournalAnalyticsEffects {
   fakeJournalViewDidEnter$ = createEffect(() => this.actions$.pipe(
     ofType(FakeJournalDidEnter),
     switchMap(() => {
+      // TODO - MES-9495 - remove old analytics
       this.analytics.setCurrentPage(AnalyticsScreenNames.FAKE_JOURNAL);
+
+      // GA4 Analytics
+      this.analytics.setGACurrentPage(AnalyticsScreenNames.FAKE_JOURNAL);
       return of(AnalyticRecorded());
     }),
   ));
@@ -31,9 +35,17 @@ export class FakeJournalAnalyticsEffects {
   practiceStartFullTestAnalyticsEffect$ = createEffect(() => this.actions$.pipe(
     ofType(StartE2EPracticeTest),
     switchMap(({ category }: ReturnType<typeof StartE2EPracticeTest>) => {
+      // TODO - MES-9495 - remove old analytics
       this.analytics.logEvent(
         AnalyticsEventCategories.FAKE_JOURNAL,
         AnalyticsEvents.PRACTICE_FULL_TEST_SELECTED,
+        category,
+      );
+
+      // GA4 Analytics
+      this.analytics.logGAEvent(
+        GoogleAnalyticsEvents.PRACTICE_MODE_NAVIGATION,
+        GoogleAnalyticsEventsTitles.FULL_MODE_SELECTED,
         category,
       );
       return of(AnalyticRecorded());
