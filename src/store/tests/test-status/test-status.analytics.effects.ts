8eb39710950d7089494d4848113f11a5c9fc3902
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2024-04-09 10:51:19 +0100

    migrated GA4 analytics for test status

--- src/store/tests/test-status/test-status.analytics.effects.ts
+++ src/store/tests/test-status/test-status.analytics.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { concatMap } from 'rxjs/operators';
 import { AnalyticsProvider } from '@providers/analytics/analytics';
-import { AnalyticsEventCategories, AnalyticsEvents } from '@providers/analytics/analytics.model';
+import { AnalyticsEventCategories, AnalyticsEvents, GoogleAnalyticsEvents } from '@providers/analytics/analytics.model';
 import { AnalyticRecorded } from '@providers/analytics/analytics.actions';
 import * as testStatusActions from './test-status.actions';
 
@@ -19,10 +19,17 @@ export class TestStatusAnalyticsEffects {
   setTestStatusDecidedEffect$ = createEffect(() => this.actions$.pipe(
     ofType(testStatusActions.SetTestStatusDecided),
     concatMap(() => {
+
+      // TODO - MES-9495 - remove old analytics
       this.analytics.logEvent(
         AnalyticsEventCategories.TEST_LIFECYCLE,
         AnalyticsEvents.TEST_DECIDED,
       );
+
+      //GA4 Analytics
+      this.analytics.logGAEvent(
+        GoogleAnalyticsEvents.TEST_DECIDED
+      )
       return of(AnalyticRecorded());
     }),
   ));
@@ -30,10 +37,17 @@ export class TestStatusAnalyticsEffects {
   setTestStatusWriteUpEffect$ = createEffect(() => this.actions$.pipe(
     ofType(testStatusActions.SetTestStatusWriteUp),
     concatMap(() => {
+
+      // TODO - MES-9495 - remove old analytics
       this.analytics.logEvent(
         AnalyticsEventCategories.TEST_LIFECYCLE,
         AnalyticsEvents.TEST_IN_WRITE_UP,
       );
+
+      //GA4 Analytics
+      this.analytics.logGAEvent(
+        GoogleAnalyticsEvents.TEST_IN_WRITE_UP
+      )
       return of(AnalyticRecorded());
     }),
   ));
@@ -41,10 +55,17 @@ export class TestStatusAnalyticsEffects {
   setTestStatusAutosavedEffect$ = createEffect(() => this.actions$.pipe(
     ofType(testStatusActions.SetTestStatusAutosaved),
     concatMap(() => {
+
+      // TODO - MES-9495 - remove old analytics
       this.analytics.logEvent(
         AnalyticsEventCategories.TEST_LIFECYCLE,
         AnalyticsEvents.TEST_AUTOSAVED,
       );
+
+      // GA4 Analytics
+      this.analytics.logGAEvent(
+        GoogleAnalyticsEvents.TEST_AUTOSAVED
+      )
       return of(AnalyticRecorded());
     }),
   ));
@@ -52,10 +73,17 @@ export class TestStatusAnalyticsEffects {
   setTestStatusSubmittedEffect$ = createEffect(() => this.actions$.pipe(
     ofType(testStatusActions.SetTestStatusSubmitted),
     concatMap(() => {
+
+      // TODO - MES-9495 - remove old analytics
       this.analytics.logEvent(
         AnalyticsEventCategories.TEST_LIFECYCLE,
         AnalyticsEvents.TEST_SUBMITTED,
       );
+
+      // GA4 Analytics
+      this.analytics.logGAEvent(
+        GoogleAnalyticsEvents.TEST_SUBMITTED
+      )
       return of(AnalyticRecorded());
     }),
   ));
