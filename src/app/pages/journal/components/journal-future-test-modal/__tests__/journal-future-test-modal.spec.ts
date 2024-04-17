32a692aef662adafe2361b9c6443d6e9e67418dc
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-04-17 15:09:25 +0100

    Included new function to check if a rekey takes place is the future and displays a modal if it does

---
+++ src/app/pages/journal/components/journal-future-test-modal/__tests__/journal-future-test-modal.spec.ts
@@ -0,0 +1,93 @@
+import { ComponentFixture, waitForAsync, TestBed } from '@angular/core/testing';
+import { IonicModule, NavParams, ModalController } from '@ionic/angular';
+import { ModalControllerMock, NavParamsMock } from '@mocks/index.mock';
+import { By } from '@angular/platform-browser';
+import { DeviceProvider } from '@providers/device/device';
+import { DeviceProviderMock } from '@providers/device/__mocks__/device.mock';
+import { ComponentsModule } from '@components/common/common-components.module';
+import { LogHelper } from '@providers/logs/logs-helper';
+import { LogHelperMock } from '@providers/logs/__mocks__/logs-helper.mock';
+import { ModalEvent } from '../journal-future-test-modal.constants';
+import { JournalFutureTestModal } from '../journal-future-test-modal';
+
+describe('JournalRekeyModal', () => {
+  let fixture: ComponentFixture<JournalFutureTestModal>;
+  let component: JournalFutureTestModal;
+  let modalController: ModalController;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        JournalFutureTestModal,
+      ],
+      imports: [
+        IonicModule,
+        ComponentsModule,
+      ],
+      providers: [
+        { provide: NavParams, useClass: NavParamsMock },
+        { provide: ModalController, useClass: ModalControllerMock },
+        { provide: DeviceProvider, useClass: DeviceProviderMock },
+        { provide: LogHelper, useClass: LogHelperMock },
+      ],
+    });
+
+    fixture = TestBed.createComponent(JournalFutureTestModal);
+    modalController = TestBed.inject(ModalController);
+    component = fixture.componentInstance;
+  }));
+
+  describe('DOM', () => {
+    it('should call onStartTest when the Start test button is clicked', () => {
+      fixture.detectChanges();
+      spyOn(component, 'onStartTest');
+      const button = fixture.debugElement.query(By.css('ion-button.start-test-button'));
+      button.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+      expect(component.onStartTest).toHaveBeenCalled();
+    });
+
+    it('should call onRekeyTest when the Rekey a paper test button is clicked', () => {
+      fixture.detectChanges();
+      spyOn(component, 'onRekeyTest');
+      const button = fixture.debugElement.query(By.css('ion-button.rekey-test-button'));
+      button.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+      expect(component.onRekeyTest).toHaveBeenCalled();
+    });
+
+    it('should call onCancel when the Cancel button is clicked', () => {
+      fixture.detectChanges();
+      spyOn(component, 'onCancel');
+      const button = fixture.debugElement.query(By.css('ion-button.cancel-button'));
+      button.triggerEventHandler('click', null);
+
+      fixture.detectChanges();
+      expect(component.onCancel).toHaveBeenCalled();
+    });
+  });
+
+  describe('onCancel', () => {
+    it('should call dismiss with CANCEL', async () => {
+      spyOn(modalController, 'dismiss');
+      await component.onCancel();
+      expect(await modalController.dismiss).toHaveBeenCalledWith(ModalEvent.CANCEL);
+    });
+  });
+  describe('onStartTest', () => {
+    it('should call dismiss with START', async () => {
+      spyOn(modalController, 'dismiss');
+      await component.onStartTest();
+      expect(await modalController.dismiss).toHaveBeenCalledWith(ModalEvent.START);
+    });
+  });
+  describe('onRekeyTest', () => {
+    it('should call dismiss with REKEY', async () => {
+      spyOn(modalController, 'dismiss');
+      await component.onRekeyTest();
+      expect(await modalController.dismiss).toHaveBeenCalledWith(ModalEvent.REKEY);
+    });
+  });
+});
