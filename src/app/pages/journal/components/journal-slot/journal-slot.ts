b34e4886978422c49d918327b21cbec26ef04018
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-06-18 11:29:27 +0100

    removed unneeded code

--- src/app/pages/journal/components/journal-slot/journal-slot.ts
+++ src/app/pages/journal/components/journal-slot/journal-slot.ts
@@ -4,18 +4,12 @@ import { TestSlot } from '@dvsa/mes-journal-schema';
 import { get, has, isEmpty } from 'lodash-es';
 import { SlotItem } from '@providers/slot-selector/slot-item';
 import { SlotSelectorProvider } from '@providers/slot-selector/slot-selector';
-import { CompletedJournalSlot } from '@pages/journal/journal.page';
-import { formatApplicationReference } from '@shared/helpers/formatters';
-import { ApplicationReference } from '@dvsa/mes-test-schema/categories/common';
 
 @Component({
   selector: 'journal-slots',
   templateUrl: 'journal-slot.html',
 })
 export class JournalSlotComponent {
-
-  @Input()
-  completedTests: CompletedJournalSlot[] = [];
 
   @Input()
   slots: SlotItem[] = [];
@@ -29,22 +23,6 @@ export class JournalSlotComponent {
   constructor(
     private slotSelector: SlotSelectorProvider,
   ) {
-  }
-
-  /**
-   * Find the completed test for the given slot if exists
-   * @param slotData
-   */
-  findCompletedTest(slotData: TestSlot): CompletedJournalSlot {
-    if (!!get(slotData, 'booking')) {
-      const tempAppRef = parseInt(formatApplicationReference({
-        applicationId: slotData.booking.application.applicationId,
-        bookingSequence: slotData.booking.application.bookingSequence,
-        checkDigit: slotData.booking.application.checkDigit,
-      } as ApplicationReference), 10);
-      return null
-    }
-    return null;
   }
 
   didSlotPass = (
