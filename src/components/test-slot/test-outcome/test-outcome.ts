36dab03e31f624986cc884896283a027019b377e
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2024-06-21 15:47:57 +0100

    Revert "Mes-8978: delegated future test modal (#1646)"
    This reverts commit dd7083c36cf2ab49d3ed58fcc7b98c4b0108ce96.

--- src/components/test-slot/test-outcome/test-outcome.ts
+++ src/components/test-slot/test-outcome/test-outcome.ts
@@ -31,7 +31,6 @@ import { CategoryWhitelistProvider } from '@providers/category-whitelist/categor
 import { PreviewModeModal } from '@pages/fake-journal/components/preview-mode-modal/preview-mode-modal';
 import { ContinueUnuploadedTest } from '@pages/unuploaded-tests/unuploaded-tests.actions';
 import { AccessibilityService } from '@providers/accessibility/accessibility.service';
-import { JournalFutureTestModal } from '@pages/journal/components/journal-future-test-modal/journal-future-test-modal';
 
 @Component({
   selector: 'test-outcome',
@@ -237,28 +236,11 @@ export class TestOutcomeComponent implements OnInit {
     await this.router.navigate([TestFlowPageNames.WAITING_ROOM_PAGE]);
   }
 
-  async rekeyDelegatedTestStart() {
+  async rekeyDelegatedTest(): Promise<void> {
     this.store$.dispatch(StartTest(this.slotDetail.slotId, this.category, true, true));
     this.store$.dispatch(SetExaminerConducted(this.examinerId));
     this.store$.dispatch(SetExaminerBooked(this.examinerId));
     await this.routeByCat.navigateToPage(TestFlowPageNames.WAITING_ROOM_TO_CAR_PAGE, this.category);
-  }
-
-  async rekeyDelegatedTest(): Promise<void> {
-    //compare the current date to the date of the test without factoring in time to see if the test is in the future
-    if (new Date(new Date(this.slot.slotDetail.start).toDateString()) > new Date(new Date().toDateString())) {
-      const modal: HTMLIonModalElement = await this.modalController.create({
-        component: JournalFutureTestModal,
-        cssClass: 'mes-modal-alert text-zoom-regular',
-      });
-      await modal.present();
-      const { data } = await modal.onDidDismiss<ModalEvent>();
-      if (data === ModalEvent.START) {
-        await this.rekeyDelegatedTestStart();
-      }
-    } else {
-      await this.rekeyDelegatedTestStart();
-    }
   }
 
   displayRekeyModal = async (): Promise<void> => {
