c3b3493a7eeb079a6d6e14427e90e711648a8ed1
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-07-25 15:57:47 +0100

    Removed unneeded tests

--- src/components/common/data-grid/__tests__/data-grid.spec.ts
+++ src/components/common/data-grid/__tests__/data-grid.spec.ts
@@ -24,33 +24,6 @@ describe('DataGridComponent', () => {
     component = fixture.componentInstance;
   }));
 
-  describe('ngOnInit', () => {
-    it('should call cropData if croppedRows is null and ' +
-      'rowCropCount && passedData are both present', () => {
-      component.croppedRows = null;
-      component.rowCropCount = 3;
-      component.passedData = [[1], [2], [3], [4], [5], [6]];
-      spyOn(component, 'cropData');
-
-      component.ngOnInit();
-
-      expect(component.cropData).toHaveBeenCalled();
-    });
-    it('should call loopColours if colourScheme is present and ' +
-      'finalColourArray is not present', () => {
-      component.croppedRows = null;
-      component.rowCropCount = 3;
-      component.passedData = [[1], [2], [3], [4], [5], [6]];
-
-      component.colourScheme = ['1', '2', '3', '4'];
-      component.finalColourArray = null;
-      spyOn(component, 'loopColours');
-
-      component.ngOnInit();
-
-      expect(component.loopColours).toHaveBeenCalled();
-    });
-  });
   describe('ngOnChanges', () => {
     it('should set finalColourArray to loopColours if the changes include colourScheme', () => {
       spyOn(component, 'loopColours').and.returnValue(['1', '2']);
@@ -68,28 +41,6 @@ describe('DataGridComponent', () => {
       component.ngOnChanges({ data: { previousValue: '1', currentValue: '2' } as SimpleChange });
 
       expect(component.finalColourArray).toEqual(['1', '2']);
-    });
-    it('should run cropData if rowCropCount and passedData are present', () => {
-      spyOn(component, 'loopColours').and.returnValue(['1', '2']);
-      spyOn(component, 'cropData');
-
-      component.rowCropCount = 1;
-      component.passedData = [['1']];
-
-      component.ngOnChanges({ data: { previousValue: '1', currentValue: '2' } as SimpleChange });
-
-      expect(component.cropData).toHaveBeenCalled();
-    });
-  });
-  describe('cropData', () => {
-    it('should correctly crop an array into 2 depending on the parameter', () => {
-      component.croppedRows = null;
-      component.rowCropCount = 3;
-      component.passedData = [[1], [2], [3], [4], [5], [6]];
-
-      component.cropData();
-
-      expect(component.croppedRows).toEqual({ preCrop: [[1], [2], [3]], postCrop: [[4], [5], [6]] });
     });
   });
   describe('loopColours', () => {
