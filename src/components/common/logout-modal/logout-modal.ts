64c10bc5c5c12ed083c680615d29c68d827448f7
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-10-23 13:32:48 +0100

    recreated original logout modal

---
+++ src/components/common/logout-modal/logout-modal.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+export enum LogoutModalEvent {
+  LOGOUT = 'logout',
+  CANCEL = 'cancel',
+}
+
+@Component({
+  selector: 'logout-modal',
+  templateUrl: './logout-modal.html',
+  styleUrls: ['./logout-modal.scss'],
+})
+export class LogoutModal {
+  constructor(public modalController: ModalController) {}
+
+  async onCancel() {
+    await this.modalController.dismiss({ event: LogoutModalEvent.CANCEL });
+  }
+  async onLogout() {
+    await this.modalController.dismiss({ event: LogoutModalEvent.LOGOUT });
+  }
+}
