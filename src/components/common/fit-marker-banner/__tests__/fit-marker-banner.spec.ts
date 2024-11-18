4ef4459b9b3c4a6e5ae1a56b48d93de01322b6fd
Rhys Lewis <88314925+RLCorp@users.noreply.github.com>
2024-11-18 15:18:18 +0000

    mes-10108-fixUnitTests (#1824)
    * fix unit tests
    
    * linting

--- src/components/common/fit-marker-banner/__tests__/fit-marker-banner.spec.ts
+++ src/components/common/fit-marker-banner/__tests__/fit-marker-banner.spec.ts
@@ -1,23 +1,24 @@
-// TODO: MES-10108 - fix these tests
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { FitMarkerBannerComponent } from '@components/common/fit-marker-banner/fit-marker-banner';
-//
-// describe('FitMarkerBannerComponent', () => {
-//   let fixture: ComponentFixture<FitMarkerBannerComponent>;
-//   let component: FitMarkerBannerComponent;
-//
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [FitMarkerBannerComponent],
-//     });
-//
-//     fixture = TestBed.createComponent(FitMarkerBannerComponent);
-//     component = fixture.componentInstance;
-//   }));
-//
-//   describe('DOM', () => {
-//     it('should create', () => {
-//       expect(component).toBeDefined();
-//     });
-//   });
-// });
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FitMarkerBannerComponent } from '@components/common/fit-marker-banner/fit-marker-banner';
+import { IonicModule } from '@ionic/angular';
+
+describe('FitMarkerBannerComponent', () => {
+  let fixture: ComponentFixture<FitMarkerBannerComponent>;
+  let component: FitMarkerBannerComponent;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [FitMarkerBannerComponent],
+      imports: [IonicModule],
+    });
+
+    fixture = TestBed.createComponent(FitMarkerBannerComponent);
+    component = fixture.componentInstance;
+  }));
+
+  describe('DOM', () => {
+    it('should create', () => {
+      expect(component).toBeDefined();
+    });
+  });
+});
