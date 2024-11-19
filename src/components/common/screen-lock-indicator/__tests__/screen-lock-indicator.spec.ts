09246635265f19c705f4d94c6939fdca38d01a22
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-11-19 11:32:07 +0000

    Fixed more tests

--- src/components/common/screen-lock-indicator/__tests__/screen-lock-indicator.spec.ts
+++ src/components/common/screen-lock-indicator/__tests__/screen-lock-indicator.spec.ts
@@ -1,27 +1,27 @@
-// TODO: MES-10108 - fix these tests
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { TranslateModule, TranslateService } from '@ngx-translate/core';
-// import { translateServiceMock } from '@shared/helpers/__mocks__/translate.mock';
-// import { LockScreenIndicator } from '../lock-screen-indicator';
-//
-// describe('LockScreenIndicator', () => {
-//   let fixture: ComponentFixture<LockScreenIndicator>;
-//   let component: LockScreenIndicator;
-//
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [LockScreenIndicator],
-//       imports: [TranslateModule],
-//       providers: [{ provide: TranslateService, useValue: translateServiceMock }],
-//     });
-//
-//     fixture = TestBed.createComponent(LockScreenIndicator);
-//     component = fixture.componentInstance;
-//   }));
-//
-//   describe('Class', () => {
-//     it('should create', () => {
-//       expect(component).toBeDefined();
-//     });
-//   });
-// });
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { translateServiceMock } from '@shared/helpers/__mocks__/translate.mock';
+import { LockScreenIndicator } from '../lock-screen-indicator';
+import { IonicModule } from '@ionic/angular';
+
+describe('LockScreenIndicator', () => {
+  let fixture: ComponentFixture<LockScreenIndicator>;
+  let component: LockScreenIndicator;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [LockScreenIndicator],
+      imports: [TranslateModule, IonicModule],
+      providers: [{ provide: TranslateService, useValue: translateServiceMock }],
+    });
+
+    fixture = TestBed.createComponent(LockScreenIndicator);
+    component = fixture.componentInstance;
+  }));
+
+  describe('Class', () => {
+    it('should create', () => {
+      expect(component).toBeDefined();
+    });
+  });
+});
