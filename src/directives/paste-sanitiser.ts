ac742fde6ca181b8f31abf2d88af158ad0937c8e
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2024-11-21 14:39:26 +0000

    Changed paste sanitiser to no erase previous text

--- src/directives/paste-sanitiser.ts
+++ src/directives/paste-sanitiser.ts
@@ -12,32 +12,25 @@ export class PasteSanitiserDirective {
   onInput(event: ClipboardEvent): void {
     const inputField = this.el.nativeElement;
     if (!inputField) return;
-
     const numbersOnly = inputField.hasAttribute('numbersOnly');
-    const pastedData = event.clipboardData?.getData('text');
 
-    if (pastedData) {
-      setTimeout(() => {
-        let sanitisedData = pastedData;
+    setTimeout(() => {
+      let sanitisedData = inputField.value; // Check if numbersOnly attribute is true
+      if (numbersOnly) {
+        // Replace non-numeric characters
+        sanitisedData = sanitisedData.replace(/\D/g, '');
+      }
 
-        // Check if numbersOnly attribute is true
-        if (numbersOnly) {
-          // Replace non-numeric characters
-          sanitisedData = sanitisedData.replace(/\D/g, '');
-        }
+      // Strip emojis from string
+      sanitisedData = sanitisedData.replace(this.emojiPattern, '');
 
-        // Strip emojis from string
-        sanitisedData = sanitisedData.replace(this.emojiPattern, '');
-
-        const maxLength = Number(inputField.getAttribute('maxLength') || Number(inputField.getAttribute('charLimit')));
-        // Apply maxLength or charLimit check after sanitizing
-        if (maxLength && sanitisedData.length > maxLength) {
-          sanitisedData = sanitisedData.substring(0, maxLength);
-        }
-
-        // Cast sanitisedData as a number if numbersOnly attribute is present, otherwise just use sanitisedData
-        inputField.value = numbersOnly ? Number(sanitisedData) : sanitisedData;
-      });
-    }
+      const maxLength = Number(inputField.getAttribute('maxLength') || Number(inputField.getAttribute('charLimit')));
+      // Apply maxLength or charLimit check after sanitizing
+      if (maxLength && sanitisedData.length > maxLength) {
+        sanitisedData = sanitisedData.substring(0, maxLength);
+      }
+      // Cast sanitisedData as a number if numbersOnly attribute is present, otherwise just use sanitisedData
+      inputField.value = numbersOnly ? Number(sanitisedData) : sanitisedData;
+    });
   }
 }
