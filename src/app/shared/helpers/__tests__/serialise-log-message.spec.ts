bf4d516b06178733d6a7a398d5fabb9086d28382
mattc <mattthew.culliford@dvsa.gov.uk>
2024-11-28 15:02:51 +0000

    reinstate chrome unit tests

--- src/app/shared/helpers/__tests__/serialise-log-message.spec.ts
+++ src/app/shared/helpers/__tests__/serialise-log-message.spec.ts
@@ -5,8 +5,7 @@ describe('serialiseLogMessage', () => {
     const res = serialiseLogMessage('test');
     expect(res).toEqual('test');
   });
-  // TODO: MES-10108 - fix these tests
-  xit('should return an error that contains the stack, class and information', () => {
+  it('should return an error that contains the stack, class and information', () => {
     const res = serialiseLogMessage(new Error('test'));
     expect(res).toContain('stack');
     expect(res).toContain('Error');
