d3860a6487666e555890ad844e0d768471b71039
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-02-26 10:53:34 +0000

    lint

--- src/app/pages/examiner-records/components/examiner-records-components.module.ts
+++ src/app/pages/examiner-records/components/examiner-records-components.module.ts
@@ -4,11 +4,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule } from '@components/common/common-components.module';
 import { IonicModule } from '@ionic/angular';
 import { ColourFilterRadioComponent } from '@pages/examiner-records/components/colour-filter-radio/colour-filter-radio';
+import { CustomDateRangeModalModule } from '@pages/examiner-records/components/custom-date-range-modal/custom-date-range-modal.module';
 import { ExaminerRecordsLabelTextComponentModule } from '@pages/examiner-records/components/examiner-records-label-text/examiner-records-label-text.module';
 import { ExaminerRecordsLearnMoreModalModule } from '@pages/examiner-records/components/examiner-records-learn-more-modal/examiner-records-learn-more-modal.module';
 import { ExaminerReportsCard } from '@pages/examiner-records/components/examiner-reports-card/examiner-reports-card';
 import { CompressionProvider } from '@providers/compression/compression';
-import { CustomDateRangeModalModule } from '@pages/examiner-records/components/custom-date-range-modal/custom-date-range-modal.module';
 
 @NgModule({
   declarations: [ColourFilterRadioComponent, ExaminerReportsCard],
