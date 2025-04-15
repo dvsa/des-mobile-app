ad309a8b01ebd6a0a9a7e0deab62c1f59ab67bb7
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-04-15 11:13:16 +0100

    update test

--- src/components/common/datetime-input/custom-calendar-component/_tests_/custom-calendar.component.spec.ts
+++ src/components/common/datetime-input/custom-calendar-component/_tests_/custom-calendar.component.spec.ts
@@ -1,21 +1,35 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
 import { IonicModule } from '@ionic/angular';
 
 import { CustomCalendarComponent } from '../custom-calendar.component';
 
-describe('CustomCalendarComponentComponent', () => {
+const CUSTOM_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+describe('CustomCalendarComponent', () => {
   let component: CustomCalendarComponent;
   let fixture: ComponentFixture<CustomCalendarComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
+      providers: [{ provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS }],
       imports: [CustomCalendarComponent, IonicModule.forRoot()],
-    }).compileComponents();
+    });
 
     fixture = TestBed.createComponent(CustomCalendarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
