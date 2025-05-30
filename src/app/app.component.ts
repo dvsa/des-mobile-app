81e9c5842584d40e7fef78441f072afae72d0968
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2025-05-30 12:08:21 +0100

    feat: enable examiner records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,13 @@ import { Observable, Subscription, merge } from 'rxjs';
 
 import { SENTRY_ERRORS } from '@app/sentry-error-handler';
 import { SideMenuClosed, SideMenuItemSelected, SideMenuOpened } from '@pages/dashboard/dashboard.actions';
-import { DASHBOARD_PAGE, LOGIN_PAGE, UNUPLOADED_TESTS_PAGE, USEFUL_LINKS_PAGE } from '@pages/page-names.constants';
+import {
+  DASHBOARD_PAGE,
+  EXAMINER_RECORDS,
+  LOGIN_PAGE,
+  UNUPLOADED_TESTS_PAGE,
+  USEFUL_LINKS_PAGE,
+} from '@pages/page-names.constants';
 import { unsubmittedTestSlotsCount$ } from '@pages/unuploaded-tests/unuploaded-tests.selector';
 import { AccessibilityService } from '@providers/accessibility/accessibility.service';
 import { AppConfigProvider } from '@providers/app-config/app-config';
@@ -60,12 +66,11 @@ export class AppComponent extends LogoutBasePageComponent implements OnInit {
       showUnSubmittedCount: true,
       hideWhenRole: [ExaminerRole.DLG],
     },
-    // TODO: MES-10432 Uncomment when examiner records page is implemented
-    // {
-    //   title: EXAMINER_RECORDS,
-    //   descriptor: 'Examiner records',
-    //   hideWhenRole: [ExaminerRole.DLG],
-    // },
+    {
+      title: EXAMINER_RECORDS,
+      descriptor: 'Examiner records',
+      hideWhenRole: [ExaminerRole.DLG],
+    },
     {
       title: USEFUL_LINKS_PAGE,
       descriptor: 'Useful links',
