35846ea8dfdcace702f4020d098bc7ae01fc77ee
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-06-02 09:38:03 +0100

    Added tests and reformatted code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -110,7 +110,7 @@ export class AppComponent extends LogoutBasePageComponent implements OnInit {
   async ngOnInit() {
     try {
       await this.platform.ready();
-      await this.dataStore.initializeStore();
+      await this.dataStore.initDataStore();
       if (this.platform.is('cordova')) {
         await this.deviceProvider.disableSingleAppMode();
       }
