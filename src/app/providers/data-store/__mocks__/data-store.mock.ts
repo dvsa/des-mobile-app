c1881b58384e81a38d21ad5368098d721e8bd4fa
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-06-02 10:23:40 +0100

    Removed unneeded code from the mock

--- src/app/providers/data-store/__mocks__/data-store.mock.ts
+++ src/app/providers/data-store/__mocks__/data-store.mock.ts
@@ -5,17 +5,7 @@ export class DataStoreProviderMock {
 
   getItem = jasmine.createSpy('getItem').and.returnValue(Promise.resolve('get'));
 
-  setSecureContainer = jasmine.createSpy('setSecureContainer').and.returnValue(Promise.resolve());
-
-  createContainer = jasmine.createSpy('createContainer').and.returnValue(Promise.resolve());
-
   removeItem = jasmine.createSpy('removeItem');
 
   getKeys = jasmine.createSpy('getKeys').and.returnValue(Promise.resolve(['TESTS']));
-
-  migrateKey = jasmine.createSpy('migrateKey').and.returnValue(Promise.resolve());
-
-  migrateAllKeys = jasmine.createSpy('migrateAllKeys').and.returnValue(Promise.resolve());
-
-  hasStorageBeenMigrated = jasmine.createSpy('hasStorageBeenMigrated').and.returnValue(Promise.resolve(false));
 }
