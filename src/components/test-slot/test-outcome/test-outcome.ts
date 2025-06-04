cf4440341245571077ed1b8a914e9ca3273b0262
andrew setterfield <andrew.setterfield@dvsa.gov.uk>
2025-06-04 10:02:10 +0100

    Revert "feat: remove remnants of any started test for a rekey test.  Same logic is added to 'start test' button"
    This reverts commit feb4fcf59e4f7029d9d45551ecc1f1aa2edf2f0a.

--- src/components/test-slot/test-outcome/test-outcome.ts
+++ src/components/test-slot/test-outcome/test-outcome.ts
@@ -31,7 +31,7 @@ import { EarlyStartModalDidEnter, ResumingWriteUp } from '@store/journal/journal
 import { SetExaminerBooked } from '@store/tests/examiner-booked/examiner-booked.actions';
 import { SetExaminerConducted } from '@store/tests/examiner-conducted/examiner-conducted.actions';
 import { TestStatus } from '@store/tests/test-status/test-status.model';
-import { ActivateTest, RemoveTestBySlotId, StartTest } from '@store/tests/tests.actions';
+import { ActivateTest, StartTest } from '@store/tests/tests.actions';
 
 @Component({
   selector: 'test-outcome',
@@ -207,8 +207,6 @@ export class TestOutcomeComponent implements OnInit {
     if (this.isE2EPracticeMode()) {
       this.store$.dispatch(StartE2EPracticeTest(this.slotDetail.slotId.toString(), this.category));
     } else {
-      //remove any reference that may already exist in the store prior to starting the test
-      this.store$.dispatch(RemoveTestBySlotId(this.slotDetail.slotId));
       this.store$.dispatch(StartTest(this.slotDetail.slotId, this.category, this.startTestAsRekey || this.isRekey));
     }
 
@@ -222,19 +220,19 @@ export class TestOutcomeComponent implements OnInit {
       this.store$.dispatch(ContinueUnuploadedTest('Rekey'));
     }
 
-    //remove any reference that may already exist in the store prior to starting the test
-    this.store$.dispatch(RemoveTestBySlotId(this.slotDetail.slotId));
-
-    this.store$.dispatch(
-      StartTest(
-        this.slotDetail.slotId,
-        this.category,
-        true,
-        false,
-        DateTime.at(this.slotDetail.start).format('YYYY-MM-DD')
-      )
-    );
-
+    if (this.testStatus === null || this.testStatus === TestStatus.Booked) {
+      this.store$.dispatch(
+        StartTest(
+          this.slotDetail.slotId,
+          this.category,
+          true,
+          false,
+          DateTime.at(this.slotDetail.start).format('YYYY-MM-DD')
+        )
+      );
+    } else {
+      this.store$.dispatch(ActivateTest(this.slotDetail.slotId, this.category, true));
+    }
     await this.router.navigate([
       this.category !== TestCategory.SC ? TestFlowPageNames.WAITING_ROOM_PAGE : TestFlowPageNames.COMMUNICATION_PAGE,
     ]);
