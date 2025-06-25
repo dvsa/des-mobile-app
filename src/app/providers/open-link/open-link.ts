fef212b8f854cd4492cd7c510b1fede7a4a732e7
Ryan Gittins <45790769+gittins7@users.noreply.github.com>
2025-06-25 12:09:06 +0100

    Mes 10522 | Citroën stop drive order work (#1986)
    * WIP: created banner and modal for Citroën recall link on journal page
    
    Also amended useful links functionality to now be in a provider for wider use
    
    * WIP: created banner and modal for Citroën recall link on journal page
    
    Also amended useful links functionality to now be in a provider for wider use
    
    * amended banner positioning and in-line styling
    
    * Latest design refinement and app launch invocation
    
    * Amended logic to grab useful links to ensure links are populated before navigating away from DES
    
    * PR Amendments

---
+++ src/app/providers/open-link/open-link.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { UsefulLink } from '@dvsa/mes-config-schema/remote-config';
+import { ModalController } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { LinkModalComponent } from '@pages/useful-links/components/link-modal/link-modal.component';
+import { UsefulLinkSelected } from '@pages/useful-links/useful-links.actions';
+import { AccessibilityService } from '@providers/accessibility/accessibility.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OpenLinkProvider {
+  constructor(
+    private modalController: ModalController,
+    private accessibilityService: AccessibilityService,
+    private store$: Store
+  ) {}
+
+  // /**
+  //  * Opens the link modal
+  //  * Dispatches the action to set the selected link, it determines this by appending the word Selected to the action name
+  //  * @param link
+  //  */
+  async openLinkModal(link: UsefulLink): Promise<void> {
+    const displayText = link.displayText.replace(/ /g, '_');
+    this.store$.dispatch(UsefulLinkSelected(displayText));
+    const modal: HTMLIonModalElement = await this.modalController.create({
+      id: 'linkModal',
+      component: LinkModalComponent,
+      componentProps: { link },
+      cssClass: `${this.accessibilityService.getTextZoomClass()} mes-modal-alert`,
+      backdropDismiss: false,
+      showBackdrop: true,
+    });
+    await modal.present();
+  }
+}
