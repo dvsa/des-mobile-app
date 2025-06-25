3839e21dc64a22117545949161a4adc259153ff8
ryan <ryan_gittins_33@hotmail.co.uk>
2025-06-25 11:40:26 +0100

    Amended logic to grab useful links to ensure links are populated before navigating away from DES

--- src/app/pages/journal/components/learn-more-modal/learn-more-modal.ts
+++ src/app/pages/journal/components/learn-more-modal/learn-more-modal.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { UsefulLink } from '@dvsa/mes-config-schema/remote-config';
+import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { OpenLinkProvider } from '@providers/open-link/open-link';
 import { UrlProvider } from '@providers/url/url';
@@ -9,10 +8,7 @@ import { UrlProvider } from '@providers/url/url';
   templateUrl: './learn-more-modal.html',
   styleUrls: ['./learn-more-modal.scss'],
 })
-export class LearnMoreModal implements OnInit {
-  usefulLinks: UsefulLink[] = [];
-  recallLink: UsefulLink;
-
+export class LearnMoreModal {
   constructor(
     public modalController: ModalController,
     public openLinkProvider: OpenLinkProvider,
@@ -20,20 +16,15 @@ export class LearnMoreModal implements OnInit {
   ) {}
 
   /**
-   * Initializes the component by fetching useful links.
-   * sets recallLink to the last index of useful links (currently the Citroën recall link)
-   */
-  ngOnInit() {
-    this.usefulLinks = this.urlProvider.getUsefulLinks();
-    this.recallLink = this.usefulLinks[this.usefulLinks.length - 1];
-  }
-
-  /**
    * Opens the open link modal & alerts user they are leaving the application
    */
   async openRecallLink() {
+    const usefulLinks = this.urlProvider.getUsefulLinks();
+
     await this.closeModal().then(() => {
-      this.openLinkProvider.openLinkModal(this.recallLink);
+      this.openLinkProvider.openLinkModal(
+        usefulLinks.filter((link) => link.displayText.toLowerCase().includes('recall'))[0]
+      );
     });
   }
 
