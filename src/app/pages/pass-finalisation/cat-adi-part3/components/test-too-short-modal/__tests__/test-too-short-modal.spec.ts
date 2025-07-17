401a92410f334b49e3f6da812adbd7a433cc9d1e
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-07-17 16:25:42 +0100

    Added removed test file

--- src/app/pages/pass-finalisation/cat-adi-part3/components/test-too-short-modal/__tests__/test-too-short-modal.spec.ts
+++ src/app/pages/pass-finalisation/cat-adi-part3/components/test-too-short-modal/__tests__/test-too-short-modal.spec.ts
@@ -1,40 +1,36 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppModule } from '@app/app.module';
-import { ModalController } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
 import { ModalControllerMock } from '@mocks/ionic-mocks/modal-controller.mock';
 import { TestTooShortModal } from '../test-too-short-modal';
 
-xdescribe('TestTooShortModal', () => {
+describe('TestTooShortModal', () => {
   let fixture: ComponentFixture<TestTooShortModal>;
   let component: TestTooShortModal;
   let modalController: ModalController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [TestTooShortModal, AppModule],
+      imports: [TestTooShortModal, IonicModule, AppModule],
       providers: [{ provide: ModalController, useClass: ModalControllerMock }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(TestTooShortModal);
-    component = fixture.componentInstance;
     modalController = TestBed.inject(ModalController);
+    component = fixture.componentInstance;
+    component.modalCtrl = modalController;
+    fixture.detectChanges();
   });
 
-  describe('TestTooShortModal', () => {
-    describe('clickReturn', () => {
-      it('should dismiss the modal with false', async () => {
-        spyOn(modalController, 'dismiss').and.resolveTo(false);
-        await component.clickReturn();
-        expect(modalController.dismiss).toHaveBeenCalledWith(false);
-      });
-    });
+  it('should dismiss the modal with false', async () => {
+    await component.clickReturn();
+    expect(modalController.dismiss).toHaveBeenCalledWith(false);
+  });
 
-    describe('clickContinue', () => {
-      it('should dismiss the modal with true', async () => {
-        spyOn(modalController, 'dismiss').and.resolveTo(true);
-        await component.clickContinue();
-        expect(modalController.dismiss).toHaveBeenCalledWith(true);
-      });
-    });
+  it('should dismiss the modal with true', async () => {
+    await component.clickContinue();
+    expect(modalController.dismiss).toHaveBeenCalledWith(true);
   });
 });
