4173d247c103763faed835d1ef052bf62e7d63cd
Rhys Lewis <rhys.lewis@dvsa.gov.uk>
2025-08-13 14:55:25 +0100

    Added short delay for consistency purposes

--- src/app/providers/device-authentication/device-authentication.ts
+++ src/app/providers/device-authentication/device-authentication.ts
@@ -42,16 +42,14 @@ export class DeviceAuthenticationProvider {
   performBiometricVerification = async (isPracticeMode = false): Promise<void> => {
     try {
       await this.deviceProvider.disableSingleAppMode();
+
+      // Wait for a short duration to ensure that single app mode is disabled fully (THIS CODE WILL NOT WORK CONSISTENTLY ON AN IPAD IF THIS LINE IS NOT PRESENT)
+      await new Promise((resolve) => setTimeout(resolve, 100));
+
       await NativeBiometric.verifyIdentity({
         reason: 'Please authenticate',
         useFallback: true, // fallback to passcode if biometric authentication unavailable
       });
-      // setTimeout((async ()  => {
-      //   await NativeBiometric.verifyIdentity({
-      //     reason: 'Please authenticate',
-      //     useFallback: true, // fallback to passcode if biometric authentication unavailable
-      //   });
-      // }), 500);
     } finally {
       if (!isPracticeMode) {
         await this.loadingProvider.handleUILoading(true);
