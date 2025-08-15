dfb89bf9c6cb6150480290c506ae7c5ee8924cef
ryan <ryan_gittins_33@hotmail.co.uk>
2025-08-15 12:46:06 +0100

    added import into journal-future-test-modal.module.ts for the analytics effects

--- src/app/pages/journal/components/journal-future-test-modal/journal-future-test-modal.module.ts
+++ src/app/pages/journal/components/journal-future-test-modal/journal-future-test-modal.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ComponentsModule } from '@components/common/common-components.module';
+import { EffectsModule } from '@ngrx/effects';
 import { JournalFutureTestModal } from '@pages/journal/components/journal-future-test-modal/journal-future-test-modal';
+import { JournalFutureTestModalAnalyticsEffects } from '@pages/journal/components/journal-future-test-modal/journal-future-test-modal.analytics.effects';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ComponentsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ComponentsModule,
+    EffectsModule.forFeature([JournalFutureTestModalAnalyticsEffects]),
+  ],
   declarations: [JournalFutureTestModal],
 })
 export class JournalFutureTestModalModule {}
