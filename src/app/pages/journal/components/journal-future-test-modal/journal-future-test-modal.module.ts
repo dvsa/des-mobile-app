0cb9189b0fad87db545b940ebcbe477871a8cf92
ryan <ryan_gittins_33@hotmail.co.uk>
2025-08-15 13:28:27 +0100

    Re-arranged directory structure of analytics & effects to be more in-line with rest of application

--- src/app/pages/journal/components/journal-future-test-modal/journal-future-test-modal.module.ts
+++ src/app/pages/journal/components/journal-future-test-modal/journal-future-test-modal.module.ts
@@ -4,18 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { ComponentsModule } from '@components/common/common-components.module';
-import { EffectsModule } from '@ngrx/effects';
 import { JournalFutureTestModal } from '@pages/journal/components/journal-future-test-modal/journal-future-test-modal';
-import { JournalFutureTestModalAnalyticsEffects } from '@pages/journal/components/journal-future-test-modal/journal-future-test-modal.analytics.effects';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ComponentsModule,
-    EffectsModule.forFeature([JournalFutureTestModalAnalyticsEffects]),
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, ComponentsModule],
   declarations: [JournalFutureTestModal],
 })
 export class JournalFutureTestModalModule {}
