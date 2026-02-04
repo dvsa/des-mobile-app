af5f9aa17df923f8a9eae284efaa116c3b7ba22b
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-02-04 14:25:20 +0000

    Corrected cancel enum to allow app to display correct page content

--- src/app/providers/authentication/authentication.constants.ts
+++ src/app/providers/authentication/authentication.constants.ts
@@ -2,8 +2,7 @@ import { AuthResult } from '@ionic-enterprise/auth';
 
 export enum AuthenticationError {
   NO_INTERNET = 'The Internet connection appears to be offline.',
-  USER_CANCELLED = 'The operation couldn’t be completed. ' +
-    '(com.apple.AuthenticationServices.WebAuthenticationSession error 1.). (null)',
+  USER_CANCELLED = 'user canceled auth',
   NO_RESPONSE = 'application did not receive response from broker',
   USER_NOT_AUTHORISED = 'User is not authorized to access this message with an explicit deny',
 }
