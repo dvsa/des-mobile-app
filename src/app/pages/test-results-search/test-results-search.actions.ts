1509c143b1a505752a5f40518938ed6f8636d533
Rhys Lewis <88314925+RLCorp@users.noreply.github.com>
2026-02-06 12:15:23 +0000

    Added additional analytics to advanced search (#2148)

--- src/app/pages/test-results-search/test-results-search.actions.ts
+++ src/app/pages/test-results-search/test-results-search.actions.ts
@@ -9,6 +9,17 @@ export const PerformApplicationReferenceSearch = createAction(
 
 export const PerformDriverNumberSearch = createAction('[TestResultSearch] Performed a driver number search');
 
+export const TestCentreSelected = createAction('[TestResultSearch] Selected a test centre');
+export const StaffNumberChanged = createAction('[TestResultSearch] Entered a staff number');
+export const StartDateChanged = createAction('[TestResultSearch] Changed the start date');
+export const EndDateChanged = createAction('[TestResultSearch] Changed the end date');
+export const ActivityCodeChanged = createAction('[TestResultSearch] Changed the activity code');
+export const TestCategoryChanged = createAction('[TestResultSearch] Changed the test category');
+export const PassCertificateChanged = createAction('[TestResultSearch] Changed the pass certificate number');
+export const RekeyBoxToggled = createAction('[TestResultSearch] Toggled the rekey box', (isActive: boolean) => ({
+  isActive,
+}));
+
 export const PerformLDTMSearch = createAction(
   '[TestResultSearch] Performed a LDTM search',
   (advancedSearchParams: AdvancedSearchParams) => ({ advancedSearchParams })
