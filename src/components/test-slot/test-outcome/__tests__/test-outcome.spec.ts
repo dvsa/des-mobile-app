27fa54a89307d09fcc58136112014a0d1be29182
Andrew Setterfield <48550267+andrewsetterfield@users.noreply.github.com>
2026-04-20 09:59:49 +0100

    feat: (mes-10940) loosen category comparison for bike tests (#2262)
    * feat: loosen category comparison for bike tests
    
    * feat: use mod1 boolean to prevent mod2 comparisons if not needed
    
    * chore: increase unit test coverage
    
    * chore: remove inline comment

--- src/components/test-slot/test-outcome/__tests__/test-outcome.spec.ts
+++ src/components/test-slot/test-outcome/__tests__/test-outcome.spec.ts
@@ -567,6 +567,23 @@ describe('TestOutcomeComponent', () => {
         expect(component.subscription.unsubscribe).toHaveBeenCalled();
       });
     });
+
+    describe('isCategoryEqual', () => {
+      it('should return true for identical categories', () => {
+        expect(component.isCategoryEqual(TestCategory.B, TestCategory.B)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUA1M1, TestCategory.EUA1M1)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUA1M2, TestCategory.EUAM2)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUAMM1, TestCategory.EUA1M1)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUA1M1, TestCategory.EUA2M1)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUA2M1, TestCategory.EUAM1)).toEqual(true);
+        expect(component.isCategoryEqual(TestCategory.EUAMM1, TestCategory.EUAM1)).toEqual(true);
+      });
+
+      it('should return false for different categories', () => {
+        expect(component.isCategoryEqual(TestCategory.C, TestCategory.B)).toEqual(false);
+        expect(component.isCategoryEqual(TestCategory.EUA1M1, TestCategory.EUAM2)).toEqual(false);
+      });
+    });
   });
 
   describe('DOM', () => {
