c7b500cc9db22b647f9859c7a89871eea8543e9b
MarcBowen <marc.bowen@dvsa.gov.uk>
2026-05-11 11:32:20 +0100

    update sonar scanner version

--- buildScripts/sonar-properties-update.sh
+++ buildScripts/sonar-properties-update.sh
@@ -10,5 +10,4 @@ echo "
 APP_VERSION=$(xmllint -xpath 'string(//*[local-name()="widget"]/@version)' config.xml)
 
 # Update the project version by replacing the <version> with the value inside config.xml
-touch sonar-project.properties
 sed -i "" "s/^sonar.projectVersion=.*/sonar.projectVersion=${APP_VERSION}/" sonar-project.properties
