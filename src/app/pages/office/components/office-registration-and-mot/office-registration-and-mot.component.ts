1d732bdea1c69f4fc1cb818f4f5d9ddc198ecc77
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-09-15 15:01:15 +0100

    Removed ionic module

--- src/app/pages/office/components/office-registration-and-mot/office-registration-and-mot.component.ts
+++ src/app/pages/office/components/office-registration-and-mot/office-registration-and-mot.component.ts
@@ -2,13 +2,13 @@ import { Component, Input } from '@angular/core';
 import { MotStatusDisplayTextComponent } from '@components/common/mot-status-display-text/mot-status-display-text.component';
 import { RegistrationWithPreviousMotTextComponent } from '@components/common/registration-with-previous-mot-text/registration-with-previous-mot-text.component';
 import { MotStatusCodes } from '@dvsa/mes-mot-schema';
-import { IonicModule } from '@ionic/angular/lazy';
+import { IonCol, IonRow, IonText } from '@ionic/angular';
 
 @Component({
   selector: 'office-registration-and-mot',
   templateUrl: './office-registration-and-mot.component.html',
   styleUrls: ['./office-registration-and-mot.component.scss'],
-  imports: [RegistrationWithPreviousMotTextComponent, MotStatusDisplayTextComponent, IonicModule],
+  imports: [RegistrationWithPreviousMotTextComponent, MotStatusDisplayTextComponent, IonRow, IonCol, IonText],
   standalone: true,
 })
 export class OfficeRegistrationAndMotComponent {
