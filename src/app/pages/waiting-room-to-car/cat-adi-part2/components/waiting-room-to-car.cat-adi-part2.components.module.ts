2082a8bc8168b2e141a1f4bf5e03bec0c8bae9de
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-09-22 14:35:31 +0100

    Fixed an issue where user could not complete adi2s

--- src/app/pages/waiting-room-to-car/cat-adi-part2/components/waiting-room-to-car.cat-adi-part2.components.module.ts
+++ src/app/pages/waiting-room-to-car/cat-adi-part2/components/waiting-room-to-car.cat-adi-part2.components.module.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ComponentsModule } from '@components/common/common-components.module';
 import { DirectivesModule } from '@directives/directives.module';
-import { IonCol, IonRow } from '@ionic/angular';
+import { IonCol, IonInput, IonRow } from '@ionic/angular';
 import { WaitingRoomToCarComponentsModule } from '../../components/waiting-room-to-car.components.module';
 import { OrditTrainerCatAdiPart2Component } from './ordit-trainer/ordit-trainer.cat-adi-part2';
 import { TrainerRegistrationNumberCatAdiPart2Component } from './trainer-registration-number/trainer-registration-number.cat-adi-part2';
@@ -20,13 +20,13 @@ import { VehicleChecksModalCatADIPart2Module } from './vehicle-checks-modal/vehi
   imports: [
     CommonModule,
     ComponentsModule,
-
     DirectivesModule,
     WaitingRoomToCarComponentsModule,
     ReactiveFormsModule,
     VehicleChecksModalCatADIPart2Module,
     IonCol,
     IonRow,
+    IonInput,
   ],
   exports: [
     TrainingRecordsCatAdiPart2Component,
