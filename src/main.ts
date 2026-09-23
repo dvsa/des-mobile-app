b6cabcb3495900cd62c98a118b0062130c0cc0c5
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-09-23 11:23:11 +0100

    migrated cli

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@app/app.module';
@@ -9,6 +9,6 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
   // eslint-disable-next-line no-console
   .catch((err) => console.log(err));
