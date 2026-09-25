9d43136d2aafb71d9f5996e7ac5159d8c976f740
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-09-25 12:12:43 +0100

    removed deprecated code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from '@app/app.module';
 import { environment } from '@environments/environment';
 
@@ -8,7 +7,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
+platformBrowser()
   .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
   // eslint-disable-next-line no-console
   .catch((err) => console.log(err));
